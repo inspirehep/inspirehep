a365308e6e1c17b6f3ad348e546899467b0987d6
harunurhan <harunurhan17@gmail.com>
2016-07-12 10:30:49 +0200

    Fix source maps for ts files in chrome dev tools
    * Fix source maps of ts files in chrome dev tools.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -46,7 +46,7 @@ module.exports = webpackMerge(commonConfig, {
    * See: http://webpack.github.io/docs/configuration.html#devtool
    * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
    */
-  devtool: 'cheap-module-source-map',
+  devtool: '#inline-source-map',
 
   /**
    * Options affecting the output of the compilation.
