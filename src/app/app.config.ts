191d76b0d043d7acb1304261ad3232325e9fdb9e
harunurhan <harunurhan17@gmail.com>
2016-11-15 14:47:24 +0100

    fix travis tslint and tslint issues
    Signed-off-by: harunurhan <harunurhan17@gmail.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,110 +4,104 @@ export let APP_CONFIG = new OpaqueToken('app.config');
 
 export const INSPIRE_EDITOR_CONFIG: AppConfig = {
   schemaOptions: {
-    "abstracts.items.properties.value": {
-      "x_editor_priority": 1
+    'abstracts.items.properties.value': {
+      x_editor_priority: 1
     },
-    "accelerator_experiments.items.properties.curated_relation": {
-      "x_editor_hidden": true
+    'arxiv_eprints.items.properties.value': {
+      x_editor_priority: 1
     },
-    "accelerator_experiments.items.properties.record": {
-      "x_editor_hidden": true
+    'authors.items.properties.affiliations.items.properties.curated_relation': {
+      x_editor_hidden: true
     },
-    "arxiv_eprints.items.properties.value": {
-      "x_editor_priority": 1
+    'authors.items.properties.affiliations.items.properties.record': {
+      x_editor_hidden: true
     },
-    "authors.items.properties.affiliations.items.properties.curated_relation": {
-      "x_editor_hidden": true
+    'authors.items.properties.affiliations.items.properties.value': {
+      x_editor_priority: 1
     },
-    "authors.items.properties.affiliations.items.properties.record": {
-      "x_editor_hidden": true
+    'authors.items.properties.full_name': {
+      x_editor_priority: 1
     },
-    "authors.items.properties.affiliations.items.properties.value": {
-      "x_editor_priority": 1
+    'authors.items.properties.affiliations': {
+      x_editor_priority: 2
     },
-    "authors.items.properties.full_name": {
-      "x_editor_priority": 1
+    'authors.items.properties.curated_relation': {
+      x_editor_hidden: true
     },
-    "authors.items.properties.affiliations": {
-      "x_editor_priority": 2
+    'authors.items.properties.uuid': {
+      x_editor_hidden: true
     },
-    "authors.items.properties.curated_relation": {
-      "x_editor_hidden": true
+    'authors.items.properties.record': {
+      x_editor_hidden: true
     },
-    "authors.items.properties.uuid": {
-      "x_editor_hidden": true
+    'collaboration.items.properties.record': {
+      x_editor_hidden: true
     },
-    "authors.items.properties.record": {
-      "x_editor_hidden": true
+    'collaboration.items.properties.value': {
+      x_editor_priority: 1
     },
-    "collaboration.items.properties.record": {
-      "x_editor_hidden": true
+    'dois.items.properties.value': {
+      x_editor_priority: 1
     },
-    "collaboration.items.properties.value": {
-      "x_editor_priority": 1
+    'external_system_numbers.items.properties.value': {
+      x_editor_priority: 1
     },
-    "dois.items.properties.value": {
-      "x_editor_priority": 1
+    'hidden_notes.items.properties.value': {
+      x_editor_priority: 1
     },
-    "external_system_numbers.items.properties.value": {
-      "x_editor_priority": 1
+    'isbns.items.properties.value': {
+      x_editor_priority: 1
     },
-    "hidden_notes.items.properties.value": {
-		  "x_editor_priority": 1
+    'keywords.items.properties.keyword': {
+      x_editor_priority: 1
     },
-    "isbns.items.properties.value": {
-		  "x_editor_priority": 1
+    'persistent_identifiers.items.properties.value': {
+      x_editor_priority: 1
     },
-    "keywords.items.properties.keyword": {
-		  "x_editor_priority": 1
+    'public_notes.items.properties.value': {
+      x_editor_priority: 1
     },
-    "persistent_identifiers.items.properties.value": {
-		  "x_editor_priority": 1
+    'publication_info.items.properties.conference_record': {
+      x_editor_hidden: true
     },
-    "public_notes.items.properties.value": {
-	  	"x_editor_priority": 1
+    'publication_info.items.properties.curated_relation': {
+      x_editor_hidden: true
     },
-    "publication_info.items.properties.conference_record": {
-      "x_editor_hidden": true
+    'publication_info.items.properties.parent_record': {
+      x_editor_hidden: true
     },
-    "publication_info.items.properties.curated_relation": {
-      "x_editor_hidden": true
+    'publication_info.items.properties.journal_record': {
+      x_editor_hidden: true
     },
-    "publication_info.items.properties.parent_record": {
-      "x_editor_hidden": true
+    'publication_info.items.properties.journal_title': {
+      x_editor_priority: 1
     },
-    "publication_info.items.properties.journal_record": {
-      "x_editor_hidden": true
+    'publication_info.items.properties.journal_volume': {
+      x_editor_priority: 2
     },
-    "publication_info.items.properties.journal_title": {
-      "x_editor_priority": 1
+    'publication_info.items.properties.page_start': {
+      x_editor_priority: 3
     },
-    "publication_info.items.properties.journal_volume": {
-      "x_editor_priority": 2
+    'publication_info.items.properties.page_end': {
+      x_editor_priority: 4
     },
-    "publication_info.items.properties.page_start": {
-      "x_editor_priority": 3
+    'publication_info.items.properties.year': {
+      x_editor_priority: 5
     },
-    "publication_info.items.properties.page_end": {
-      "x_editor_priority": 4
+    'references.items.properties.record': {
+      x_editor_hidden: true
     },
-    "publication_info.items.properties.year": {
-      "x_editor_priority": 5
+    'report_numbers.items.properties.value': {
+      x_editor_priority: 1
     },
-    "references.items.properties.record": {
-      "x_editor_hidden": true
+    'self': {
+      x_editor_hidden: true
     },
-    "report_numbers.items.properties.value": {
-      "x_editor_priority": 1
+    'titles.items.properties.title': {
+      x_editor_priority: 1
     },
-    "self": {
-      "x_editor_hidden": true
-    },
-    "titles.items.properties.title": {
-      "x_editor_priority": 1
-    },
-    "urls.items.properties.value": {
-      "x_editor_priority": 1
+    'urls.items.properties.value': {
+      x_editor_priority: 1
     }
-}
+  }
 };
