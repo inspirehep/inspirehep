0e6ee164e413fc585b22d6b492b5b0df3c8d1921
harunurhan <harunurhan17@gmail.com>
2018-04-16 14:27:01 +0200

    add immutablejs and complete search reducer

--- ui/src/reducers/__tests_/search.test.js
+++ ui/src/reducers/__tests_/search.test.js
@@ -1,10 +1,23 @@
+import { Map, fromJS } from 'immutable';
 import reducer from '../search';
 import * as types from '../../actions/actionTypes';
 
 describe('search reducer', () => {
-  it('should mark state.search true when SEARCHING', () => {
-    const state = reducer({}, { type: types.SEARCHING });
-    const expected = { searching: true };
+  it('SEARCHING', () => {
+    const state = reducer(Map(), { type: types.SEARCHING });
+    const expected = Map({ searching: true });
+    expect(state).toEqual(expected);
+  });
+
+  it('SEARCH_SUCCESSFUL', () => {
+    const state = reducer(Map(), { type: types.SEARCH_SUCCESS, payload: ['found'] });
+    const expected = fromJS({ searching: false, results: ['found'] });
+    expect(state).toEqual(expected);
+  });
+
+  it('SEARCH_ERROR', () => {
+    const state = reducer(Map(), { type: types.SEARCH_ERROR, payload: { message: 'error' } });
+    const expected = fromJS({ searching: false, error: { message: 'error' } });
     expect(state).toEqual(expected);
   });
 });
