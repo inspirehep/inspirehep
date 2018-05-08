e1773ec1397df13d118c5c8211f1d188a203157d
Dinika Saxena <dinika.saxena@cern.ch>
2018-05-08 17:20:44 +0200

    add exceptions dashboard
    * Adds initial implementation of exceptions dashboard

--- ui/src/holdingpen/containers/DashboardPage.jsx
+++ ui/src/holdingpen/containers/DashboardPage.jsx
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 
 class DashboardPage extends Component {
   render() {
-    return (
-      <div>
-        DashboardPage
-      </div>
-    );
+    return <div>DashboardPage</div>;
   }
 }
 
--- ui/src/holdingpen/index.jsx
+++ ui/src/holdingpen/index.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import DashboardPage from './containers/DashboardPage';
 import SearchPage from './containers/SearchPage';
+import ExceptionsDashboardPage from './containers/ExceptionsDashboardPage';
 
 class Holdingpen extends Component {
   render() {
@@ -10,6 +11,11 @@ class Holdingpen extends Component {
       <div>
         <Route exact path="/holdingpen" component={SearchPage} />
         <Route exact path="/holdingpen/dashboard" component={DashboardPage} />
+        <Route
+          exact
+          path="/holdingpen/exceptions-dashboard"
+          component={ExceptionsDashboardPage}
+        />
       </div>
     );
   }
