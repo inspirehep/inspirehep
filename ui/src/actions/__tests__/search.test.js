d1339b266e1aac857870e6f4788e52b08483640b
Harris Tzovanakis <me@drjova.com>
2018-05-23 17:15:17 +0200

    search: fix relative url issue
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- ui/src/actions/__tests__/search.test.js
+++ ui/src/actions/__tests__/search.test.js
@@ -34,7 +34,7 @@ describe('search - async action creator', () => {
   });
 
   it('creates SEARCH_SUCCESS and pushes new history state when search is done', async (done) => {
-    const testQueryUrl = 'test?size=10&q=test';
+    const testQueryUrl = '/test?size=10&q=test';
     mockHttp.onGet(testQueryUrl).replyOnce(200, {});
 
     const expectedActions = [
@@ -50,7 +50,7 @@ describe('search - async action creator', () => {
   });
 
   it('creates SEARCH_SUCCESS and pushes new history state when without search if scope query is present', async (done) => {
-    const testQueryUrl = 'test?size=10';
+    const testQueryUrl = '/test?size=10';
     mockHttp.onGet(testQueryUrl).replyOnce(200, {});
 
     const expectedActions = [
@@ -66,7 +66,7 @@ describe('search - async action creator', () => {
   });
 
   it('creates SEARCH_SUCCESS but skips history state push if there is no query at all', async (done) => {
-    const testQueryUrl = 'test?';
+    const testQueryUrl = '/test?';
     mockHttp.onGet(testQueryUrl).replyOnce(200, {});
 
     const expectedActions = [
@@ -81,7 +81,7 @@ describe('search - async action creator', () => {
   });
 
   it('creates SEARCH_SUCCESS and pushes new history state with page=1 if location has page param and there is a new query', async (done) => {
-    const testQueryUrl = 'test?page=1&q=test';
+    const testQueryUrl = '/test?page=1&q=test';
     mockHttp.onGet(testQueryUrl).replyOnce(200, {});
 
     const expectedActions = [
@@ -102,7 +102,7 @@ describe('search - async action creator', () => {
   });
 
   it('creates SEARCH_SUCCESS and pushes new history state without reseting page=1 if location has page param and there is a new query with page', async (done) => {
-    const testQueryUrl = 'test?page=3';
+    const testQueryUrl = '/test?page=3';
     mockHttp.onGet(testQueryUrl).replyOnce(200, {});
 
     const expectedActions = [
@@ -123,7 +123,7 @@ describe('search - async action creator', () => {
   });
 
   it('creates SEARCH_ERROR when search fails', async (done) => {
-    const testQueryUrl = 'test?size=10&q=test';
+    const testQueryUrl = '/test?size=10&q=test';
     mockHttp.onGet(testQueryUrl).networkError();
 
     const expectedActions = [
