63fc59ada55b61449f2cd9e66d53d4e792abccd8
harunurhan <harunurhan17@gmail.com>
2018-06-04 17:16:14 +0200

    limit visible checkbox aggregation buckets
    Limits visible checkbox aggregation to 10 by default and each time
    'Show more' is clicked, 10 more are displayed.

--- ui/src/common/components/CheckboxAggregation.jsx
+++ ui/src/common/components/CheckboxAggregation.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
-import { List, Tag } from 'antd';
+import { List, Tag, Button } from 'antd';
 
 import CheckboxItem from './CheckboxItem';
+
+const BUCKET_CHUNK_SIZE = 10;
 
 class CheckboxAggregation extends Component {
   static getDerivedStateFromProps(nextProps, prevState) {
     let selectionMap;
     if (nextProps.selections) {
-      selectionMap = nextProps.selections
-        .reduce((map, key) => map.set(key, true), Immutable.Map());
+      selectionMap = nextProps.selections.reduce(
+        (map, key) => map.set(key, true),
+        Immutable.Map()
+      );
     } else {
       selectionMap = Immutable.Map();
     }
@@ -23,37 +27,66 @@ class CheckboxAggregation extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { selectionMap: Immutable.Map() };
+    this.state = {
+      selectionMap: Immutable.Map(),
+      maxBucketCountToDisplay: BUCKET_CHUNK_SIZE,
+    };
+
+    this.onShowMoreClick = this.onShowMoreClick.bind(this);
   }
 
   onSelectionChange(key, selected) {
     let { selectionMap } = this.state;
     selectionMap = selectionMap.set(key, selected);
     this.setState({ selectionMap });
-    const selections = selectionMap.keySeq()
+    const selections = selectionMap
+      .keySeq()
       .filter(bucketKey => selectionMap.get(bucketKey))
       .toArray();
     this.props.onChange(selections);
   }
 
+  onShowMoreClick() {
+    const { maxBucketCountToDisplay } = this.state;
+    this.setState({
+      maxBucketCountToDisplay: maxBucketCountToDisplay + BUCKET_CHUNK_SIZE,
+    });
+  }
+
+  renderShowMore() {
+    const { buckets } = this.props;
+    const { maxBucketCountToDisplay } = this.state;
+
+    if (maxBucketCountToDisplay >= buckets.size) {
+      return null;
+    }
+
+    return <Button onClick={this.onShowMoreClick}>Show more</Button>;
+  }
+
   render() {
+    const { maxBucketCountToDisplay, selectionMap } = this.state;
     return (
       <div style={{ width: '100%' }}>
-        <List header={<strong>{this.props.name}</strong>}>
-          {this.props.buckets.map(bucket => (
+        <List
+          header={<strong>{this.props.name}</strong>}
+          footer={this.renderShowMore()}
+        >
+          {this.props.buckets.take(maxBucketCountToDisplay).map(bucket => (
             <List.Item key={bucket.get('key')}>
-              <List.Item.Meta title={
-                <CheckboxItem
-                  checked={this.state.selectionMap.get(bucket.get('key'))}
-                  onChange={(checked) => { this.onSelectionChange(bucket.get('key'), checked); }}
-                >
-                  {bucket.get('key')}
-                </CheckboxItem>
-              }
+              <List.Item.Meta
+                title={
+                  <CheckboxItem
+                    checked={selectionMap.get(bucket.get('key'))}
+                    onChange={checked => {
+                      this.onSelectionChange(bucket.get('key'), checked);
+                    }}
+                  >
+                    {bucket.get('key')}
+                  </CheckboxItem>
+                }
               />
-              <Tag>
-                {bucket.get('doc_count')}
-              </Tag>
+              <Tag>{bucket.get('doc_count')}</Tag>
             </List.Item>
           ))}
         </List>
@@ -69,6 +102,5 @@ CheckboxAggregation.propTypes = {
   // eslint-disable-next-line react/no-unused-prop-types
   selections: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-
 
 export default CheckboxAggregation;
