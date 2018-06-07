0882519571e8aaa27f7bae527b1d30f4442801f7
harunurhan <harunurhan17@gmail.com>
2018-06-07 13:09:10 +0200

    fix homepage layout

--- ui/src/home/index.jsx
+++ ui/src/home/index.jsx
@@ -1,39 +1,52 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'antd';
 
 import './index.scss';
 import SearchBoxContainer from '../common/containers/SearchBoxContainer';
+import InlineList from '../common/components/InlineList';
+
+const LITERATURE_QUERY_EXAMPLES = [
+  'find a richter, b and t quark and date > 1984',
+  'find j phys.rev.,D50,1140',
+];
 
 class Home extends Component {
   render() {
     return (
-      <div className="__Home__">
-        <div>
+      <Row className="__Home__" type="flex" justify="center" align="middle">
+        <Col span={10}>
           <Row>
-            <Col span={10} offset={4}>
-              <h3>Discover High-Energy Phytsics content</h3>
+            <Col>
+              <h3>Discover High-Energy Physics content</h3>
             </Col>
           </Row>
           <Row>
-            <Col span={10} offset={4}>
-              <div className="large-searchbox">
-                <SearchBoxContainer />
-              </div>
+            <Col>
+              <SearchBoxContainer />
             </Col>
           </Row>
-          <Row>
-            <Col span={10} offset={4}>
-              Query Examples
+          <Row className="examples-container">
+            <Col>
+              <InlineList
+                label="Try"
+                items={LITERATURE_QUERY_EXAMPLES}
+                renderItem={query => (
+                  <Link to={`/literature?q=${query}`}>{query}</Link>
+                )}
+              />
             </Col>
           </Row>
-          <Row>
-            <Col span={22} offset={1}>
-              INSPIRE Labs provides a sneak preview of new features and designs
-              currently under development.
+          <Row className="description-container">
+            <Col>
+              <h3>
+                INSPIRE Labs provides a sneak preview of new features and
+                designs currently under development.
+              </h3>
             </Col>
           </Row>
-        </div>
-      </div>
+        </Col>
+      </Row>
     );
   }
 }
