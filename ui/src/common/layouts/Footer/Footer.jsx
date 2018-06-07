7fa576ee8d1e0c7f46e32227264f869d54ff42cc
harunurhan <harunurhan17@gmail.com>
2018-06-07 14:21:51 +0200

    add footer

---
+++ ui/src/common/layouts/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Layout, Row, Col } from 'antd';
+
+import './Footer.scss';
+import InlineList from '../../components/InlineList';
+
+const FOOTER_LINKS = [
+  {
+    display: 'Privacy Policy',
+    href: '//inspirehep.net/info/general/privacy-policy',
+  },
+  {
+    display: 'Terms of Use',
+    href: '//inspirehep.net/info/general/terms-of-use',
+  },
+  {
+    display: 'Twitter',
+    href: 'https://twitter.com/inspirehep',
+  },
+  {
+    display: 'Blog',
+    href: '//blog.inspirehep.net',
+  },
+];
+
+const Footer = () => (
+  <Layout.Footer className="__Footer__">
+    <Row type="flex" align="bottom" justify="space-between">
+      <Col span={4}>
+        <a href="//invenio-software.org/">Powered by Invenio</a>
+      </Col>
+      <Col>
+        <InlineList
+          separateItems={false}
+          items={FOOTER_LINKS}
+          renderItem={link => (
+            <a className="footer-link" href={link.href}>
+              {link.display}
+            </a>
+          )}
+        />
+      </Col>
+    </Row>
+  </Layout.Footer>
+);
+
+export default Footer;
