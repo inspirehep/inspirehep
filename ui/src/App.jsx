860308bd2c2b68c21230ed5cd044ef81c2d09750
harunurhan <harunurhan17@gmail.com>
2018-07-09 13:43:05 +0200

    user Loadable to split code into multiple chunks
    * Uses react-loadable to split code for some routes so that they are
    not including into main bundle, therefore not needed to be loaded
    initially. However they are loaded ASAP without blocking the user
    interaction so that they are served immediately once user needs them.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Layout } from 'antd';
+import Loadable from 'react-loadable';
 
 import './App.scss';
 import Header from './common/layouts/Header';
 import Footer from './common/layouts/Footer';
+import Loading from './common/components/Loading';
 
-import Holdingpen from './holdingpen';
-import Literature from './literature';
-import Home from './home';
+const Holdingpen$ = Loadable({
+  loader: () => import('./holdingpen'),
+  loading: Loading,
+});
+const Literature$ = Loadable({
+  loader: () => import('./literature'),
+  loading: Loading,
+});
+const Home$ = Loadable({
+  loader: () => import('./home'),
+  loading: Loading,
+});
 
 class App extends Component {
+  componentDidMount() {
+    Loadable.preloadAll();
+  }
+
   render() {
     return (
       <Layout className="__App__">
         <Header />
         <Layout.Content className="content">
           <Switch id="main">
-            <Route exact path="/" component={Home} />
-            <Route path="/holdingpen" component={Holdingpen} />
-            <Route path="/literature" component={Literature} />
+            <Route exact path="/" component={Home$} />
+            <Route path="/holdingpen" component={Holdingpen$} />
+            <Route path="/literature" component={Literature$} />
           </Switch>
         </Layout.Content>
         <Footer />
