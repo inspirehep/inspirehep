4d54f5eb3b8ce2601bffe739bdfdd6396aa4b2cd
harunurhan <harunurhan17@gmail.com>
2018-07-09 13:43:05 +0200

    user Loadable to split code into multiple chunks
    * Uses react-loadable to split code for some routes so that they are
    not including into main bundle, therefore not needed to be loaded
    initially. However they are loaded ASAP without blocking the user
    interaction so that they are served immediately once user needs them.

--- ui/src/literature/index.jsx
+++ ui/src/literature/index.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Loadable from 'react-loadable';
 
-import SearchPage from './containers/SearchPage';
-import DetailPage from './containers/DetailPage';
-
+import Loading from '../common/components/Loading';
 import './index.scss';
+
+const SearchPage$ = Loadable({
+  loader: () => import('./containers/SearchPage'),
+  loading: Loading,
+});
+const DetailPage$ = Loadable({
+  loader: () => import('./containers/DetailPage'),
+  loading: Loading,
+});
 
 class Literature extends Component {
   render() {
     return (
       <div className="__Literature__">
-        <Route exact path="/literature" component={SearchPage} />
-        <Route exact path="/literature/:id" component={DetailPage} />
+        <Route exact path="/literature" component={SearchPage$} />
+        <Route exact path="/literature/:id" component={DetailPage$} />
       </div>
     );
   }
