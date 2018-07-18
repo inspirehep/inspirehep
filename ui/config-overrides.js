c6334ed9d7e110ccffa50160b6bba48588182452
harunurhan <harunurhan17@gmail.com>
2018-07-18 15:58:04 +0200

    add less to override antd theme variables

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -1,8 +1,16 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireSass = require('react-app-rewire-sass-modules');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  let overridenConfig = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-  overridenConfig = rewireSass(config, env);
+  let overridenConfig = injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    config
+  );
+  overridenConfig = rewireSass(overridenConfig, env);
+  overridenConfig = rewireLess.withLoaderOptions({ javascriptEnabled: true })(
+    overridenConfig,
+    env
+  );
   return overridenConfig;
 };
