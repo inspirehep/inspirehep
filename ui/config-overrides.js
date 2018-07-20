073b48e953f474ef0c6b8da68803a1af9bab4192
harunurhan <harunurhan17@gmail.com>
2018-07-20 17:00:07 +0200

    use scss to override antd theme
    * Converts scss variables to less on the fly so that they override
    ant-design ones. This way it is possible to use those variables in
    component style files.
    
    * Imports variables for all scss files on the fly so that it is
    necessary to put import statement for each file.

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -1,13 +1,33 @@
+const path = require('path');
 const { injectBabelPlugin } = require('react-app-rewired');
-const rewireSass = require('react-app-rewire-sass-modules');
+const SassRuleRewirer = require('react-app-rewire-sass-rule');
 const rewireLess = require('react-app-rewire-less');
+
+function rewireSassVariablesToLess(config) {
+  const sassVariablesToLessRule = {
+    test: /\.scss$/,
+    issuer: /\.less$/,
+    use: {
+      loader: './scssVariablesToLess.js',
+    },
+  };
+  config.module.rules.push(sassVariablesToLessRule);
+  return config;
+}
 
 module.exports = function override(config, env) {
   let overridenConfig = injectBabelPlugin(
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
     config
   );
-  overridenConfig = rewireSass(overridenConfig, env);
+  overridenConfig = new SassRuleRewirer()
+    .withRuleOptions({ issuer: { exclude: /\.less$/ } })
+    .withLoaderOptions({
+      data: '@import "variables";',
+      includePaths: [path.resolve(__dirname, './src')],
+    })
+    .rewire(overridenConfig, env);
+  overridenConfig = rewireSassVariablesToLess(overridenConfig);
   overridenConfig = rewireLess.withLoaderOptions({ javascriptEnabled: true })(
     overridenConfig,
     env
