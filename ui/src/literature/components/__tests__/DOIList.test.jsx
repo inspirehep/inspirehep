bc1df94259692d40bbe95683d93f4305b682c537
Dinika Saxena <dinika.saxena@cern.ch>
2018-07-25 11:31:52 +0200

    display material for dois and publication_info
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- ui/src/literature/components/__tests__/DOIList.test.jsx
+++ ui/src/literature/components/__tests__/DOIList.test.jsx
@@ -9,12 +9,12 @@ describe('DOIList', () => {
     // TODO assert if value is there! since it is inside function not in snapshot.
     const dois = fromJS([
       { value: '12.1234/1234567890123_1234' },
-      { value: '99.9999/9999999999999_9999' },
-
+      {
+        value: '99.9999/9999999999999_9999',
+        material: 'erratum',
+      },
     ]);
-    const wrapper = shallow((
-      <DOIList dois={dois} />
-    ));
+    const wrapper = shallow(<DOIList dois={dois} />);
     expect(wrapper.dive()).toMatchSnapshot();
   });
 });
