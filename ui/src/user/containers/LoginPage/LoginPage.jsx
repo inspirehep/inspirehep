bc39031e1c2a174f8b44eb0035e1299b19bfed6c
Dinika Saxena <dinika.saxena@cern.ch>
2018-08-10 16:17:47 +0200

    change login page layout
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- ui/src/user/containers/LoginPage/LoginPage.jsx
+++ ui/src/user/containers/LoginPage/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Row, Col, Button } from 'antd';
+import { Row, Card, Button } from 'antd';
 
 import orcidLogo from './orcid.svg';
 import './LoginPage.scss';
@@ -18,13 +18,27 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <Row className="__LoginPage__ h-100 pt5" type="flex" justify="center">
-        <Col>
+      <Row
+        className="__LoginPage__ h-100"
+        type="flex"
+        justify="center"
+        align="middle"
+      >
+        <Card align="middle">
+          <p className="f4">Please sign in to suggest content to INSPIRE</p>
           <Button className="login-button h3" onClick={this.onLoginClick}>
             <img className="logo mr2" src={orcidLogo} alt="ORCID" />
             <strong>Login with ORCID</strong>
           </Button>
-        </Col>
+          <a
+            className="db pt3"
+            href="https://labs.inspirehep.net/help/knowledge-base/what-is-orcid/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            What is ORCID?
+          </a>
+        </Card>
       </Row>
     );
   }
