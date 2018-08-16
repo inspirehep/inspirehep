ac16b6f03091b68eb5ee3dd549d65433b24226d1
Harris Tzovanakis <me@drjova.com>
2018-08-16 11:30:23 +0200

    Revert "global: remove restricted route for holdingpen"
    This reverts commit ea5a538ed27d05273adcc4c816e3250836b7efe7.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -7,6 +7,7 @@ import './App.scss';
 import Header from './common/layouts/Header';
 import Footer from './common/layouts/Footer';
 import Loading from './common/components/Loading';
+import PrivateRoute from './common/PrivateRoute';
 
 const Holdingpen$ = Loadable({
   loader: () => import('./holdingpen'),
@@ -46,7 +47,7 @@ class App extends Component {
           <Switch id="main">
             <Route exact path="/" component={Home$} />
             <Route path="/user" component={User$} />
-            <Route path="/holdingpen" component={Holdingpen$} />
+            <PrivateRoute path="/holdingpen" component={Holdingpen$} />
             <Route path="/literature" component={Literature$} />
             <Route path="/submissions" component={Submissions$} />
             <Route path="/errors" component={Errors$} />
