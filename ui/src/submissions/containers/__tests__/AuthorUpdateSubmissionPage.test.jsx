c078ae6d73535437dca089d8e802b168346916bc
harunurhan <harunurhan17@gmail.com>
2018-08-20 14:22:15 +0200

    revert enzyme version bump

--- ui/src/submissions/containers/__tests__/AuthorUpdateSubmissionPage.test.jsx
+++ ui/src/submissions/containers/__tests__/AuthorUpdateSubmissionPage.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { getStore } from '../../../fixtures/store';
 import { AUTHOR_UPDATE_FORM_DATA_REQUEST } from '../../../actions/actionTypes';
@@ -14,11 +13,9 @@ describe('AuthorUpdateSubmissionPage', () => {
         id: '123',
       },
     };
-    mount(
-      <Provider store={store}>
-        <AuthorUpdateSubmissionPage match={matchProps} />
-      </Provider>
-    );
+    shallow(
+      <AuthorUpdateSubmissionPage match={matchProps} store={store} />
+    ).dive();
     const actions = store.getActions();
     const expectedAction = actions.find(
       action => action.type === AUTHOR_UPDATE_FORM_DATA_REQUEST
@@ -34,9 +31,9 @@ describe('AuthorUpdateSubmissionPage', () => {
       },
     };
     const store = getStore();
-    const wrapper = mount(
+    const wrapper = shallow(
       <AuthorUpdateSubmissionPage match={initalMatchProps} store={store} />
-    );
+    ).dive();
     wrapper.setProps({ match: { params: { id: '999' } } });
     const actions = store.getActions();
     const expectedAction = actions.find(
@@ -45,6 +42,26 @@ describe('AuthorUpdateSubmissionPage', () => {
         action.payload.recordId === '999'
     );
     expect(expectedAction).toBeDefined();
+  });
+
+  it('does not dispatches author update form data request action again when match props changed but recordId is same', () => {
+    const initalMatchProps = {
+      params: {
+        id: '123',
+      },
+    };
+    const store = getStore();
+    const wrapper = shallow(
+      <AuthorUpdateSubmissionPage match={initalMatchProps} store={store} />
+    ).dive();
+    wrapper.setProps({ match: { params: { id: '123' } } });
+    const actions = store.getActions();
+    const expectedActions = actions.filter(
+      action =>
+        action.type === AUTHOR_UPDATE_FORM_DATA_REQUEST &&
+        action.payload.recordId === '123'
+    );
+    expect(expectedActions.length).toBe(1);
   });
 
   // FIXME: assert that dispatches submit request to update the author onSubmit
