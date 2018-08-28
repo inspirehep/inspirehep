7a8d536cc433958a1eb1a142471465817cede2a3
Dinika Saxena <dinika.saxena@cern.ch>
2018-08-28 14:14:31 +0200

    add tests for sub-routes
    User-Story: INSPIR-1201
    signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- ui/src/fixtures/store.js
+++ ui/src/fixtures/store.js
@@ -4,6 +4,9 @@ import { initialState as search } from '../reducers/search';
 import { initialState as literature } from '../reducers/literature';
 import { initialState as user } from '../reducers/user';
 import { initialState as submissions } from '../reducers/submissions';
+import { initialState as inspect } from '../reducers/inspect';
+import { initialState as exceptions } from '../reducers/exceptions';
+
 import { thunkMiddleware } from '../store';
 
 export function getState() {
@@ -17,6 +20,8 @@ export function getState() {
     search,
     user,
     submissions,
+    inspect,
+    exceptions,
   };
 }
 
