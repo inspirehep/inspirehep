3dbeb12f6bcc578ce733780be3613bfaa29e057a
Dinika Saxena <dinika.saxena@cern.ch>
2018-08-28 14:14:31 +0200

    add tests for sub-routes
    User-Story: INSPIR-1201
    signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

---
+++ ui/src/holdingpen/__tests__/index.test.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
+import { getStore } from '../../fixtures/store';
+import Holdingpen from '../';
+import DashboardPage from '../containers/DashboardPage';
+import ExceptionsPage from '../containers/ExceptionsPage';
+import InspectPage from '../containers/InspectPage';
+
+describe('Literature', () => {
+  it('renders initial state', () => {
+    const component = shallow(<Holdingpen />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('navigates to DashboardPage when /holdingpen/dashboard', () => {
+    const wrapper = mount(
+      <Provider store={getStore()}>
+        <MemoryRouter
+          initialEntries={['/holdingpen/dashboard']}
+          initialIndex={0}
+        >
+          <Holdingpen />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(wrapper.find(DashboardPage)).toExist();
+  });
+
+  it('navigates to DashboardPage when /holdingpen/exceptions', () => {
+    const wrapper = mount(
+      <Provider store={getStore()}>
+        <MemoryRouter
+          initialEntries={['/holdingpen/exceptions']}
+          initialIndex={0}
+        >
+          <Holdingpen />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find(ExceptionsPage)).toExist();
+  });
+
+  it('navigates to InspectPage when /holdingpen/inspect/:id', () => {
+    const wrapper = mount(
+      <Provider store={getStore()}>
+        <MemoryRouter
+          initialEntries={['/holdingpen/inspect/1']}
+          initialIndex={0}
+        >
+          <Holdingpen />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find(InspectPage)).toExist();
+  });
+});
