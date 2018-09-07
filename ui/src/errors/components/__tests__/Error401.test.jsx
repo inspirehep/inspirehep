34801b85bc5e071e38ba3bcb7a30105c0afdfb9f
harunurhan <harunurhan17@gmail.com>
2018-09-07 16:19:43 +0200

    errors: fix folder structure

--- ui/src/errors/components/__tests__/Error401.test.jsx
+++ ui/src/errors/components/__tests__/Error401.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Error401 from '../components/Error401';
+import Error401 from '../Error401';
 
 describe('Error401', () => {
   it('renders Error401 with correct props', () => {
