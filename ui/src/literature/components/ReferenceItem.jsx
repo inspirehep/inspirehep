070eba32d6b9152837e1b94124c638536af8c7bb
Dinika Saxena <dinika.saxena@cern.ch>
2018-09-19 14:46:29 +0200

    put reference label and reference description in 2 different columns
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- ui/src/literature/components/ReferenceItem.jsx
+++ ui/src/literature/components/ReferenceItem.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Map } from 'immutable';
-import { List } from 'antd';
+import { List, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 
 import AuthorList from '../../common/components/AuthorList';
@@ -9,43 +9,54 @@ import Latex from '../../common/components/Latex';
 import PublicationInfoList from '../../common/components/PublicationInfoList';
 
 class ReferenceItem extends Component {
-  static renderTitle(reference) {
+  static renderLabel(reference) {
+    const label = reference.get('label');
+    const labelDisplay = label ? `[${label}] ` : null;
+    return labelDisplay;
+  }
+
+  static renderTitleOrMisc(reference) {
     const recordId = reference.get('control_number');
     const title = reference.getIn(['titles', 0, 'title'], '');
-    const label = reference.get('label');
-
-    const labelDisplay = label ? `[${label}] ` : '';
-
     if (recordId && title) {
       return (
-        <Link to={`/literature/${recordId}`}>
-          {labelDisplay} <Latex>{title}</Latex>
+        <Link className="f5" to={`/literature/${recordId}`}>
+          <Latex>{title}</Latex>
         </Link>
       );
+    } else if (title) {
+      return <span className="f5">{title}</span>;
     }
-    return `${labelDisplay}${title}`;
+    return null;
   }
 
   render() {
     const { reference } = this.props;
     const publicationInfo = reference.get('publication_info');
+    const misc = reference.get('misc');
     return (
       <List.Item>
-        <List.Item.Meta
-          title={ReferenceItem.renderTitle(reference)}
-          description={
-            <Fragment>
-              <AuthorList
-                recordId={reference.get('control_number')}
-                authors={reference.get('authors')}
-              />
-              <PublicationInfoList
-                publicationInfo={publicationInfo}
-                labeled={false}
-              />
-            </Fragment>
-          }
-        />
+        <Row gutter={24} type="flex" justify="space-around" align="middle">
+          <Col>{ReferenceItem.renderLabel(reference)}</Col>
+          <Col>
+            <List.Item.Meta
+              title={ReferenceItem.renderTitleOrMisc(reference)}
+              description={
+                <Fragment>
+                  {misc && <span>{misc}</span>}
+                  <AuthorList
+                    recordId={reference.get('control_number')}
+                    authors={reference.get('authors')}
+                  />
+                  <PublicationInfoList
+                    publicationInfo={publicationInfo}
+                    labeled={false}
+                  />
+                </Fragment>
+              }
+            />
+          </Col>
+        </Row>
       </List.Item>
     );
   }
