33ddf52dc11162552a9b323cf5b691178279e689
Mona Al Owe <awi_moni@yahoo.com>
2018-09-28 12:05:05 +0200

    “Tools” menu should not show curator tools to non-curators
    Corrected the reference extractor link. Made HoldingPen available in the tools menu  only for non-beta users.

--- ui/src/common/layouts/Header/Header.jsx
+++ ui/src/common/layouts/Header/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Layout, Row, Col } from 'antd';
 import PropTypes from 'prop-types';
+import { Set } from 'immutable';
 
 import SearchBoxContainer from './../../containers/SearchBoxContainer';
 import DropdownMenu from '../../components/DropdownMenu';
 import './Header.scss';
 import logo from './logo.svg';
 import LoginOrUserDropdownContainer from '../../containers/LoginOrUserDropdownContainer';
+import { doSetsHaveCommonItem } from '../../utils';
 
-const TOOL_LINKS = [
+const ONLY_SUPER_USERS_AND_CATALOGERS = Set(['superuser', 'cataloger']);
+
+const UNAUTHORIZED_TOOL_LINKS = [
   {
-    to: '/holdingpen/dashboard',
-    display: 'Holdingpen',
-  },
-  {
-    href: '//inspirehep.net/textmining',
+    href: '//inspirehep.net/textmining/',
     display: 'Reference extractor',
   },
   {
@@ -29,46 +29,70 @@ const TOOL_LINKS = [
   },
 ];
 
-const Header = props => (
-  <Layout.Header className="__Header__">
-    <Row type="flex" align="middle" gutter={16}>
-      <Col lg={5}>
-        <Link to="/">
-          <img src={logo} alt="INSPIRE Labs" />
-        </Link>
-      </Col>
-      <Col lg={13} xl={14}>
-        {props.shouldDisplaySearchBox && <SearchBoxContainer />}
-      </Col>
-      <Col lg={6} xl={5}>
-        <Row type="flex" justify="end">
-          <Col className="nav-item-container">
-            <DropdownMenu
-              title="Tools"
-              titleClassName="nav-item"
-              items={TOOL_LINKS}
-            />
-          </Col>
-          <Col className="nav-item-container">
-            <Link className="nav-item" to="/submissions/authors">
-              Submit
+const ALL_TOOL_LINKS = [
+  {
+    href: '/holdingpen',
+    display: 'Holdingpen',
+  },
+  ...UNAUTHORIZED_TOOL_LINKS,
+];
+
+class Header extends Component {
+  getToolLinksForUser() {
+    const { userRoles } = this.props;
+
+    if (doSetsHaveCommonItem(userRoles, ONLY_SUPER_USERS_AND_CATALOGERS)) {
+      return ALL_TOOL_LINKS;
+    }
+    return UNAUTHORIZED_TOOL_LINKS;
+  }
+
+  render() {
+    const { shouldDisplaySearchBox } = this.props;
+
+    return (
+      <Layout.Header className="__Header__">
+        <Row type="flex" align="middle" gutter={16}>
+          <Col lg={5}>
+            <Link to="/">
+              <img src={logo} alt="INSPIRE Labs" />
             </Link>
           </Col>
-          <Col className="nav-item-container">
-            <LoginOrUserDropdownContainer />
+          <Col lg={13} xl={14}>
+            {shouldDisplaySearchBox && <SearchBoxContainer />}
+          </Col>
+          <Col lg={6} xl={5}>
+            <Row type="flex" justify="end">
+              <Col className="nav-item-container">
+                <DropdownMenu
+                  title="Tools"
+                  titleClassName="nav-item"
+                  items={this.getToolLinksForUser()}
+                />
+              </Col>
+              <Col className="nav-item-container">
+                <Link className="nav-item" to="/submissions/authors">
+                  Submit
+                </Link>
+              </Col>
+              <Col className="nav-item-container">
+                <LoginOrUserDropdownContainer />
+              </Col>
+            </Row>
           </Col>
         </Row>
-      </Col>
-    </Row>
-  </Layout.Header>
-);
-
+      </Layout.Header>
+    );
+  }
+}
 Header.propTypes = {
   shouldDisplaySearchBox: PropTypes.bool.isRequired,
+  userRoles: PropTypes.instanceOf(Set).isRequired,
 };
 
 const stateToProps = state => ({
   shouldDisplaySearchBox: state.router.location.pathname !== '/',
+  userRoles: Set(state.user.getIn(['data', 'roles'])),
 });
 
 export default connect(stateToProps)(Header);
