6b13d5d207cc33ad94c9afbb88c90b66a9722340
Dinika Saxena <dinika.saxena@cern.ch>
2018-10-05 15:15:38 +0200

    format section titles in author submission forms
    User-Story: INSPIR-1425
    
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- ui/src/submissions/components/CollapsableForm/CollapseFormSection.jsx
+++ ui/src/submissions/components/CollapsableForm/CollapseFormSection.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Collapse } from 'antd';
 
 class CollapsableFormSection extends Component {
   render() {
+    const { header, ...panelProps } = this.props;
     return (
       <Collapse.Panel
         className="bg-white mb3 overflow-hidden"
-        {...this.props}
+        header={header && <h3 className="fw6">{header}</h3>}
+        {...panelProps}
       />
     );
   }
 }
+
+CollapsableFormSection.propTypes = {
+  header: PropTypes.string,
+};
+
+CollapsableFormSection.defaultProps = {
+  header: null,
+};
 
 export default CollapsableFormSection;
