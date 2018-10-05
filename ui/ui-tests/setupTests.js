0f4cdaf6ebac9c56882ba98235ef43e6d0ac0130
harunurhan <harunurhan17@gmail.com>
2018-10-05 17:56:31 +0200

    ui-tests: fix logout and ci reporter

--- ui/ui-tests/setupTests.js
+++ ui/ui-tests/setupTests.js
@@ -1,4 +1,4 @@
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
-jest.setTimeout(30000);
+jest.setTimeout(60000);
 expect.extend({ toMatchImageSnapshot });
--- ui/ui-tests/utils/user.js
+++ ui/ui-tests/utils/user.js
@@ -6,9 +6,13 @@ async function login() {
 }
 
 async function logout() {
+  const page = await browser.newPage();
+  await page.goto(routes.public.home);
   await page.hover('[data-test-id=my-account-dropdown]');
   await page.waitFor('[data-test-id=logout]');
   await page.click('[data-test-id=logout]');
+  await page.waitFor(() => !document.querySelector('[data-test-id=logout]'));
+  await page.close();
 }
 
 module.exports = {
