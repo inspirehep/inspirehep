daf59e53f07c9aad20abf4264d099b94186d8ba1
harunurhan <harunurhan17@gmail.com>
2018-10-05 15:57:12 +0200

    add image snapshot diff reporter

---
+++ ui/ui-tests/imageSnapshotDiffReporter.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+
+const fs = require('fs');
+const path = require('path');
+
+class ImageSnapshotDiffReporter {
+  constructor(globalConfig, options) {
+    this._globalConfig = globalConfig;
+    this._options = options;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  onTestResult(test, testResult) {
+    if (
+      testResult.numFailingTests &&
+      testResult.failureMessage.match(/different from snapshot/)
+    ) {
+      const parsedTestPath = path.parse(test.path);
+      const diffOutputDir = path.join(
+        parsedTestPath.dir,
+        '__image_snapshots__',
+        '__diff_output__'
+      );
+      const dasherizedTestFileName = parsedTestPath.base.replace(/\./g, '-');
+      // console.log(diffOutputDir);
+      // console.log(dasherizedTestFileName);
+      const diffFilePaths = fs
+        .readdirSync(diffOutputDir)
+        .filter(file => file.startsWith(dasherizedTestFileName));
+      diffFilePaths.forEach(diffFilePath => {
+        const diffAsBase64 = fs
+          .readFileSync(path.join(diffOutputDir, diffFilePath))
+          .toString('base64');
+        console.log(`BASE64 for ${diffFilePath}:`);
+        console.log(diffAsBase64);
+        console.log('===========================');
+      });
+    }
+  }
+}
+
+module.exports = ImageSnapshotDiffReporter;
