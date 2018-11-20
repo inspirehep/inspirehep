e49ee973dc215ff26117398e74b9aa88aab5470d
harunurhan <harunurhan17@gmail.com>
2018-11-20 11:08:22 +0100

    add piwik tracker

---
+++ ui/src/tracker.js
@@ -0,0 +1,16 @@
+import Piwik from 'react-piwik';
+
+// eslint-disable-next-line import/prefer-default-export
+export function injectTrackerToHistory(history) {
+  const { REACT_APP_PIWIK_URL, REACT_APP_PIWIK_SITE_ID } = process.env;
+
+  if (REACT_APP_PIWIK_URL != null && REACT_APP_PIWIK_SITE_ID != null) {
+    const piwik = new Piwik({
+      url: process.env.REACT_APP_PIWIK_URL,
+      siteId: Number(process.env.REACT_APP_PIWIK_SITE_ID),
+    });
+    return piwik.connectToHistory(history);
+  }
+
+  return history;
+}
