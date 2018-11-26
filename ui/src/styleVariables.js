ce22184da19f611a7e3f2e45cb0e78a4e3c21dba
harunurhan <harunurhan17@gmail.com>
2018-11-26 14:41:51 +0100

    bump to CRA v2
    * Uses craco to override config instead react-app-rewire
    
    * Changes handling of style variables that both override ant less
    variables and accessible in scss files

---
+++ ui/src/styleVariables.js
@@ -0,0 +1,7 @@
+// Variables to override https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+
+module.exports = {
+  'primary-color': '#1890ff',
+  'link-hover-color': '#40a9ff',
+  'layout-body-background': '#f0f2f5',
+};
