5c8f719f7a954b1ad8329c99e2332debe0bf4188
harunurhan <harunurhan17@gmail.com>
2018-12-13 15:54:27 +0100

    literature: add new cite formats

--- ui/src/literature/__tests__/citeArticle.test.js
+++ ui/src/literature/__tests__/citeArticle.test.js
@@ -10,24 +10,25 @@ describe('citeArticle', () => {
     mockHttp.reset();
   });
 
-  it('sends request with Accept header based on format and returns text', async (done) => {
+  it('sends request with Accept header based on format and returns text', async done => {
     const citeUrl = '/literature/12345';
-    const format = 'test';
-    mockHttp.onGet(citeUrl, null, { Accept: 'application/x-test' }).replyOnce(200, 'Test');
+    const format = 'x-test';
+    mockHttp
+      .onGet(citeUrl, null, { Accept: 'application/x-test' })
+      .replyOnce(200, 'Test');
     const content = await citeArticle(format, 12345);
     expect(content).toEqual('Test');
     done();
   });
 
-  it('sends request with Accept header based on format and returns text', async (done) => {
+  it('sends request with Accept header based on format and returns text', async done => {
     const citeUrl = '/literature/12345';
-    const format = 'another';
-    mockHttp.onGet(citeUrl, null, { Accept: 'application/x-test' }).replyOnce(200, 'Test');
+    const format = 'x-another';
+    mockHttp
+      .onGet(citeUrl, null, { Accept: 'application/x-test' })
+      .replyOnce(200, 'Test');
     const content = await citeArticle(format, 12345);
-    expect(content).toEqual('nothing for format: another');
+    expect(content).toEqual('Not supported yet :(');
     done();
   });
-
-
-  // TODO: test error case
 });
