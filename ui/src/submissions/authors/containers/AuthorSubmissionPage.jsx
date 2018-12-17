4b299b6636704b477c8f5c250f2e2e0c9bc9065f
harunurhan <harunurhan17@gmail.com>
2018-12-17 11:56:45 +0100

    submissions: change author submission pages
    * Removes "Suggest author" description from update form

--- ui/src/submissions/authors/containers/AuthorSubmissionPage.jsx
+++ ui/src/submissions/authors/containers/AuthorSubmissionPage.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Map } from 'immutable';
+import { Row, Col } from 'antd';
 
 import { submitAuthor } from '../../../actions/submissions';
 import AuthorSubmission from '../components/AuthorSubmission';
+import ExternalLink from '../../../common/components/ExternalLink';
 
 class AuthorSubmissionPage extends Component {
   constructor(props) {
@@ -19,7 +21,26 @@ class AuthorSubmissionPage extends Component {
 
   render() {
     const { error } = this.props;
-    return <AuthorSubmission error={error} onSubmit={this.onSubmit} />;
+    return (
+      <Row type="flex" justify="center">
+        <Col className="mt3 mb3" span={14}>
+          <Row className="mb3 pa3 bg-white">
+            <h3>Suggest author</h3>
+            This form allows you to add new author information. All
+            modifications are transferred to{' '}
+            <ExternalLink href="//inspirehep.net/hepnames">
+              inspirehep.net/hepnames
+            </ExternalLink>{' '}
+            upon approval.
+          </Row>
+          <Row>
+            <Col>
+              <AuthorSubmission error={error} onSubmit={this.onSubmit} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    );
   }
 }
 
