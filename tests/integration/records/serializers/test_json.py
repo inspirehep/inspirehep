ac797ed431d249e5c5fdcce3f50ea8df481e78c6
Harris Tzovanakis <me@drjova.com>
2019-02-07 14:03:49 +0100

    tests: addition of api client
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- tests/integration/records/serializers/test_json.py
+++ tests/integration/records/serializers/test_json.py
@@ -11,13 +11,13 @@ from copy import deepcopy
 import pytest
 
 
-def test_literature_application_json(client, db, create_record):
+def test_literature_application_json(api_client, db, create_record):
     record = create_record("lit", with_indexing=True)
     record_control_number = record.json["control_number"]
 
     expected_status_code = 200
     expected_result = deepcopy(record.json)
-    response = client.get("/literature/{}".format(record_control_number))
+    response = api_client.get("/literature/{}".format(record_control_number))
 
     response_status_code = response.status_code
     response_data = json.loads(response.data)
@@ -26,13 +26,13 @@ def test_literature_application_json(client, db, create_record):
     assert expected_result == response_data["metadata"]
 
 
-def test_literature_application_json_ui(client, db, create_record):
+def test_literature_application_json_ui(api_client, db, create_record):
     record = create_record("lit", with_indexing=True)
     record_control_number = record.json["control_number"]
 
     expected_status_code = 200
     expected_result = deepcopy(record.json)
-    response = client.get("/literature/{}".format(record_control_number))
+    response = api_client.get("/literature/{}".format(record_control_number))
 
     response_status_code = response.status_code
     response_data = json.loads(response.data)
