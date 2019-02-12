c83364d412522c92e1a209dc456873dcb935701e
harunurhan <harunurhan17@gmail.com>
2019-02-12 10:52:40 +0100

    merge travis files
    Merges ui travis configuration into backend one

--- ui/deploy.sh
+++ ui/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-curl -X POST "${DEPLOY_URL}" \
-    -F token=${DEPLOY_TOKEN} \
+curl -X POST "${UI_DEPLOY_URL}" \
+    -F token=${UI_DEPLOY_TOKEN} \
     -F ref=qa \
     -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)"
