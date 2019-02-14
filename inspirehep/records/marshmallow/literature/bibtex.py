bfe8d5e4e7744aca5406dae5bb81301612bf4b5f
Harris Tzovanakis <me@drjova.com>
2019-02-14 15:03:22 +0100

    records: addition of bibtex serializers
    * Moves bibtex serializers from inspire-next, and
      migrate them to marshmallow schemas.
    
    * Improves the unit tests.
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

---
+++ inspirehep/records/marshmallow/literature/bibtex.py
@@ -0,0 +1,350 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+import re
+from marshmallow import Schema, fields, post_dump, pre_dump
+from inspire_utils.record import get_value
+from inspire_utils.date import PartialDate
+from inspire_schemas.readers.literature import LiteratureReader
+from inspire_schemas.readers.conference import ConferenceReader
+from idutils import is_arxiv_post_2007, normalize_isbn
+from isbn import ISBNError
+from six import text_type
+from pybtex.database import Entry, Person
+
+
+class BibTexCommonSchema(Schema):
+    archivePrefix = fields.Method("get_archive_prefix")
+    collaboration = fields.Method("get_collaboration")
+    doc_type = fields.Raw()
+    doi = fields.Method("get_doi")
+    eprint = fields.Method("get_eprint")
+    month = fields.Method("get_month")
+    note = fields.Method("get_note")
+    primaryClass = fields.Method("get_primary_class")
+    title = fields.Method("get_title")
+    url = fields.Method("get_url")
+    year = fields.Method("get_year")
+    texkey = fields.Raw()
+
+    # from different
+    reportNumber = fields.Method("get_report_number")
+    journal = fields.Method("get_journal")
+    volume = fields.Method("get_volume")
+    type_ = fields.Method("get_type", attribute="type", dump_to="type")
+    author = fields.Method("get_author")
+    isbn = fields.Method("get_isbn")
+    number = fields.Method("get_number")
+    edition = fields.Method("get_edition")
+    pages = fields.Method("get_pages")
+    booktitle = fields.Method("get_book_title")
+    series = fields.Method("get_series")
+    publisher = fields.Method("get_publisher")
+    school = fields.Method("get_school")
+    address = fields.Method("get_address")
+
+    @staticmethod
+    def get_date(data, doc_type):
+        publication_year = BibTexCommonSchema.get_best_publication_info(data).get(
+            "year"
+        )
+        thesis_date = get_value(data, "thesis_info.date")
+        imprint_date = get_value(data, "imprints.date[0]")
+
+        if doc_type.endswith("thesis"):
+            date_choice = thesis_date or publication_year or imprint_date
+        else:
+            date_choice = publication_year or thesis_date or imprint_date
+
+        if date_choice:
+            return PartialDate.loads(str(date_choice))
+
+    @staticmethod
+    def get_authors_with_role(authors, role):
+        return [
+            author["full_name"]
+            for author in authors
+            if role in author.get("inspire_roles", ["author"])
+        ]
+
+    @staticmethod
+    def get_document_type(data, doc_type):
+        DOCUMENT_TYPE_MAP = {
+            "article": "article",
+            "book": "book",
+            "book chapter": "inbook",
+            "conference paper": "inproceedings",
+            "proceedings": "proceedings",
+            "report": "techreport",
+            "note": "article",
+        }
+
+        if doc_type in DOCUMENT_TYPE_MAP:
+            return DOCUMENT_TYPE_MAP[doc_type]
+        elif doc_type == "thesis" and get_value(data, "thesis_info.degree_type") in (
+            "phd",
+            "habilitation",
+        ):
+            return "phdthesis"
+        elif doc_type == "thesis":
+            return "mastersthesis"
+        return "misc"
+
+    @staticmethod
+    def get_bibtex_document_type(data):
+        bibtex_doc_types = [
+            BibTexCommonSchema.get_document_type(data, doc_type)
+            for doc_type in data["document_type"]
+        ] + ["misc"]
+        chosen_type = (
+            "article" if "article" in bibtex_doc_types else bibtex_doc_types[0]
+        )
+        return chosen_type
+
+    @staticmethod
+    def get_best_publication_info(data):
+        publication_info = get_value(data, "publication_info", [])
+        only_publications = [
+            entry
+            for entry in publication_info
+            if entry.get("material", "publication") == "publication"
+        ]
+        if not only_publications:
+            return {}
+
+        return sorted(only_publications, key=len, reverse=True)[0]
+
+    def get_eprint(self, data):
+        return get_value(data, "arxiv_eprints.value[0]", default=None)
+
+    def get_archive_prefix(self, data):
+        eprint = get_value(data, "arxiv_eprints.value[0]", default=None)
+        if eprint:
+            return "arXiv"
+        return None
+
+    def get_collaboration(self, data):
+        return ", ".join(get_value(data, "collaborations.value", default=[]))
+
+    def get_doi(self, data):
+        return get_value(data, "dois.value[0]")
+
+    def get_month(self, data):
+        doc_type = data.get("doc_type")
+        date = BibTexCommonSchema.get_date(data, doc_type)
+        if date:
+            return date.month
+
+    def get_year(self, data):
+        doc_type = data.get("doc_type")
+        date = BibTexCommonSchema.get_date(data, doc_type)
+        if date:
+            return date.year
+
+    def get_note(self, data):
+        notices = ("erratum", "addendum")
+        entries = [
+            entry
+            for entry in get_value(data, "publication_info", [])
+            if entry.get("material") in notices
+        ]
+
+        if not entries:
+            return None
+
+        note_strings = [
+            text_type("{field}: {journal} {volume}, {pages} {year}")
+            .format(
+                field=entry["material"].title(),
+                journal=entry.get("journal_title"),
+                volume=entry.get("journal_volume"),
+                pages=LiteratureReader.get_page_artid_for_publication_info(entry, "--"),
+                year="({})".format(entry["year"]) if "year" in entry else "",
+            )
+            .strip()
+            for entry in entries
+        ]
+
+        note_string = "[" + ", ".join(note_strings) + "]"
+        note_string = re.sub(" +", " ", note_string)
+        return re.sub(",,", ",", note_string)
+
+    def get_primary_class(self, data):
+        eprint = get_value(data, "arxiv_eprints.value[0]")
+        if eprint and is_arxiv_post_2007(eprint):
+            return get_value(data, "arxiv_eprints[0].categories[0]")
+
+    def get_title(self, data):
+        return get_value(data, "titles.title[0]")
+
+    def get_url(self, data):
+        return get_value(data, "urls.value[0]")
+
+    def get_author(self, data):
+        if "corporate_author" in data:
+            return " and ".join(
+                "{{{}}}".format(author) for author in data["corporate_author"]
+            )
+
+    def get_number(self, data):
+        return BibTexCommonSchema.get_best_publication_info(data).get("journal_issue")
+
+    def get_address(self, data):
+        conference = ConferenceReader(data)
+        pubinfo_city = conference.city
+        pubinfo_country_code = conference.country
+        if pubinfo_city and pubinfo_country_code:
+            return f"{pubinfo_city}, {pubinfo_country_code}"
+            return pubinfo_city + ", " + pubinfo_country_code
+        return get_value(data, "imprints[0].place")
+
+    def get_type(self, data):
+        doc_type = data.get("doc_type")
+        degree_type = get_value(data, "thesis_info.degree_type", "other")
+        if doc_type == "mastersthesis" and degree_type not in ("master", "diploma"):
+            return "{} thesis".format(degree_type.title())
+
+    def get_report_number(self, data):
+        if "report_numbers" in data:
+            return ", ".join(
+                report["value"] for report in data.get("report_numbers", [])
+            )
+
+    def get_school(self, data):
+        schools = [
+            school["name"] for school in get_value(data, "thesis_info.institutions", [])
+        ]
+        if schools:
+            return ", ".join(schools)
+
+    def get_publisher(self, data):
+        return get_value(data, "imprints.publisher[0]")
+
+    def get_series(self, data):
+        return get_value(data, "book_series.title[0]")
+
+    def get_book_title(self, data):
+        book_series_title = get_value(data, "book_series.title[0]")
+        conference_record_title = get_value(data, "titles.title[0]")
+        return book_series_title or conference_record_title
+
+    def get_volume(self, data):
+        publication_volume = BibTexCommonSchema.get_best_publication_info(data).get(
+            "journal_volume"
+        )
+        bookseries_volume = get_value(data, "book_series.volume[0]")
+        return publication_volume or bookseries_volume
+
+    def get_pages(self, data):
+        return LiteratureReader.get_page_artid_for_publication_info(
+            BibTexCommonSchema.get_best_publication_info(data), "--"
+        )
+
+    def get_edition(self, data):
+        return get_value(data, "editions[0]")
+
+    def get_journal(self, data):
+        return BibTexArticleSchema.get_best_publication_info(data).get("journal_title")
+
+    def get_isbn(self, data):
+        def hyphenate_if_possible(no_hyphens):
+            try:
+                return normalize_isbn(no_hyphens)
+            except ISBNError:
+                return no_hyphens
+
+        isbns = get_value(data, "isbns.value", [])
+        if isbns:
+            return ", ".join(hyphenate_if_possible(isbn) for isbn in isbns)
+
+    @pre_dump
+    def filter_data(self, data):
+        control_number = str(data.get("control_number"))
+        data["doc_type"] = BibTexCommonSchema.get_bibtex_document_type(data)
+        data["texkey"] = get_value(data, "texkeys[0]", default=control_number)
+        return data
+
+
+class BibTexTechReportSchema(BibTexCommonSchema):
+    author = fields.Method("get_author")
+    number = fields.Method("get_number")
+    type_ = fields.Method("get_type", attribute="type")
+
+
+class BibTexPhdThesisSchema(BibTexCommonSchema):
+    address = fields.Method("get_address")
+    author = fields.Method("get_author")
+    reportNumber = fields.Method("get_report_number")
+    school = fields.Method("get_school")
+    type_ = fields.Method("get_type", attribute="type")
+
+
+class BibTexInProceedingsSchema(BibTexCommonSchema):
+    publisher = fields.Method("get_publisher")
+    author = fields.Method("get_author")
+    series = fields.Method("get_series")
+    booktitle = fields.Method("get_book_title")
+    number = fields.Method("get_number")
+    volume = fields.Method("get_volume")
+    reportNumber = fields.Method("get_report_number")
+    address = fields.Method("get_address")
+    pages = fields.Method("get_pages")
+
+
+class BibTexMiscSchema(BibTexCommonSchema):
+    author = fields.Method("get_author")
+    reportNumber = fields.Method("get_report_number")
+
+
+class BibTexMastersThesisSchema(BibTexCommonSchema):
+    author = fields.Method("get_author")
+    reportNumber = fields.Method("get_report_number")
+    school = fields.Method("get_school")
+    type_ = fields.Method("get_type", attribute="type")
+
+
+class BibTexProceedingsSchema(BibTexCommonSchema):
+    address = fields.Method("get_address")
+    number = fields.Method("get_number")
+    pages = fields.Method("get_pages")
+    publisher = fields.Method("get_publlisher")
+    reportNumber = fields.Method("get_report_number")
+    series = fields.Method("get_series")
+    volume = fields.Method("get_volume")
+
+
+class BibTexBookSchema(BibTexCommonSchema):
+    address = fields.Method("get_address")
+    author = fields.Method("get_author")
+    edition = fields.Method("get_edition")
+    isbn = fields.Method("get_isbn")
+    number = fields.Method("get_number")
+    publisher = fields.Method("get_publlisher")
+    reportNumber = fields.Method("get_report_number")
+    series = fields.Method("get_series")
+    volume = fields.Method("get_volume")
+
+
+class BibTexInBookSchema(BibTexCommonSchema):
+    address = fields.Method("get_address")
+    author = fields.Method("get_author")
+    edition = fields.Method("get_edition")
+    number = fields.Method("get_number")
+    pages = fields.Method("get_pages")
+    publisher = fields.Method("get_publlisher")
+    reportNumber = fields.Method("get_report_number")
+    series = fields.Method("get_series")
+    type_ = fields.Method("get_type", attribute="type")
+    volume = fields.Method("get_volume")
+
+
+class BibTexArticleSchema(BibTexCommonSchema):
+    author = fields.Method("get_author")
+    journal = fields.Method("get_journal")
+    number = fields.Method("get_number")
+    volume = fields.Method("get_volume")
+    reportNumber = fields.Method("get_report_number")
+    pages = fields.Method("get_pages")
---
+++ tests/unit/records/marshmallow/literature/test_bibtex.py
@@ -0,0 +1,456 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+import json
+
+import pytest
+from helpers.providers.faker import faker
+from inspirehep.records.marshmallow.literature.bibtex import BibTexCommonSchema
+from inspire_schemas.api import load_schema, validate
+
+
+def test_get_author_by_role():
+    record = {
+        "authors": [
+            {"full_name": "Kiritsis, Elias", "inspire_roles": ["editor"]},
+            {"full_name": "Nitti, Francesco", "inspire_roles": ["author"]},
+            {"full_name": "Pimenta, Leandro Silva"},
+        ]
+    }
+    expected = ["Nitti, Francesco", "Pimenta, Leandro Silva"]
+    result = BibTexCommonSchema.get_authors_with_role(record["authors"], "author")
+    assert expected == result
+
+
+def test_get_editor_by_role():
+    record = {
+        "authors": [
+            {"full_name": "Kiritsis, Elias", "inspire_roles": ["editor"]},
+            {"full_name": "Nitti, Francesco", "inspire_roles": ["author"]},
+        ]
+    }
+    expected = ["Kiritsis, Elias"]
+    result = BibTexCommonSchema.get_authors_with_role(record.get("authors"), "editor")
+    assert expected == result
+
+
+def test_get_collaboration():
+    record = {
+        "document_type": ["article"],
+        "collaborations": [{"value": "CMS"}, {"value": "ATLAS"}],
+    }
+    expected_collaboration = "CMS, ATLAS"
+    schema = BibTexCommonSchema()
+    result = schema.dump(record).data
+
+    result_collaboration = result["collaboration"]
+    assert expected_collaboration == result_collaboration
+
+
+def test_no_collaboration():
+    record = {"document_type": ["article"]}
+    expected_collaboration = ""
+    schema = BibTexCommonSchema()
+    result = schema.dump(record).data
+
+    result_collaboration = result["collaboration"]
+    assert expected_collaboration == result_collaboration
+
+
+def test_bibtex_document_type_misc():
+    record = {"document_type": ["misc"]}
+    expected = "misc"
+    result = BibTexCommonSchema.get_bibtex_document_type(record)
+    assert expected == result
+
+
+def test_bibtex_document_type():
+    record = {"document_type": ["thesis"], "thesis_info": {"degree_type": "master"}}
+    expected = "mastersthesis"
+    result = BibTexCommonSchema.get_bibtex_document_type(record)
+    assert expected == result
+
+
+def test_bibtex_document_type_recognizes_phd_theses():
+    schema = load_schema("hep")
+    document_type_schema = schema["properties"]["document_type"]
+    thesis_info_schema = schema["properties"]["thesis_info"]
+
+    record = {"document_type": ["thesis"], "thesis_info": {"degree_type": "phd"}}
+    assert validate(record["document_type"], document_type_schema) is None
+    assert validate(record["thesis_info"], thesis_info_schema) is None
+
+    expected = "phdthesis"
+    result = BibTexCommonSchema.get_bibtex_document_type(record)
+
+    assert expected == result
+
+
+def test_bibtex_document_type_handles_missing_thesis_info():
+    schema = load_schema("hep")
+    subschema = schema["properties"]["document_type"]
+
+    record = {"document_type": ["thesis"]}
+    assert validate(record["document_type"], subschema) is None
+
+    expected = "mastersthesis"
+    result = BibTexCommonSchema.get_bibtex_document_type(record)
+
+    assert expected == result
+
+
+def test_bibtex_document_type_prefers_article():
+    record = {
+        "thesis_info": {"degree_type": "master"},
+        "document_type": ["thesis", "article"],
+    }
+    expected = "article"
+    result = BibTexCommonSchema.get_bibtex_document_type(record)
+    assert expected == result
+
+
+def test_get_year_from_thesis_when_pubinfo_present():
+    record = {
+        "document_type": ["thesis"],
+        "thesis_info": {"degree_type": "master", "date": "1996-09"},
+    }
+    expected_year = 1996
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_year = result["year"]
+
+    assert expected_year == result_year
+
+
+def test_get_journal():
+    record = {
+        "document_type": ["article"],
+        "publication_info": [{"journal_title": "Rhys.Rev."}],
+    }
+    expected_journal = "Rhys.Rev."
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_journal = result["journal"]
+
+    assert expected_journal == result_journal
+
+
+def test_get_volume():
+    record = {
+        "document_type": ["article"],
+        "publication_info": [{"journal_volume": "12"}],
+    }
+    expected_volume = "12"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["volume"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_report_number():
+    test_data = {
+        "document_type": ["article"],
+        "report_numbers": [
+            {"value": "CERN-SOME-REPORT"},
+            {"value": "CERN-SOME-OTHER-REPORT"},
+        ],
+    }
+    expected_report_numbers = "CERN-SOME-REPORT, CERN-SOME-OTHER-REPORT"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(test_data).data
+    result_report_numbers = result["reportNumber"]
+
+    assert expected_report_numbers == result_report_numbers
+
+
+def test_get_type():
+    test_data = {
+        "document_type": ["thesis"],
+        "thesis_info": {"degree_type": "bachelor"},
+    }
+    expected_type = "Bachelor thesis"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(test_data).data
+    result_type = result["type"]
+    assert expected_type == result_type
+
+
+def test_get_type_handles_missing_thesis_info():
+    schema = load_schema("hep")
+    subschema = schema["properties"]["document_type"]
+
+    record = {"document_type": ["thesis"]}
+    assert validate(record["document_type"], subschema) is None
+
+    expected_type = "Other thesis"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_type = result["type"]
+
+    assert expected_type == result_type
+
+
+def test_get_author():
+    record = {"document_type": ["article"], "corporate_author": ["Corp A", "Corp B"]}
+    expected_author = "{Corp A} and {Corp B}"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_author = result["author"]
+    assert expected_author == result_author
+
+
+def test_note_on_erratum():
+    record = {
+        "document_type": ["article"],
+        "publication_info": [
+            {
+                "journal_title": u"Zażółć gęślą jaźń",
+                "journal_volume": "A",
+                "page_start": "12",
+                "page_end": "15",
+                "year": 2016,
+                "material": "erratum",
+            },
+            {
+                "journal_title": "A Title",
+                "journal_volume": "B",
+                "artid": "987",
+                "material": "addendum",
+            },
+        ],
+    }
+    expected_note = (
+        "[Erratum: Zażółć gęślą jaźń A, 12--15 (2016), Addendum: A Title B, 987]"
+    )
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_note = result["note"]
+    assert expected_note == result_note
+
+
+def test_isbn():
+    record = {"document_type": ["book"], "isbns": [{"value": "9781108705011"}]}
+
+    expected_isbn = "978-1-108-70501-1"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_isbn = result["isbn"]
+    assert expected_isbn == result_isbn
+
+
+def test_isbn_invalid():
+    record = {
+        "document_type": ["book"],
+        "isbns": [{"value": "111-1-11-111111-0"}, {"value": "1111111111111"}],
+    }
+
+    expected_isbn = "111-1-11-111111-0, 1111111111111"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_isbn = result["isbn"]
+    assert expected_isbn == result_isbn
+
+
+def test_eprint():
+    arxiv_value = faker.arxiv()
+    record = {"document_type": ["article"], "arxiv_eprints": [{"value": arxiv_value}]}
+
+    expected_eprint = arxiv_value
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_eprint = result["eprint"]
+    assert expected_eprint == result_eprint
+
+
+def test_archive_prefix():
+    arxiv_value = faker.arxiv()
+    record = {"document_type": ["article"], "arxiv_eprints": [{"value": arxiv_value}]}
+
+    expected_archive_prefix = "arXiv"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_archivePrefix = result["archivePrefix"]
+    assert expected_archive_prefix == result_archivePrefix
+
+
+def test_archive_prefix_empty():
+    record = {"document_type": ["article"]}
+
+    expected_archive_prefix = None
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_archivePrefix = result["archivePrefix"]
+    assert expected_archive_prefix == result_archivePrefix
+
+
+def test_get_number():
+    record = {
+        "document_type": ["article"],
+        "publication_info": [{"journal_issue": "12"}],
+    }
+    expected_number = "12"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_number = result["number"]
+
+    assert expected_number == result_number
+
+
+def test_get_editions():
+    record = {"document_type": ["article"], "editions": ["first"]}
+    expected_number = "first"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_number = result["edition"]
+
+    assert expected_number == result_number
+
+
+def test_get_number_with_differennt_publication_info_material():
+    record = {
+        "document_type": ["article"],
+        "publication_info": [{"material": "foo", "journal_issue": "12"}],
+    }
+    expected_volume = None
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["number"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_page():
+    record = {"document_type": ["article"], "publication_info": [{"artid": "1"}]}
+    expected_volume = "1"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["pages"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_book_title_with_book_series():
+    record = {
+        "titles": [{"title": "This is the main title"}],
+        "document_type": ["book"],
+        "book_series": [{"title": "This is a book title"}],
+    }
+    expected_volume = "This is a book title"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["booktitle"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_book_title_without_book_series():
+    record = {
+        "titles": [{"title": "This is the main title"}],
+        "document_type": ["book"],
+    }
+    expected_volume = "This is the main title"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["booktitle"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_series():
+    record = {
+        "document_type": ["book"],
+        "book_series": [{"title": "This is a book title"}],
+    }
+    expected_volume = "This is a book title"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["series"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_publisher():
+    record = {"document_type": ["book"], "imprints": [{"publisher": "Elsevier"}]}
+    expected_volume = "Elsevier"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["publisher"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_school():
+    record = {
+        "document_type": ["thesis"],
+        "thesis_info": {"institutions": [{"name": "NTUA"}, {"name": "MIT"}]},
+    }
+    expected_volume = "NTUA, MIT"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["school"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_school_empty():
+    record = {"document_type": ["thesis"]}
+    expected_volume = None
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["school"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_address():
+    record = {
+        "document_type": ["book"],
+        "address": [{"cities": ["Tokyo"], "country_code": "JP"}],
+    }
+    expected_volume = "Tokyo, jp"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["address"]
+
+    assert expected_volume == result_volume
+
+
+def test_get_address_with_imprint_place():
+    record = {"document_type": ["book"], "imprints": [{"place": "This is a place"}]}
+    expected_volume = "This is a place"
+    schema = BibTexCommonSchema()
+
+    result = schema.dump(record).data
+    result_volume = result["address"]
+
+    assert expected_volume == result_volume
