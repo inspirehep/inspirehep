5c27b5af7a96a7ba13aae9763ac0fa3466769694
harunurhan <harunurhan17@gmail.com>
2019-02-15 16:31:20 +0100

    ui: change link-color and affiliations' color

--- ui/src/styleVariables.js
+++ ui/src/styleVariables.js
@@ -2,7 +2,9 @@
 
 module.exports = {
   'primary-color': '#1890ff',
+  'link-color': '#0050b3',
   'link-hover-color': '#40a9ff',
   'layout-body-background': '#f0f2f5',
   'success-color': '#52c41a',
+  'secondary-color': 'rgba(0, 0, 0, 0.45)',
 };
