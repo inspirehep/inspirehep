aac3ab022541e30c5ba6f65fd81c228f1b05594c
harunurhan <harunurhan17@gmail.com>
2019-02-25 14:04:11 +0100

    ui: remove query link from keywords

--- ui/src/literature/components/LiteratureKeyword/LiteratureKeyword.jsx
+++ ui/src/literature/components/LiteratureKeyword/LiteratureKeyword.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { Tag } from 'antd';
 import { Map } from 'immutable';
 
-import { LITERATURE } from '../../common/routes';
+import './LiteratureKeyword.scss';
 
 class LiteratureKeyword extends Component {
   render() {
     const { keyword } = this.props;
-    const value = keyword.get('value');
-    // TODO: fix the query
-    const keywordSearchLink = `${LITERATURE}/?q=keyword:"${value}"`;
+    const keywordValue = keyword.get('value');
+
     return (
-      <Tag color="blue" style={{ marginTop: 4 }}>
-        <Link to={keywordSearchLink}>{keyword.get('value')}</Link>
+      <Tag className="__LiteratureKeyword__" color="blue">
+        {keywordValue}
       </Tag>
     );
   }
