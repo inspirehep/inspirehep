ee11df428f8a0043d29102e1e54ee8173a20d9bc
harunurhan <harunurhan17@gmail.com>
2019-02-26 16:01:45 +0100

    ignore superseded records for  citations (ES)

--- inspirehep/search/api.py
+++ inspirehep/search/api.py
@@ -104,11 +104,16 @@ class LiteratureSearch(InspireSearch):
             "publication_info",
         ]
         from_rec = (page - 1) * size
-        search = LiteratureSearch().query(
-            "match", references__recid=record["control_number"]
+        citations_query = Q("match", references__recid=record["control_number"]) & ~Q(
+            "match", related_records__relation="successor"
         )
-        search = search.params(_source=_source, from_=from_rec, size=size)
-        return search.sort("-earliest_date").execute().hits
+        citations_search = (
+            LiteratureSearch()
+            .query(citations_query)
+            .params(_source=_source, from_=from_rec, size=size)
+            .sort("-earliest_date")
+        )
+        return citations_search.execute().hits
 
 
 class AuthorsSearch(InspireSearch):
