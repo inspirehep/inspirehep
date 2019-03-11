8d748542cef335380f4f51bfbec9d7633e08e8e6
Harris Tzovanakis <me@drjova.com>
2019-03-11 17:14:03 +0100

    seraializers: fix bug with ``facet_author_name``
    * CLOSES INSPIR-2101
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- inspirehep/records/marshmallow/authors/base.py
+++ inspirehep/records/marshmallow/authors/base.py
@@ -6,10 +6,12 @@
 # the terms of the MIT License; see LICENSE file for more details.
 
 from inspire_dojson.utils import strip_empty_values
+from inspire_utils.name import ParsedName
+from inspire_utils.record import get_value, get_values_for_schema
 from invenio_records_rest.schemas.json import RecordSchemaJSONV1
 from marshmallow import Schema, fields, post_dump
 
-from .common import FacetAuthorNameSchemaV1, PositionSchemaV1
+from .common import PositionSchemaV1
 
 
 class AuthorsMetadataSchemaV1(Schema):
@@ -37,7 +39,7 @@ class AuthorsMetadataSchemaV1(Schema):
     status = fields.Raw(dump_only=True)
     stub = fields.Raw(dump_only=True)
     urls = fields.Raw(dump_only=True)
-    facet_author_name = fields.Nested(FacetAuthorNameSchemaV1, dump_only=True)
+    facet_author_name = fields.Method("get_facet_author_name", dump_only=True)
 
     @staticmethod
     def get_should_display_positions(data):
@@ -54,6 +56,36 @@ class AuthorsMetadataSchemaV1(Schema):
             )
 
         return True
+
+    def get_facet_author_name(self, data):
+        facet_author_name = data.get("facet_author_name")
+        if facet_author_name is None:
+            return AuthorsMetadataSchemaV1.get_author_with_record_facet_author_name(
+                data
+            )
+        return facet_author_name
+
+    @staticmethod
+    def get_author_with_record_facet_author_name(author):
+        author_ids = author.get("ids", [])
+        author_bai = get_values_for_schema(author_ids, "INSPIRE BAI")
+        bai = author_bai[0] if author_bai else "BAI"
+        author_preferred_name = get_value(author, "name.preferred_name")
+
+        if author_preferred_name:
+            return "{}_{}".format(bai, author_preferred_name)
+        else:
+            return "{}_{}".format(
+                bai,
+                AuthorsMetadataSchemaV1.get_author_display_name(
+                    author["name"]["value"]
+                ),
+            )
+
+    @staticmethod
+    def get_author_display_name(name):
+        parsed_name = ParsedName.loads(name)
+        return " ".join(parsed_name.first_list + parsed_name.last_list)
 
     @post_dump
     def strip_empty(self, data):
