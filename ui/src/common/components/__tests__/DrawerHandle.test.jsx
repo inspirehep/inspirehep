9f9eee96321b278936f212f244ad032228c547fd
harunurhan <harunurhan17@gmail.com>
2019-03-12 10:14:00 +0100

    ui: update unit tests

---
+++ ui/src/common/components/__tests__/DrawerHandle.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import DrawerHandle from '../DrawerHandle';
+
+describe('DrawerHandle', () => {
+  it('renders DrawerHandle with all props', () => {
+    const wrapper = shallow(
+      <DrawerHandle handleText="Handle" drawerTitle="Title" width={256}>
+        <div>Content</div>
+      </DrawerHandle>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders DrawerHandle with default props', () => {
+    const wrapper = shallow(
+      <DrawerHandle drawerTitle="Title">
+        <div>Content</div>
+      </DrawerHandle>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
