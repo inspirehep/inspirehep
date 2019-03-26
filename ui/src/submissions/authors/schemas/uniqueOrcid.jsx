bf851cf1e16d2f598a51a6587ad76e4a526a3a39
harunurhan <harunurhan17@gmail.com>
2019-03-26 11:02:59 +0100

    submissions: add author unique orcid validator
    * Also adds extra step to `submitForm`e2e utility to trigger the form
    validation on blur since it only happens on blur now.
    
    * INSPIR-2155

---
+++ ui/src/submissions/authors/schemas/uniqueOrcid.jsx
@@ -0,0 +1,51 @@
+import { string } from 'yup';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import http from '../../../common/http';
+import { SUBMISSIONS_AUTHOR } from '../../../common/routes';
+
+const ONLY_CONTROL_NUMBER_SERIALIZER_REQUEST_OPTIONS = {
+  headers: {
+    Accept: 'application/vnd+inspire.record.ui+json',
+  },
+};
+
+function fetchAuthorFromOrcid(orcid) {
+  return http.get(
+    `/orcid/${orcid}`,
+    ONLY_CONTROL_NUMBER_SERIALIZER_REQUEST_OPTIONS
+  );
+}
+
+function renderAuthorExistsMessageWithUpdateLink(authorId) {
+  const authorUpdateLink = `${SUBMISSIONS_AUTHOR}/${authorId}`;
+  return (
+    <span>
+      Author with this ORCID already exist, please submit an{' '}
+      <Link to={authorUpdateLink}>update</Link>
+    </span>
+  );
+}
+
+async function isUniqueOrcid(orcid) {
+  if (!orcid) {
+    return true;
+  }
+
+  try {
+    const { data } = await fetchAuthorFromOrcid(orcid);
+    const id = data.metadata.control_number;
+    const message = renderAuthorExistsMessageWithUpdateLink(id);
+    return this.createError({ message });
+  } catch (error) {
+    // TODO: only return `true` if 404, and handle other errors
+    return true;
+  }
+}
+
+export default function uniqueOrcid() {
+  return string()
+    .trim()
+    .test('unique-orcid', null, isUniqueOrcid);
+}
