f42c2bd83b448479ea9366fb63c6b394f9aafc39
Harris Tzovanakis <me@drjova.com>
2019-03-27 14:37:47 +0100

    global: addition of flake8
    * Addition of ``flake8`` for checking the code.
    
    * Addition of ``run-code-checks.sh`` script which
      runs all the code checkers.
    
    * Move the configuration of ``isort`` inside ``setup.cfg``
      so it's consistent whenever we run in the project.
    
    * Added ``random-order`` option by default in the ``pytest.ini``.
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

---
+++ run-code-checks.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+pipenv run isort -rc -c -df  && \
+pipenv run flake8
