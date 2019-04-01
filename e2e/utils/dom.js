ddf4fdfe967f572daf85d4ba4be3e33dfbcc6f35
harunurhan <harunurhan17@gmail.com>
2019-04-01 17:28:50 +0200

    e2e: fix flaky dropdown select
    * INSPIR-2288

--- e2e/utils/dom.js
+++ e2e/utils/dom.js
@@ -2,10 +2,20 @@ const ID_ATTRIBUTE = 'data-test-id';
 const TYPE_ATTRIBUTE = 'data-test-type';
 
 async function selectFromSelectBox(page, selectId, value) {
-  await page.click(`[${ID_ATTRIBUTE}=${selectId}]`);
+  const selectSelector = `[${ID_ATTRIBUTE}=${selectId}]`;
+  await page.click(selectSelector);
+
+  const openSelectSelector = `.ant-select-open>${selectSelector}`;
+  await page.waitFor(openSelectSelector);
+
   await page.click(`[${ID_ATTRIBUTE}=${selectId}-option-${value}]`);
-  await page.click('body');
-  await page.waitFor('.ant-select-dropdown-hidden');
+
+  await page.click('label'); // FIXME: won't work if there is no label on the page
+  await page.waitFor(
+    ctx => !document.querySelector(ctx.openSelectSelector),
+    {},
+    { openSelectSelector }
+  );
 }
 
 module.exports = {
