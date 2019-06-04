a8c74451d579e4365b00c13e9a03c6f0353ea0c9
Harris Tzovanakis <me@drjova.com>
2019-06-04 14:09:18 +0200

    travist-test

--- run-tests.sh
+++ run-tests.sh
@@ -5,8 +5,28 @@
 #
 # inspirehep is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
+set -e
 
-./run-code-checks.sh && \
-poetry run py.test tests/unit && \
-poetry run py.test tests/integration && \
-poetry run py.test tests/integration-async
+if [ "$SUITE" == "all" ]; then
+    ./run-code-checks.sh && \
+    poetry run py.test tests/unit && \
+    poetry run py.test tests/integration && \
+    poetry run py.test tests/integration-async && \
+    cd ui && yarn && yarn test
+fi
+
+if [ "$SUITE" == "lint" ]; then
+    ./run-code-checks.sh
+fi
+if [ "$SUITE" == "unit" ]; then
+    poetry run py.test tests/unit
+fi
+if [ "$SUITE" == "integration" ]; then
+    poetry run py.test tests/integration
+fi
+if [ "$SUITE" == "integration-async" ]; then
+    poetry run py.test tests/integration-async
+fi
+if [ "$SUITE" == "ui" ]; then
+    cd ui && yarn && yarn test
+fi
