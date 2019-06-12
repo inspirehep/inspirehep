036251b37d44728eef6bc5a2110703986a26bfed
Harris Tzovanakis <me@drjova.com>
2019-06-12 13:48:40 +0200

    migrator: fix flaky debug
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- tests/integration/migrator/test_migrator_cli.py
+++ tests/integration/migrator/test_migrator_cli.py
@@ -20,9 +20,13 @@ from inspirehep.migrator.models import LegacyRecordsMirror
 from inspirehep.migrator.tasks import populate_mirror_from_file
 
 
-def test_migrate_file_halts_in_debug_mode(base_app, db, script_info):
+@pytest.fixture(scope="function")
+def enable_debug(base_app):
+    base_app.config["DEBUG"] = True
+
+
+def test_migrate_file_halts_in_debug_mode(base_app, enable_debug, db, script_info):
     cli_runner = CliRunner()
-    current_app.config["DEBUG"] = True
     file_name = pkg_resources.resource_filename(
         __name__, os.path.join("fixtures", "1663923.xml")
     )
@@ -33,9 +37,10 @@ def test_migrate_file_halts_in_debug_mode(base_app, db, script_info):
     assert "DEBUG" in result.output
 
 
-def test_migrate_file_doesnt_halt_in_debug_mode_when_forced(base_app, db, script_info):
+def test_migrate_file_doesnt_halt_in_debug_mode_when_forced(
+    base_app, enable_debug, db, script_info
+):
     cli_runner = CliRunner()
-    current_app.config["DEBUG"] = True
     file_name = pkg_resources.resource_filename(
         __name__, os.path.join("fixtures", "1663923.xml")
     )
@@ -79,27 +84,24 @@ def test_migrate_file_mirror_only(script_info, db, api_client):
     assert response.status_code == 404
 
 
-@pytest.mark.xfail(reason='Flaky test')
-def test_migrate_mirror_halts_in_debug_mode(base_app, db, script_info):
+def test_migrate_mirror_halts_in_debug_mode(base_app, enable_debug, db, script_info):
     cli_runner = CliRunner()
-    config = {"DEBUG": True}
-    with patch.dict(base_app.config, config):
-        result = cli_runner.invoke(migrate, ["mirror", "-a"], obj=script_info)
 
-        assert result.exit_code == 1
-        assert "DEBUG" in result.output
+    result = cli_runner.invoke(migrate, ["mirror", "-a"], obj=script_info)
+
+    assert result.exit_code == 1
+    assert "DEBUG" in result.output
 
 
 def test_migrate_mirror_doesnt_halt_in_debug_mode_when_forced(
-    base_app, db, script_info
+    base_app, enable_debug, db, script_info
 ):
     cli_runner = CliRunner()
-    config = {"DEBUG": True}
-    with patch.dict(base_app.config, config):
-        result = cli_runner.invoke(migrate, ["mirror", "-f"], obj=script_info)
 
-        assert result.exit_code == 0
-        assert "DEBUG" not in result.output
+    result = cli_runner.invoke(migrate, ["mirror", "-f"], obj=script_info)
+
+    assert result.exit_code == 0
+    assert "DEBUG" not in result.output
 
 
 def test_migrate_mirror_migrates_pending(base_app, db, script_info, api_client):
