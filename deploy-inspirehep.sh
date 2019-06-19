936cfca0c5acede54b1e49b0efecc4aa38ffff9f
Harris Tzovanakis <me@drjova.com>
2019-06-19 11:21:08 +0200

    release

--- deploy-inspirehep.sh
+++ deploy-inspirehep.sh
@@ -6,25 +6,23 @@
 # inspirehep is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-VERSION=${1:-"latest"}
-echo "Deploying $VERSION version."
-
-if [ "$VERSION" != "latest" ]; then
-        curl -X POST "${INSPIREHEP_DEPLOY_URL}" \
-        -F token=${INSPIREHEP_DEPLOY_TOKEN} \
-        -F ref=master \
-        -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
-        -F "variables[TAG_NAME]=${VERSION}" \
-        -F "variables[APPLICATION_IMAGE_NAME]=inspirehepimage" \
-        -F "variables[VERSION]=${VERSION}" \
-        -F "variables[DEPLOY]=qa"
-else
+if [[ -v TRAVIS_TAG ]]; then
+    echo "Deploying tag ${TRAVIS_TAG}"
     curl -X POST "${INSPIREHEP_DEPLOY_URL}" \
         -F token=${INSPIREHEP_DEPLOY_TOKEN} \
         -F ref=master \
         -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
-        -F "variables[BRANCH_NAME]=master" \
+        -F "variables[TAG_NAME]=${TRAVIS_TAG}" \
         -F "variables[APPLICATION_IMAGE_NAME]=inspirehepimage" \
-        -F "variables[VERSION]=latest" \
+        -F "variables[VERSION]=${TRAVIS_TAG}" \
         -F "variables[DEPLOY]=qa"
 fi
+
+curl -X POST "${INSPIREHEP_DEPLOY_URL}" \
+    -F token=${INSPIREHEP_DEPLOY_TOKEN} \
+    -F ref=master \
+    -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
+    -F "variables[BRANCH_NAME]=master" \
+    -F "variables[APPLICATION_IMAGE_NAME]=inspirehepimage" \
+    -F "variables[VERSION]=latest" \
+    -F "variables[DEPLOY]=qa"
--- deploy-ui.sh
+++ deploy-ui.sh
@@ -6,22 +6,21 @@
 # inspirehep is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-VERSION=${1:-"latest"}
-echo "Deploying $VERSION version."
-
-if [ "$VERSION" != "latest" ]; then
+if [[ -v TRAVIS_TAG ]]; then
+    echo "Deploying tag ${TRAVIS_TAG}"
     curl -X POST "${UI_DEPLOY_URL}" \
         -F token=${UI_DEPLOY_TOKEN} \
         -F ref=qa -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
-        -F "variables[TAG]=${VERSION}" \
-        -F "variables[IMAGE_BUILD]=True" \
-        -F "variables[DEPLOY]=True"
-else
-    curl -X POST "${UI_DEPLOY_URL}" \
-        -F token=${UI_DEPLOY_TOKEN} \
-        -F ref=qa \
-        -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
-        -F "variables[TAG]=latest" \
+        -F "variables[TAG]=${TRAVIS_TAG}" \
         -F "variables[IMAGE_BUILD]=True" \
         -F "variables[DEPLOY]=True"
 fi
+
+curl -X POST "${UI_DEPLOY_URL}" \
+    -F token=${UI_DEPLOY_TOKEN} \
+    -F ref=qa \
+    -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
+    -F "variables[TAG]=latest" \
+    -F "variables[IMAGE_BUILD]=True" \
+    -F "variables[DEPLOY]=True"
+
