e85570070c898f56edcde6e326230c0e8adfc520
Harris Tzovanakis <me@drjova.com>
2019-06-20 16:55:44 +0200

    travis: build images with ``git describe`` as version
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

---
+++ build-docker-images.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+GIT_DESC="$(git describe --always || echo)"
+
+echo "Deploying tag for inspirehep-ui ${GIT_DESC}"
+curl -X POST "${UI_DEPLOY_URL}" \
+    -F token=${UI_DEPLOY_TOKEN} \
+    -F ref=qa -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
+    -F "variables[TAG]=${GIT_DESC}" \
+    -F "variables[IMAGE_BUILD]=True" \
+    -F "variables[DEPLOY]=True"
+
+echo "Deploying tag for inspirehep: ${GIT_DESC}"
+curl -X POST "${INSPIREHEP_DEPLOY_URL}" \
+    -F token=${INSPIREHEP_DEPLOY_TOKEN} \
+    -F ref=master \
+    -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
+    -F "variables[BRANCH_NAME]=master" \
+    -F "variables[APPLICATION_IMAGE_NAME]=inspirehepimage" \
+    -F "variables[VERSION]=${GIT_DESC}" \
+    -F "variables[DEPLOY]=qa"
