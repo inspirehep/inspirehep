308330b60694a8b9f36c3c8d376ac7d9f2df3e56
Mona Al Owe <awi_moni@yahoo.com>
2019-06-26 11:00:49 +0200

    e2e: Add e2e tests for job submission
    * Added e2e tests for job submission and update
    * INSPIR-2493

--- e2e/utils/routes.js
+++ e2e/utils/routes.js
@@ -7,6 +7,9 @@ const AUTHOR_SUBMISSION = `${BASE_URL}/submissions/authors`;
 const AUTHOR_SUBMISSION_API = `${BASE_URL}/api/submissions/authors`;
 const LITERATURE_SUBMISSION = `${BASE_URL}/submissions/literature`;
 const LITERATURE_SUBMISSION_API = `${BASE_URL}/api/submissions/literature`;
+const JOB_SUBMISSION = `${BASE_URL}/submissions/jobs`;
+const JOB_SUBMISSION_API = `${BASE_URL}/api/submissions/jobs`;
+const JOB_API = `${BASE_URL}/api/jobs`;
 const SUBMISSIONS_SUCCESS = `${BASE_URL}/submissions/success`;
 
 const HOLDINGPEN_API = `${BASE_URL}/api/holdingpen`;
@@ -20,4 +23,7 @@ module.exports = {
   LITERATURE_SUBMISSION_API,
   SUBMISSIONS_SUCCESS,
   HOLDINGPEN_API,
+  JOB_SUBMISSION,
+  JOB_SUBMISSION_API,
+  JOB_API,
 };
--- ui/src/submissions/common/components/RichTextEditor/RichTextEditor.jsx
+++ ui/src/submissions/common/components/RichTextEditor/RichTextEditor.jsx
@@ -17,18 +17,23 @@ const QUILL_FORMATS = ['bold', 'italic', 'underline', 'list', 'bullet', 'link'];
 
 class RichTextEditor extends Component {
   render() {
+    const { ...autoCompleteProps } = this.props;
+    const dataTestType = autoCompleteProps['data-test-type'];
+    const dataTestId = autoCompleteProps['data-test-id'];
     return (
-      <QuillEditor
-        className="ant-input __RichTextEditor__"
-        theme="snow"
-        modules={QUILL_MODULES}
-        formats={QUILL_FORMATS}
-        {...this.props}
-      />
+      <div data-test-type={dataTestType} data-test-id={dataTestId}>
+        <QuillEditor
+          className="ant-input __RichTextEditor__"
+          theme="snow"
+          modules={QUILL_MODULES}
+          formats={QUILL_FORMATS}
+          {...this.props}
+        />
+      </div>
     );
   }
 }
 
-RichTextEditor.propTypes = QuillEditor.propTypes
+RichTextEditor.propTypes = QuillEditor.propTypes;
 
 export default RichTextEditor;
