d2ff0da65959e0019b5b09909e632d1aaaa0eb53
Harris Tzovanakis <me@drjova.com>
2019-06-27 15:05:50 +0200

    tests: fix the rest migration flaky tests
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- tests/integration/migrator/test_migrator_cli.py
+++ tests/integration/migrator/test_migrator_cli.py
@@ -22,7 +22,7 @@ from inspirehep.migrator.tasks import populate_mirror_from_file
 @pytest.mark.xfail(
     reason="Flaky test, the current_app configuration is not overwritten properly."
 )
-def test_migrate_file_halts_in_debug_mode(app_cli_runner, db):
+def test_migrate_file_halts_in_debug_mode(base_app, db, api_client, app_cli_runner):
     config = {"DEBUG": True}
     with patch.dict(current_app.config, config):
         file_name = pkg_resources.resource_filename(
@@ -35,7 +35,9 @@ def test_migrate_file_halts_in_debug_mode(app_cli_runner, db):
         assert "DEBUG" in result.output
 
 
-def test_migrate_file_doesnt_halt_in_debug_mode_when_forced(app_cli_runner, db):
+def test_migrate_file_doesnt_halt_in_debug_mode_when_forced(
+    base_app, db, api_client, app_cli_runner
+):
     config = {"DEBUG": True}
     with patch.dict(current_app.config, config):
         file_name = pkg_resources.resource_filename(
@@ -48,7 +50,7 @@ def test_migrate_file_doesnt_halt_in_debug_mode_when_forced(app_cli_runner, db):
         assert "DEBUG" not in result.output
 
 
-def test_migrate_file(app_cli_runner, db, api_client):
+def test_migrate_file(base_app, db, api_client, app_cli_runner):
     file_name = pkg_resources.resource_filename(
         __name__, os.path.join("fixtures", "1663923.xml")
     )
@@ -61,7 +63,7 @@ def test_migrate_file(app_cli_runner, db, api_client):
     assert json.loads(response.data)["metadata"]["control_number"] == 1663923
 
 
-def test_migrate_file_mirror_only(app_cli_runner, db, api_client):
+def test_migrate_file_mirror_only(base_app, db, api_client, app_cli_runner):
     file_name = pkg_resources.resource_filename(
         __name__, os.path.join("fixtures", "1663924.xml")
     )
@@ -76,7 +78,7 @@ def test_migrate_file_mirror_only(app_cli_runner, db, api_client):
 
 
 @pytest.mark.xfail(reason="Flaky test, the configuration is not overwritten properly.")
-def test_migrate_mirror_halts_in_debug_mode(app_cli_runner, db):
+def test_migrate_mirror_halts_in_debug_mode(base_app, db, api_client, app_cli_runner):
     config = {"DEBUG": True}
     with patch.dict(current_app.config, config):
         result = app_cli_runner.invoke(migrate, ["mirror", "-a"])
@@ -85,7 +87,9 @@ def test_migrate_mirror_halts_in_debug_mode(app_cli_runner, db):
         assert "DEBUG" in result.output
 
 
-def test_migrate_mirror_doesnt_halt_in_debug_mode_when_forced(app_cli_runner, db):
+def test_migrate_mirror_doesnt_halt_in_debug_mode_when_forced(
+    base_app, db, api_client, app_cli_runner
+):
     config = {"DEBUG": True}
     with patch.dict(current_app.config, config):
         result = app_cli_runner.invoke(migrate, ["mirror", "-f"])
@@ -94,7 +98,7 @@ def test_migrate_mirror_doesnt_halt_in_debug_mode_when_forced(app_cli_runner, db
         assert "DEBUG" not in result.output
 
 
-def test_migrate_mirror_migrates_pending(app_cli_runner, db, api_client):
+def test_migrate_mirror_migrates_pending(base_app, db, api_client, app_cli_runner):
     file_name = pkg_resources.resource_filename(
         __name__, os.path.join("fixtures", "1663924.xml")
     )
@@ -108,7 +112,9 @@ def test_migrate_mirror_migrates_pending(app_cli_runner, db, api_client):
     assert json.loads(response.data)["metadata"]["control_number"] == 1663924
 
 
-def test_migrate_mirror_broken_migrates_invalid(app_cli_runner, db, api_client):
+def test_migrate_mirror_broken_migrates_invalid(
+    base_app, db, api_client, app_cli_runner
+):
 
     file_name = pkg_resources.resource_filename(
         __name__, os.path.join("fixtures", "1663927_broken.xml")
