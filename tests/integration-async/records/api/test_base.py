6ee4d66ee887f57460ac6553d6930e6933cb3561
Harris Tzovanakis <me@drjova.com>
2019-06-28 13:24:59 +0200

    records: fix record versioning
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

---
+++ tests/integration-async/records/api/test_base.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+import pytest
+from invenio_db import db
+
+from inspirehep.records.api import LiteratureRecord
+
+
+def test_record_versioning(app, clear_environment):
+    data = {
+        "$schema": "http://localhost:5000/schemas/records/hep.json",
+        "titles": [{"title": "Test a valid record"}],
+        "document_type": ["article"],
+        "_collections": ["Literature"],
+    }
+
+    expected_version_created = 1
+    expected_count_created = 1
+    record = LiteratureRecord.create(data)
+    record_control_number = record["control_number"]
+    db.session.commit()
+
+    assert expected_version_created == record.model.version_id
+    assert expected_count_created == record.model.versions.count()
+    with pytest.raises(AttributeError):
+        record._previous_version
+
+    expected_version_updated = 2
+    expected_count_updated = 2
+    record_updated = LiteratureRecord.get_record_by_pid_value(record_control_number)
+    record_updated.update(dict(record_updated))
+    db.session.commit()
+
+    assert expected_version_updated == record_updated.model.version_id
+    assert expected_count_updated == record_updated.model.versions.count()
+    assert record._previous_version
