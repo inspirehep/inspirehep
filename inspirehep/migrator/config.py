a3dec22f19aa19ce8d2bd99f6b9d0cb46fa9820c
Harris Tzovanakis <me@drjova.com>
2019-07-02 13:56:08 +0200

    config: move migrator config to each own module
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

---
+++ inspirehep/migrator/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+# Migration
+# =========
+#: Special redis for continuous migration and ORCID token migration
+MIGRATION_REDIS_URL = None
