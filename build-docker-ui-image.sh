e19c16145b541ab9fd540703588840b90f751c1c
Harris Tzovanakis <me@drjova.com>
2019-07-06 09:29:33 +0200

    travis: build image from master or prod branch

--- build-docker-ui-image.sh
+++ build-docker-ui-image.sh
@@ -7,11 +7,17 @@
 # the terms of the MIT License; see LICENSE file for more details.
 
 GIT_DESC="$(git describe --always || echo)"
+# FIXME: We need that for building the ui images with the proper
+# SENTRY and PIWIK
+[[ "$TRAVIS_BRANCH" == "master" ]] && REF="qa" || REF="master"
 
 echo "Deploying tag for inspirehep-ui ${GIT_DESC}"
+echo "Deploying from branch ${REF}"
+
 curl -X POST "${UI_DEPLOY_URL}" \
     -F token=${UI_DEPLOY_TOKEN} \
-    -F ref=qa -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
+    -F ref=${REF} \
+    -F "variables[CACHE_DATE]=$(date +%Y-%m-%d:%H:%M:%S)" \
     -F "variables[TAG]=${GIT_DESC}" \
     -F "variables[IMAGE_BUILD]=True" \
     -F "variables[DEPLOY]=True"
