733b8eb97b630f7487ececbec25ad1cecc773cad
harunurhan <harunurhan17@gmail.com>
2019-07-09 13:20:30 +0200

    ui: fix citations summ table query link bug
    * INSPIR-2447

--- ui/src/common/constants.js
+++ ui/src/common/constants.js
@@ -1,5 +1,5 @@
 export const PUBLISHED_QUERY = { citeable: true, refereed: true };
-export const CITEABLE_QUERY = { citeable: true };
+export const CITEABLE_QUERY = { citeable: true, refereed: undefined };
 export const PUBLISHED_BAR_TYPE = 'published';
 export const CITEABLE_BAR_TYPE = 'citeable';
 
