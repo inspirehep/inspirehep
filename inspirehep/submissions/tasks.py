inspirehep/inspirehep

commit 5e1bb8642ca992b244136207e70027eca1cde347

submissions: skip ticket for job update if curator
* Does not create ticket when a job updated by a curator

inspirehep/submissions/views.py
@@ -189,7 +189,6 @@ class JobSubmissionsResource(BaseSubmissionsResource):
         data = self.prepare_data(data)
         record = JobsRecord.create(data)
         db.session.commit()
-        self.create_ticket(record, "rt/new_job.html")
         return jsonify({"pid_value": record["control_number"]}), 201
 
     def put(self, pid_value):
@@ -210,7 +209,10 @@ class JobSubmissionsResource(BaseSubmissionsResource):
         data = self.prepare_data(data, record)
         record.update(data)
         db.session.commit()
-        self.create_ticket(record, "rt/update_job.html")
+
+        if not is_superuser_or_cataloger_logged_in():
+            self.create_ticket(record, "rt/update_job.html")
+
         return jsonify({"pid_value": record["control_number"]})
 
     def prepare_new_record(self, data):

tests/integration/submissions/test_submissions_views.py
@@ -718,25 +718,32 @@ def test_new_job_submit_with_wrong_status_value(
 
 
 @patch("inspirehep.submissions.views.create_ticket_with_template")
-def test_update_job(ticket_mock, app, api_client, create_user):
+def test_update_job(create_ticket_mock, app, api_client, create_user):
     user = create_user()
     login_user_via_session(api_client, email=user.email)
+
     data = {**DEFAULT_EXAMPLE_JOB_DATA}
     response = api_client.post(
         "/submissions/jobs",
         content_type="application/json",
         data=json.dumps({"data": data}),
     )
+
     assert response.status_code == 201
+
+    create_ticket_mock.reset_mock()
+
     pid_value = response.json["pid_value"]
     record_url = url_for(".job_submission_view", pid_value=pid_value)
     data["title"] = "New test title"
     response2 = api_client.put(
         record_url, content_type="application/json", data=json.dumps({"data": data})
     )
+
     assert response2.status_code == 200
     record = api_client.get(record_url).json["data"]
     assert record["title"] == "New test title"
+    create_ticket_mock.assert_called_once()
 
 
 @patch("inspirehep.submissions.views.create_ticket_with_template")
@@ -745,19 +752,23 @@ def test_update_job_status_from_pending_not_curator(
 ):
     user = create_user()
     login_user_via_session(api_client, email=user.email)
+
     data = {**DEFAULT_EXAMPLE_JOB_DATA}
     response = api_client.post(
         "/submissions/jobs",
         content_type="application/json",
         data=json.dumps({"data": data}),
     )
+
     assert response.status_code == 201
+
     pid_value = response.json["pid_value"]
     record_url = url_for(".job_submission_view", pid_value=pid_value)
     data["status"] = "open"
     response2 = api_client.put(
         record_url, content_type="application/json", data=json.dumps({"data": data})
     )
+
     assert response2.status_code == 400
     record = api_client.get(record_url).json["data"]
     assert record["status"] == "pending"
@@ -765,29 +776,37 @@ def test_update_job_status_from_pending_not_curator(
 
 @patch("inspirehep.submissions.views.create_ticket_with_template")
 def test_update_job_status_from_pending_curator(
-    ticket_mock, app, api_client, create_user
+    create_ticket_mock, app, api_client, create_user
 ):
     user = create_user()
-    curator = create_user(role="cataloger")
     login_user_via_session(api_client, email=user.email)
+
     data = {**DEFAULT_EXAMPLE_JOB_DATA}
     response = api_client.post(
         "/submissions/jobs",
         content_type="application/json",
         data=json.dumps({"data": data}),
     )
+
     assert response.status_code == 201
+
+    create_ticket_mock.reset_mock()
+
     pid_value = response.json["pid_value"]
     record_url = url_for(".job_submission_view", pid_value=pid_value)
 
+    curator = create_user(role="cataloger")
     login_user_via_session(api_client, email=curator.email)
+
     data["status"] = "open"
     response2 = api_client.put(
         record_url, content_type="application/json", data=json.dumps({"data": data})
     )
+
     assert response2.status_code == 200
     record = api_client.get(record_url).json["data"]
     assert record["status"] == "open"
+    create_ticket_mock.assert_not_called()
 
 
 @patch("inspirehep.submissions.views.create_ticket_with_template")

commit b67bd9ee813f6a4883894e71d2a45d6a781c0be6

jobs: create ticket on new job submission
* Broken by 5e1bb8642ca992b244136207e70027eca1cde347

* Also makes ticket creation call async

inspirehep/submissions/tasks.py
@@ -14,7 +14,7 @@ from inspirehep.rt.tickets import CreateTicketException, create_ticket_with_temp
 logger = logging.getLogger(__name__)
 
 
-@shared_task(ingore_result=False, max_retries=5)
+@shared_task(ignore_result=False, max_retries=5)
 def async_create_ticket_with_template(
     queue, requestor, template_path, template_context, title, recid=None
 ):

inspirehep/submissions/views.py
@@ -189,6 +189,7 @@ class JobSubmissionsResource(BaseSubmissionsResource):
         data = self.prepare_data(data)
         record = JobsRecord.create(data)
         db.session.commit()
+        self.create_ticket(record, "rt/new_job.html")
         return jsonify({"pid_value": record["control_number"]}), 201
 
     def put(self, pid_value):
@@ -314,7 +315,7 @@ class JobSubmissionsResource(BaseSubmissionsResource):
             "job_url_edit": JOB_EDIT,
             "hep_url": INSPIREHEP_URL,
         }
-        async_create_ticket_with_template(
+        async_create_ticket_with_template.delay(
             rt_queue,
             requestor,
             rt_template,

tests/integration/submissions/test_submissions_views.py
@@ -674,7 +674,7 @@ def test_job_get_requires_authentication(ticket_mock, api_client):
 
 
 @patch("inspirehep.submissions.views.async_create_ticket_with_template")
-def test_new_job_submit_by_user(ticket_mock, app, api_client, create_user):
+def test_new_job_submit_by_user(create_ticket_mock, app, api_client, create_user):
     user = create_user()
     login_user_via_session(api_client, email=user.email)
     response = api_client.post(
@@ -684,10 +684,11 @@ def test_new_job_submit_by_user(ticket_mock, app, api_client, create_user):
     )
     assert response.status_code == 201
 
-    job_id = json.loads(response.data)['pid_value']
+    job_id = json.loads(response.data)["pid_value"]
     job_data = JobsRecord.get_record_by_pid_value(job_id)
 
-    assert job_data['status'] == 'pending'
+    assert job_data["status"] == "pending"
+    create_ticket_mock.delay.assert_called_once()
 
 
 @patch("inspirehep.submissions.views.async_create_ticket_with_template")
@@ -703,10 +704,10 @@ def test_new_job_submit_by_cataloger(ticket_mock, app, api_client, create_user):
     )
     assert response.status_code == 201
 
-    job_id = json.loads(response.data)['pid_value']
+    job_id = json.loads(response.data)["pid_value"]
     job_data = JobsRecord.get_record_by_pid_value(job_id)
 
-    assert job_data['status'] == 'open'
+    assert job_data["status"] == "open"
 
 
 @patch("inspirehep.submissions.views.async_create_ticket_with_template")
@@ -769,7 +770,7 @@ def test_update_job(create_ticket_mock, app, api_client, create_user):
     assert response2.status_code == 200
     record = api_client.get(record_url).json["data"]
     assert record["title"] == "New test title"
-    create_ticket_mock.assert_called_once()
+    create_ticket_mock.delay.assert_called_once()
 
 
 @patch("inspirehep.submissions.views.async_create_ticket_with_template")
@@ -832,7 +833,7 @@ def test_update_job_status_from_pending_curator(
     assert response2.status_code == 200
     record = api_client.get(record_url).json["data"]
     assert record["status"] == "open"
-    create_ticket_mock.assert_not_called()
+    create_ticket_mock.delay.assert_not_called()
 
 
 @patch("inspirehep.submissions.views.async_create_ticket_with_template")