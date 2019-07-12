288bb1ba29869fa95904e6c16b989dfb92da0714
Mona Al Owe <awi_moni@yahoo.com>
2019-07-12 13:14:00 +0200

    citations: reindex references of records when earliest_date changes
    * Reindexes all references for records when earliest_date of the record changes. This is needed to keep the citations_by_year of the references up-to-date.

--- tests/integration-async/records/api/test_base.py
+++ tests/integration-async/records/api/test_base.py
@@ -39,3 +39,81 @@ def test_record_versioning(app, clear_environment):
     assert expected_version_updated == record_updated.model.version_id
     assert expected_count_updated == record_updated.model.versions.count()
     assert record._previous_version
+
+
+def test_get_modified_references_returns_all_references_when_earliest_date_changed(
+    app, clear_environment
+):
+    cited_data = {
+        "$schema": "http://localhost:5000/schemas/records/hep.json",
+        "titles": [{"title": "Test a valid record"}],
+        "document_type": ["article"],
+        "_collections": ["Literature"],
+    }
+    cited_record = LiteratureRecord.create(cited_data)
+
+    citing_data = {
+        "$schema": "http://localhost:5000/schemas/records/hep.json",
+        "titles": [{"title": "My Title"}],
+        "document_type": ["article"],
+        "_collections": ["Literature"],
+        "preprint_date": "2019-06-28",
+        "references": [
+            {
+                "record": {
+                    "$ref": f"http://localhost:5000/api/literature/{cited_record['control_number']}"
+                }
+            }
+        ],
+    }
+
+    citing_record = LiteratureRecord.create(citing_data)
+    db.session.commit()
+
+    assert citing_record.get_modified_references() == [cited_record.id]
+
+    data_update = {"preprint_date": "2018-06-28"}
+    citing_data.update(data_update)
+    citing_record.update(citing_data)
+    db.session.commit()
+
+    assert citing_record.get_modified_references() == [cited_record.id]
+
+
+def test_get_modified_references_returns_no_references_when_non_impacting_metadata_changed(
+    app, clear_environment
+):
+    cited_data = {
+        "$schema": "http://localhost:5000/schemas/records/hep.json",
+        "titles": [{"title": "Test a valid record"}],
+        "document_type": ["article"],
+        "_collections": ["Literature"],
+    }
+    cited_record = LiteratureRecord.create(cited_data)
+
+    citing_data = {
+        "$schema": "http://localhost:5000/schemas/records/hep.json",
+        "titles": [{"title": "My title"}],
+        "document_type": ["article"],
+        "_collections": ["Literature"],
+        "preprint_date": "2019-06-28",
+        "references": [
+            {
+                "record": {
+                    "$ref": f"http://localhost:5000/api/literature/{cited_record['control_number']}"
+                }
+            }
+        ],
+    }
+
+    citing_record = LiteratureRecord.create(citing_data)
+    db.session.commit()
+
+    assert citing_record.get_modified_references() == [cited_record.id]
+
+    data_update = {"titles": [{"title": "updated title"}]}
+    citing_data.update(data_update)
+    citing_record.update(citing_data)
+    db.session.commit()
+
+    assert citing_record.get_modified_references() == []
