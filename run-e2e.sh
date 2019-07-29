eb40de32a5a4cef3c852343409dcd2dc49b4e0e3
Harris Tzovanakis <me@drjova.com>
2019-07-29 14:07:11 +0200

    inspirehep: fix the image

--- run-e2e.sh
+++ run-e2e.sh
@@ -3,6 +3,6 @@ rc=0
 docker-compose -f ./docker-compose.core.yml up -d --force-recreate
 docker-compose -f ./docker-compose.core.yml exec web ./scripts/setup
 docker-compose -f ./docker-compose.core.yml exec web-next inspirehep db create
-docker-compose -f ./docker-compose.core.yml exec web poetry run inspirehep importer records -d data/records/authors
+docker-compose -f ./docker-compose.core.yml exec web inspirehep importer records -d data/records/authors
 docker-compose -f ./docker-compose.core.yml -f ./e2e/docker-compose.e2e.yml run --rm e2e bash -c "cd /opt/e2e && yarn && yarn test" || rc=$?
 exit $rc
