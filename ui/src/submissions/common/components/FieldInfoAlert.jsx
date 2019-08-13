280d653a94d8763f49b0c8f42b074ed0eebde445
harunurhan <harunurhan17@gmail.com>
2019-08-13 10:19:21 +0200

    ui: add deadline info for postdoc submissions
    * INSPIR-2621

---
+++ ui/src/submissions/common/components/FieldInfoAlert.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Alert, Row, Col } from 'antd';
+import PropTypes from 'prop-types';
+import { WRAPPER_COL, LABEL_COL } from '../withFormItem';
+
+class FieldInfoAlert extends Component {
+  render() {
+    const { description } = this.props;
+    return (
+      <Row>
+        <Col span={WRAPPER_COL.span} offset={LABEL_COL.span}>
+          <Alert type="info" description={description} showIcon />
+        </Col>
+      </Row>
+    );
+  }
+}
+
+FieldInfoAlert.propTypes = {
+  description: PropTypes.node.isRequired,
+};
+
+export default FieldInfoAlert;
