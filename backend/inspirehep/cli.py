e306e0da7ed7d98576e61672ad5f3b15fed7d530
Harris Tzovanakis <me@drjova.com>
2019-08-14 16:07:11 +0200

    cli: cleanup
    * Remove ``reindex`` command from the main ``cli``, it
      is already registered in ``indexer`` cli.
    
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- backend/inspirehep/cli.py
+++ backend/inspirehep/cli.py
@@ -9,16 +9,14 @@
 
 from invenio_base.app import create_cli
 
-from inspirehep.cli.fixtures import fixtures
 from inspirehep.factory import create_app
+from inspirehep.fixtures import fixtures
 from inspirehep.orcid.cli import orcid
 from inspirehep.records.cli import citations, importer
-from inspirehep.records.indexer.cli import reindex_records
 
 cli = create_cli(create_app=create_app)
 
 cli.add_command(fixtures)
-cli.add_command(reindex_records)
 cli.add_command(importer)
 cli.add_command(citations)
 cli.add_command(orcid)
