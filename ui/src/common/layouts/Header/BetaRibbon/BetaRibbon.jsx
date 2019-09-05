312a5d43d31f7e918ac775fe91575dc0348e5768
harunurhan <harunurhan17@gmail.com>
2019-09-05 14:46:07 +0200

    ui: add beta ribbon everywhere

---
+++ ui/src/common/layouts/Header/BetaRibbon/BetaRibbon.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './BetaRibbon.scss';
+
+class BetaRibbon extends Component {
+  render() {
+    return (
+      <div className="__BetaRibbon__">
+        Beta
+      </div>
+    );
+  }
+}
+
+export default BetaRibbon;
