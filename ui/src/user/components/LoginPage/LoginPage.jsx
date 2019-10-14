ba81978515f9b3b87e41d5b9447c9e29a87f421a
Miguel Garcia <grcmiguel@outlook.com>
2019-10-14 11:51:40 +0200

    ui: change text in login
    * INSPIR-2798

--- ui/src/user/components/LoginPage/LoginPage.jsx
+++ ui/src/user/components/LoginPage/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+
 import PropTypes from 'prop-types';
 import { Row, Card, Button } from 'antd';
 
 import orcidLogo from '../../../common/orcid.svg';
 import './LoginPage.scss';
+import ExternalLink from '../../../common/components/ExternalLink';
 
 class LoginPage extends Component {
   render() {
@@ -16,19 +18,26 @@ class LoginPage extends Component {
         align="middle"
       >
         <Card align="middle">
-          <p className="f4">Please sign in to INSPIRE</p>
+          <p className="f4">
+            Please sign in to <strong>iN</strong>SPIRE
+          </p>
+          <p className="pb2">
+            To suggest content to INSPIRE, an ORCID is required. Registration is
+            free, quick, and open to all! Sign up at{' '}
+            <ExternalLink href="https://orcid.org/register!">
+              https://orcid.org/register
+            </ExternalLink>
+          </p>
           <Button className="login-button h3" onClick={onLoginClick}>
             <img className="logo mr2" src={orcidLogo} alt="ORCID" />
             <strong>Login with ORCID</strong>
           </Button>
-          <a
-            className="db pt3"
+          <ExternalLink
             href="https://labs.inspirehep.net/help/knowledge-base/what-is-orcid/"
-            target="_blank"
-            rel="noreferrer noopener"
+            className="db pt3"
           >
             What is ORCID?
-          </a>
+          </ExternalLink>
         </Card>
       </Row>
     );
