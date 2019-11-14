68cf24659f534f4c7100972fac6541452964bf68
harunurhan <harunurhan17@gmail.com>
2019-11-14 15:10:59 +0100

    ui: select correct document_type after import
    * Selects correct document type on literature submission after import,
    to display correct form ui (article, book, book chapter etc.)
    
    INSPIR-2917

--- ui/src/submissions/literature/containers/LiteratureSubmissionPageContainer.jsx
+++ ui/src/submissions/literature/containers/LiteratureSubmissionPageContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Map } from 'immutable';
@@ -31,100 +31,83 @@ const DOC_TYPE_OPTIONS = [
   },
 ];
 
-class LiteratureSubmissionPage extends Component {
-  constructor(props) {
-    super(props);
+function LiteratureSubmissionPage({ error, importedFormData, onSubmit }) {
+  const [docType, setDocType] = useState(DOC_TYPE_OPTIONS[0].value);
+  const [isDataImportSkipped, setDataImportSkipped] = useState(false);
+  const shouldDisplayForm = isDataImportSkipped || importedFormData != null;
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onDocTypeChange = this.onDocTypeChange.bind(this);
-    this.onDataImportSkipClick = this.onDataImportSkipClick.bind(this);
+  const onDataImportSkipClick = useCallback(() => {
+    setDataImportSkipped(true);
+  }, []);
+  const onDocTypeChange = useCallback(newDocType => {
+    setDocType(newDocType);
+  }, []);
 
-    this.state = {
-      docType: DOC_TYPE_OPTIONS[0].value,
-      hasDataImportSkipped: false,
-    };
-  }
-
-  async onSubmit(formData) {
-    const { dispatch } = this.props;
-    await dispatch(submit(LITERATURE_PID_TYPE, formData));
-  }
-
-  onDocTypeChange(docType) {
-    this.setState({ docType });
-  }
-
-  onDataImportSkipClick() {
-    this.setState({ hasDataImportSkipped: true });
-  }
-
-  shouldDisplayForm() {
-    const { hasDataImportSkipped } = this.state;
-    const { importedFormData } = this.props;
-    return hasDataImportSkipped || importedFormData != null;
-  }
-
-  render() {
-    const { error, importedFormData } = this.props;
-    const { docType } = this.state;
-
-    return (
-      <Row type="flex" justify="center">
-        <Col className="mt3 mb3" xs={24} md={21} lg={16} xl={15} xxl={14}>
-          <Row className="mb3 pa3 bg-white">
-            <h3>Suggest content</h3>
-            This form allows you to suggest a preprint, an article, a book, a
-            conference proceeding or a thesis you would like to see added to
-            INSPIRE. We will check your suggestion with our{' '}
-            <ExternalLink href="//inspirehep.net/info/hep/collection-policy">
-              selection policy
-            </ExternalLink>{' '}
-            and transfer it to INSPIRE.
-          </Row>
-          <Row className="mb3 pa3 bg-white">
-            <Col>
-              <DataImporterContainer onSkipClick={this.onDataImportSkipClick} />
-            </Col>
-          </Row>
-          {this.shouldDisplayForm() && (
-            <>
-              <Row className="mb3 ph3 pt3 bg-white">
-                <Col>
-                  <Form.Item
-                    label="Type of the document"
-                    labelCol={LABEL_COL}
-                    wrapperCol={WRAPPER_COL}
-                  >
-                    <SelectBox
-                      data-test-id="document-type-select"
-                      className="w-100"
-                      value={docType}
-                      options={DOC_TYPE_OPTIONS}
-                      onChange={this.onDocTypeChange}
-                    />
-                  </Form.Item>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <LiteratureSubmission
-                    initialFormData={importedFormData}
-                    error={error}
-                    onSubmit={this.onSubmit}
-                    docType={docType}
+  useEffect(
+    () => {
+      if (importedFormData) {
+        const newDocType = importedFormData.get('document_type');
+        setDocType(newDocType);
+      }
+    },
+    [importedFormData]
+  );
+  return (
+    <Row type="flex" justify="center">
+      <Col className="mt3 mb3" xs={24} md={21} lg={16} xl={15} xxl={14}>
+        <Row className="mb3 pa3 bg-white">
+          <h3>Suggest content</h3>
+          This form allows you to suggest a preprint, an article, a book, a
+          conference proceeding or a thesis you would like to see added to
+          INSPIRE. We will check your suggestion with our{' '}
+          <ExternalLink href="//inspirehep.net/info/hep/collection-policy">
+            selection policy
+          </ExternalLink>{' '}
+          and transfer it to INSPIRE.
+        </Row>
+        <Row className="mb3 pa3 bg-white">
+          <Col>
+            <DataImporterContainer onSkipClick={onDataImportSkipClick} />
+          </Col>
+        </Row>
+        {shouldDisplayForm && (
+          <>
+            <Row className="mb3 ph3 pt3 bg-white">
+              <Col>
+                <Form.Item
+                  label="Type of the document"
+                  labelCol={LABEL_COL}
+                  wrapperCol={WRAPPER_COL}
+                >
+                  <SelectBox
+                    data-test-id="document-type-select"
+                    className="w-100"
+                    value={docType}
+                    options={DOC_TYPE_OPTIONS}
+                    onChange={onDocTypeChange}
                   />
-                </Col>
-              </Row>
-            </>
-          )}
-        </Col>
-      </Row>
-    );
-  }
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <LiteratureSubmission
+                  initialFormData={importedFormData}
+                  error={error}
+                  onSubmit={onSubmit}
+                  docType={docType}
+                />
+              </Col>
+            </Row>
+          </>
+        )}
+      </Col>
+    </Row>
+  );
 }
 
 LiteratureSubmissionPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
   error: PropTypes.instanceOf(Map),
   importedFormData: PropTypes.instanceOf(Map),
 };
@@ -134,6 +117,10 @@ const stateToProps = state => ({
   importedFormData: state.submissions.get('initialData'),
 });
 
-const dispatchToProps = dispatch => ({ dispatch });
+const dispatchToProps = dispatch => ({
+  async onSubmit(formData) {
+    await dispatch(submit(LITERATURE_PID_TYPE, formData));
+  },
+});
 
 export default connect(stateToProps, dispatchToProps)(LiteratureSubmissionPage);
