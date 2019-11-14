580b422013a0f0a4de8b1106dfb4690dbd5d734d
harunurhan <harunurhan17@gmail.com>
2019-11-14 17:42:27 +0100

    ui: validate literature form after import
    INSPIR-2866

---
+++ ui/src/common/hooks/useIsMounted.js
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from 'react';
+
+export default function useIsMounted() {
+  const mountedRef = useRef(false);
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+  return mountedRef.current;
+}
