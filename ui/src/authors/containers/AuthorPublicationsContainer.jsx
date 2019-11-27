457da6395cdaf5e0c64a8f9059788665042bf2a6
Miguel Garcia <grcmiguel@outlook.com>
2019-11-27 10:00:05 +0100

    ui: rename '0 publications' to '0 research works'
    * Fix for INSPIR-2945

--- ui/src/authors/containers/AuthorPublicationsContainer.jsx
+++ ui/src/authors/containers/AuthorPublicationsContainer.jsx
@@ -27,7 +27,7 @@ function AuthorPublications({ authorFacetName, isCatalogerLoggedIn }) {
       namespace={AUTHOR_PUBLICATIONS_NS}
       baseQuery={baseQuery}
       baseAggregationsQuery={baseAggregationsQuery}
-      noResultsTitle="0 Publications"
+      noResultsTitle="0 Research works"
     />
   );
 }
