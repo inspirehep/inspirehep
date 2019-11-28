607de7df8b253cc7a2988e0d99aa6f36fd929b6c
harunurhan <harunurhan17@gmail.com>
2019-11-28 13:34:05 +0100

    ui: fix browser back after (ex: facets)
    INSPIR-2943

--- ui/src/middlewares/syncLocationWithSearch.js
+++ ui/src/middlewares/syncLocationWithSearch.js
@@ -1,7 +1,11 @@
 import { LOCATION_CHANGE } from 'connected-react-router';
 import { stringify } from 'qs';
 
-import { searchQueryUpdate, newSearch } from '../actions/search';
+import {
+  searchQueryUpdate,
+  newSearch,
+  searchQueryReset,
+} from '../actions/search';
 import { SEARCHABLE_COLLECTION_PATHNAMES } from '../reducers/search';
 
 const idInUrlRegExp = new RegExp('/\\d+');
@@ -29,6 +33,7 @@ function pathnameToSearchNamespace(pathname) {
   return pathname.substring(1);
 }
 
+// FIXME: this can be moved to reducer?
 export default function({ dispatch, getState }) {
   return next => action => {
     if (action.type === LOCATION_CHANGE) {
@@ -40,6 +45,8 @@ export default function({ dispatch, getState }) {
       // use this instead of getState() for ease of testing
       const nextLocation = action.payload.location;
 
+      // dipsatch new search to clear to search state for previous location when pathname changes
+      // ex: from `/literature?...` to `/` (home)
       if (
         prevLocation.pathname !== nextLocation.pathname &&
         isSearchPage(prevLocation)
@@ -49,12 +56,22 @@ export default function({ dispatch, getState }) {
       }
 
       if (isSearchPage(nextLocation)) {
-        const { isFirstRendering } = action.payload;
         const nextNamespace = pathnameToSearchNamespace(nextLocation.pathname);
+
+        const { isFirstRendering } = action.payload;
         if (
-          isFirstRendering ||
+          isFirstRendering || // when first route that user hits the application is a search page
+          // make sure LOCATION_CHANGE is not caused by SEARCH_QUERY_UPDATE, to avoid infinite loop
           !isLocationSyncedWithSearchQuery(nextNamespace, getState())
         ) {
+          // reset whole query when `back` is clicked on the same search page, then location query will be used
+          if (
+            prevLocation.pathname === nextLocation.pathname &&
+            action.payload.action === 'POP'
+          ) {
+            dispatch(searchQueryReset(nextNamespace));
+          }
+
           const { query } = nextLocation;
           dispatch(searchQueryUpdate(nextNamespace, query));
         }
