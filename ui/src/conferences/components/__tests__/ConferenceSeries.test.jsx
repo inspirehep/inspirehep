d7fa1d7ce2f59cf4752e20ff3eff70bf68bf1567
Miguel Garcia <grcmiguel@outlook.com>
2019-12-16 16:37:27 +0100

    ui: display several series
    * INSPIR-3022

--- ui/src/conferences/components/__tests__/ConferenceSeries.test.jsx
+++ ui/src/conferences/components/__tests__/ConferenceSeries.test.jsx
@@ -6,12 +6,21 @@ import ConferenceSeries from '../ConferenceSeries';
 
 describe('ConferenceSeries', () => {
   it('renders with only name', () => {
-    const series = fromJS({ name: 'Conference Name' });
+    const series = fromJS([{ name: 'Conference Name' }]);
     const wrapper = shallow(<ConferenceSeries series={series} />);
     expect(wrapper).toMatchSnapshot();
   });
   it('renders with name and number', () => {
-    const series = fromJS({ name: 'Conference Name', number: 10 });
+    const series = fromJS([{ name: 'Conference Name', number: 10 }]);
+    const wrapper = shallow(<ConferenceSeries series={series} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('renders several series', () => {
+    const series = fromJS([
+      { name: 'Conference 1' },
+      { name: 'Conference 2', number: 10 },
+      { name: 'Conference 3' },
+    ]);
     const wrapper = shallow(<ConferenceSeries series={series} />);
     expect(wrapper).toMatchSnapshot();
   });
