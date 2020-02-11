738b3cc574a7d9577af31133eb14d3efdb50874a
Harris Tzovanakis <me@drjova.com>
2020-02-11 14:43:53 +0100

    docker: init

---
+++ build-and-deploy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+TAG="${TRAVIS_TAG:-$(git describe --always --tags)}"
+
+retry() {
+    "${@}" || "${@}" || exit 2
+}
+
+login() {
+  echo "Logging into Docker Hub"
+  retry docker login \
+      "--username=${DOCKERHUB_USER}" \
+      "--password=${DOCKERHUB_PASSWORD}"
+}
+
+buildPush() {
+  context="${1}"
+  image="${2}"
+  echo "Building docker image for ${context}"
+
+  retry docker build \
+    --build-arg VERSION="${TAG}" \
+    -t "${image}:${TAG}" \
+    -t "${image}" \
+    "${context}"
+
+  echo "Pushing image to ${image}:${TAG}"
+  retry docker push "${image}:${TAG}"
+  retry docker push "${image}"
+}
+
+logout() {
+  echo "Logging out""${@}"
+  retry docker logout
+}
+
+deployQA() {
+  app="${1}"
+  if [ -z "${TRAVIS_TAG}" ]; then
+    echo "Deploying ${app} ..."
+    curl -X POST \
+      -F token=${DEPLOY_QA_TOKEN} \
+      -F ref=master \
+      -F variables[APP_NAME]=${app} \
+      -F variables[NEW_TAG]=${TAG} \
+      https://gitlab.cern.ch/api/v4/projects/62928/trigger/pipeline
+  fi
+}
+
+
+main() {
+  login
+  buildPush "." "inspirehep/editor"
+  logout
+  deployQA "editor"
+}
+main
