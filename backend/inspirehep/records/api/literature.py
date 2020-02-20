72e77aa72590a84138a00037ff7ebfddf799fa18
Pawel Zembrzuski <pawel.kacper.zembrzuski@cern.ch>
2020-02-20 14:31:39 +0100

    POC: without tests fixes as I cannot re-record :/
    INSPIR-3151

--- backend/inspirehep/records/api/literature.py
+++ backend/inspirehep/records/api/literature.py
@@ -7,8 +7,11 @@
 import datetime
 import json
 import uuid
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures._base import TimeoutError
 from io import BytesIO
 
+import boto3
 import magic
 import requests
 import structlog
@@ -23,7 +26,7 @@ from inspire_utils.record import get_value
 from invenio_db import db
 from redis import StrictRedis
 
-from inspirehep.files.api import current_s3_instance
+from inspirehep.files.api.s3 import S3
 from inspirehep.orcid.api import push_to_orcid
 from inspirehep.pidstore.api import PidStoreLiterature
 from inspirehep.records.api.mixins import (
@@ -283,73 +286,145 @@ class LiteratureRecord(
 
     def add_documents(self, documents):
         builder = LiteratureBuilder()
-        for document in documents:
-            if not document.get("hidden", False):
-                new_file_data = self.add_file(**document)
-                document.update(new_file_data)
+        aws_session_params = {
+            "aws_access_key_id": current_app.config.get("S3_ACCESS_KEY"),
+            "aws_secret_access_key": current_app.config.get("S3_SECRET_KEY"),
+        }
+        with ThreadPoolExecutor() as executor:
+            tasks = {
+                executor.submit(
+                    self.add_file,
+                    aws_session_params,
+                    current_app.config.get("S3_BUCKET_PREFIX"),
+                    current_app.config.get("S3_HOSTNAME"),
+                    current_app.config.get("S3_FILE_ACL"),
+                    **document,
+                ): idx
+                for idx, document in enumerate(documents)
+                if not document.get("hidden", False)
+            }
             try:
-                builder.add_document(**document)
-            except ValueError:
+                for future in as_completed(tasks, timeout=30):
+                    idx = tasks[future]
+                    new_file_data = future.result()
+                    documents[idx].update(new_file_data)
+                    try:
+                        builder.add_document(**documents[idx])
+                    except ValueError:
+                        LOGGER.exception(
+                            "Duplicated document found",
+                            recid=self.get("control_number"),
+                            uuid=self.id,
+                            document=documents[idx].get("key"),
+                        )
+            except TimeoutError:
                 LOGGER.exception(
-                    "Duplicated document found",
+                    "Threads didn't return results on time",
                     recid=self.get("control_number"),
                     uuid=self.id,
-                    document=document.get("key"),
                 )
         return builder.record.get("documents")
 
     def add_figures(self, figures):
         builder = LiteratureBuilder()
-        for figure in figures:
-            new_file_data = self.add_file(**figure)
-            figure.update(new_file_data)
+        aws_session_params = {
+            "aws_access_key_id": current_app.config.get("S3_ACCESS_KEY"),
+            "aws_secret_access_key": current_app.config.get("S3_SECRET_KEY"),
+        }
+        with ThreadPoolExecutor() as executor:
+            tasks = {
+                executor.submit(
+                    self.add_file,
+                    aws_session_params,
+                    current_app.config.get("S3_BUCKET_PREFIX"),
+                    current_app.config.get("S3_HOSTNAME"),
+                    current_app.config.get("S3_FILE_ACL"),
+                    **figure,
+                ): idx
+                for idx, figure in enumerate(figures)
+            }
             try:
-                builder.add_figure(**figure)
-            except ValueError:
+                for future in as_completed(tasks, timeout=30):
+                    idx = tasks[future]
+                    new_file_data = future.result()
+                    figures[idx].update(new_file_data)
+                    try:
+                        builder.add_figure(**figures[idx])
+                    except ValueError:
+                        LOGGER.exception(
+                            "Duplicated figure found",
+                            recid=self.get("control_number"),
+                            uuid=self.id,
+                            figure=figures[idx].get("key"),
+                        )
+            except TimeoutError:
                 LOGGER.exception(
-                    "Duplicated figure found",
+                    "Threads didn't return results on time",
                     recid=self.get("control_number"),
                     uuid=self.id,
-                    figure=figure.get("key"),
                 )
         return builder.record.get("figures")
 
+    @staticmethod
     def add_file(
-        self, url, original_url=None, key=None, filename=None, *args, **kwargs
+        aws_session_params,
+        s3_bucket_prefix,
+        s3_hostname,
+        s3_acl,
+        url,
+        original_url=None,
+        key=None,
+        filename=None,
+        *args,
+        **kwargs,
     ):
+        """Adds files to s3. Should be called in separate thread
+
+            aws_session_params (dict): requires
+        """
+        session = boto3.session.Session(**aws_session_params)
+        s3_client = session.client("s3", endpoint_url=s3_hostname, **aws_session_params)
+        s3_instance = S3(
+            client=s3_client,
+            resource=None,
+            s3_bucket_prefix=s3_bucket_prefix,
+            s3_hostname=s3_hostname,
+            s3_file_acl=s3_acl,
+        )
+
         file_data = download_file_from_url(url)
         new_key = hash_data(file_data)
         mimetype = magic.from_buffer(file_data, mime=True)
         size = len(file_data)
         file_data = BytesIO(file_data)
         filename = filename or key
-        acl = current_app.config["S3_FILE_ACL"]
-        if current_s3_instance.file_exists(new_key):
+        acl = s3_acl
+        if s3_instance.file_exists(new_key):
             LOGGER.info(
                 "Replacing file metadata",
                 key=new_key,
-                recid=self.get("control_number"),
-                uuid=self.id,
+                recid=kwargs.get("control_number"),
+                uuid=kwargs.get("id"),
             )
-            current_s3_instance.replace_file_metadata(new_key, filename, mimetype, acl)
+            s3_instance.replace_file_metadata(new_key, filename, mimetype, acl)
         else:
             LOGGER.info(
                 "Uploading file to s3",
                 key=new_key,
-                recid=self.get("control_number"),
-                uuid=self.id,
+                recid=kwargs.get("control_number"),
+                uuid=kwargs.get("id"),
             )
-            current_s3_instance.upload_file(file_data, new_key, filename, mimetype, acl)
+            s3_instance.upload_file(file_data, new_key, filename, mimetype, acl)
         result = {
             "key": new_key,
             "filename": filename,
-            "url": current_s3_instance.get_file_url(new_key),
+            "url": s3_instance.get_file_url(new_key),
             "mimetype": mimetype,
             "size": size,
         }
         if (
             url.startswith("http")
-            and not current_s3_instance.is_s3_url(url)
+            and not s3_instance.is_s3_url(url)
             and not original_url
         ):
             result["original_url"] = url
