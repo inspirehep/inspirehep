d31ad7c07542df69547f64d4fec1f8873502abb9
Miguel Garcia <grcmiguel@outlook.com>
2020-03-19 18:00:50 +0100

    router: remove citation summary hash from url in other page
    * INSPIR-3262

--- ui/src/common/containers/__tests__/SearchBoxContainer.test.jsx
+++ ui/src/common/containers/__tests__/SearchBoxContainer.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { fromJS } from 'immutable';
+import { replace } from 'connected-react-router';
 
 import { getStoreWithState } from '../../../fixtures/store';
 import SearchBoxContainer from '../SearchBoxContainer';
@@ -54,5 +55,30 @@ describe('SearchBoxContainer', () => {
       },
     ];
     expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('resets the hash on search in collection different to literature', async () => {
+    const currentNamespace = 'literature';
+    const newNamespace = 'authors';
+    const store = getStoreWithState({
+      search: fromJS({
+        currentNamespace,
+      }),
+      router: {
+        location: {
+          hash: '#whatever',
+        },
+      },
+    });
+    const wrapper = mount(
+      <Provider store={store}>
+        <SearchBoxContainer />
+      </Provider>
+    );
+    const onSearch = wrapper.find(SearchBox).prop('onSearch');
+    onSearch(newNamespace, 'test');
+
+    const expectedAction = replace({ hash: '' });
+    expect(store.getActions()).toContainEqual(expectedAction);
   });
 });
