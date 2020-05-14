74796e086997d7d82bba59592b029f9982d34a3c
harunurhan <harunurhan17@gmail.com>
2020-05-14 18:04:46 +0200

    seminars: add https for links in confirmation email

--- backend/tests/integration/mailing/test_api_seminars.py
+++ backend/tests/integration/mailing/test_api_seminars.py
@@ -3,6 +3,7 @@ from flask import current_app, render_template
 from mock import patch
 
 from inspirehep.mailing.api.seminars import send_seminar_confirmation_email
+from inspirehep.utils import get_inspirehep_url
 
 
 @patch("inspirehep.mailing.api.seminars.send_email")
@@ -10,7 +11,7 @@ def test_send_seminar_confirmation_email(mock_send_email, base_app, db, es_clear
     expected_recipient = "johndoe@gmail.com"
     seminar = {"control_number": "1234"}
     expected_subject = "Your seminar (1234) has been successfully submitted!"
-    host = current_app.config["SERVER_NAME"]
+    host = get_inspirehep_url()
     expected_content = render_template(
         "mailing/seminars/confirmation_new.html",
         seminar_url=f"{host}/seminars/1234",
