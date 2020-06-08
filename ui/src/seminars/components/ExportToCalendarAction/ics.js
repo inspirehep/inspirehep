41648941b3afaf5658f05a93ed6b6e1ad5e61138
harunurhan <harunurhan17@gmail.com>
2020-06-08 13:39:00 +0200

    ui: add minimal ics utility to replace `ics` package
    `ics` package introduces lots of dependencies that are not relevent to our use case

--- ui/src/seminars/components/ExportToCalendarAction/ics.js
+++ ui/src/seminars/components/ExportToCalendarAction/ics.js
@@ -1,47 +1,46 @@
 import moment from 'moment';
-import { createEvent } from 'ics';
 import { List } from 'immutable';
 
 import { SEMINARS } from '../../../common/routes';
 import { stripHtml } from '../../../common/utils';
 
-function toDateTimeArray(datetimeString) {
+function formatDateTime(datetimeString) {
   const datetime = moment.utc(datetimeString);
-  return [
-    datetime.year(),
-    datetime.month() + 1, // moment months are 0 indexed
-    datetime.date(),
-    datetime.hour(),
-    datetime.minutes(),
-  ];
+  return datetime.format('YYYYMMDDTHHmmssZ').replace('+00:00', 'Z');
 }
 
 export default function getIcsFileContent(seminar) {
+  const controlNumber = seminar.get('control_number');
+  const id = `inspirehep-seminar-${controlNumber}`;
+  const location = seminar.getIn(['address', 'place_name']);
+  const url = `${window.location.origin}${SEMINARS}/${controlNumber}`;
+  const title = seminar.getIn(['title', 'title']);
+  const start = formatDateTime(seminar.get('start_datetime'));
+  const end = formatDateTime(seminar.get('end_datetime'));
+  const description = stripHtml(seminar.getIn(['abstract', 'value'], ''));
+
   const categories = seminar
     .get('inspire_categories', List())
     .map(category => category.get('term'))
-    .toArray();
-  const start = toDateTimeArray(seminar.get('start_datetime'));
-  const end = toDateTimeArray(seminar.get('end_datetime'));
-  const controlNumber = seminar.get('control_number');
-  const description = stripHtml(seminar.getIn(['abstract', 'value'], ''));
+    .join(',');
 
-  const event = {
-    productId: 'inspirehep/seminars',
-    uid: `inspirehep-seminar-${controlNumber}`,
-    start,
-    end,
-    title: seminar.getIn(['title', 'title']),
-    description,
-    location: seminar.getIn(['address', 'place_name']),
-    url: `${window.location.origin}${SEMINARS}/${controlNumber}`,
-    categories: categories.length > 0 ? categories : undefined,
-  };
-  const { value, error } = createEvent(event);
-
-  if (error) {
-    throw error;
-  }
-
-  return value;
+  return [
+    'BEGIN:VCALENDAR',
+    'VERSION:2.0',
+    'CALSCALE:GREGORIAN',
+    'PRODID:inspirehep/seminars',
+    'BEGIN:VEVENT',
+    `DTSTART:${start}`,
+    `DTEND:${end}`,
+    `UID:${id}`,
+    `SUMMARY:${title}`,
+    `URL:${url}`,
+    description && `DESCRIPTION:${description}`,
+    categories && `CATEGORIES:${categories}`,
+    location && `LOCATION:${location}`,
+    'END:VEVENT',
+    'END:VCALENDAR',
+  ]
+    .filter(Boolean)
+    .join('\n'); // TODO: handle OS dependent line-break (`\r\n` on windows)
 }
