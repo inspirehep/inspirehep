a6b0a112b6478aa39ce1c75d494bef7fa4910ad6
Harris Tzovanakis <me@drjova.com>
2020-06-29 15:43:13 +0200

    ui: remove NewFeatureTag from self-citations, cited by tab and seminars

--- ui/src/literature/components/CitationSummaryBox.jsx
+++ ui/src/literature/components/CitationSummaryBox.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { Col, Row } from 'antd';
 import PropTypes from 'prop-types';
 
-import ContentBox from '../../../common/components/ContentBox';
-import CitationSummaryGraphContainer from '../../../common/containers/CitationSummaryGraphContainer';
-import CitationSummaryTableContainer from '../../../common/containers/CitationSummaryTableContainer';
-import NewFeatureTag from '../../../common/components/NewFeatureTag';
-import './CitationSummaryBox.scss';
-import ExcludeSelfCitationsContainer from '../../containers/ExcludeSelfCitationsContainer';
+import ContentBox from '../../common/components/ContentBox';
+import CitationSummaryGraphContainer from '../../common/containers/CitationSummaryGraphContainer';
+import CitationSummaryTableContainer from '../../common/containers/CitationSummaryTableContainer';
+import ExcludeSelfCitationsContainer from '../containers/ExcludeSelfCitationsContainer';
 
 function CitationSummaryBox({ namespace }) {
   return (
     <ContentBox subTitle="Citation Summary">
       <ExcludeSelfCitationsContainer namespace={namespace} />
-      <NewFeatureTag className="without-margin-left" />
+
       <Row gutter={{ xs: 0, lg: 32 }}>
         <Col span={24}>
           <CitationSummaryTableContainer />
