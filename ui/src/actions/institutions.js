3d4592734e3758cf44dc8e7b30d89f849f598a38
Mona <awi_moni@yahoo.com>
2020-08-06 17:30:01 +0200

    ui: add request options default

--- ui/src/actions/institutions.js
+++ ui/src/actions/institutions.js
@@ -3,7 +3,6 @@ import {
   INSTITUTION_SUCCESS,
   INSTITUTION_ERROR,
 } from './actionTypes';
-import { UI_SERIALIZER_REQUEST_OPTIONS } from '../common/http';
 import generateRecordFetchAction from './recordsFactory';
 import { INSTITUTIONS_PID_TYPE } from '../common/constants';
 
@@ -12,7 +11,6 @@ const fetchInstitution = generateRecordFetchAction({
   fetchingActionActionType: INSTITUTION_REQUEST,
   fecthSuccessActionType: INSTITUTION_SUCCESS,
   fetchErrorActionType: INSTITUTION_ERROR,
-  requestOptions: UI_SERIALIZER_REQUEST_OPTIONS,
 });
 
 export default fetchInstitution;
--- ui/src/actions/seminars.js
+++ ui/src/actions/seminars.js
@@ -1,5 +1,4 @@
 import { SEMINAR_REQUEST, SEMINAR_SUCCESS, SEMINAR_ERROR } from './actionTypes';
-import { UI_SERIALIZER_REQUEST_OPTIONS } from '../common/http';
 import generateRecordFetchAction from './recordsFactory';
 import { SEMINARS_PID_TYPE } from '../common/constants';
 
@@ -8,7 +7,6 @@ const fetchSeminar = generateRecordFetchAction({
   fetchingActionActionType: SEMINAR_REQUEST,
   fecthSuccessActionType: SEMINAR_SUCCESS,
   fetchErrorActionType: SEMINAR_ERROR,
-  requestOptions: UI_SERIALIZER_REQUEST_OPTIONS,
 });
 
 export default fetchSeminar;
