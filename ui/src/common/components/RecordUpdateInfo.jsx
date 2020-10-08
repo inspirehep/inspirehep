592523836357712718e1842577c24d89ccc405cd
Oguz Demirbasci <ogguzx@gmail.com>
2020-10-08 12:25:37 +0200

    ui: update date in author profile
    Ref #1373

---
+++ ui/src/common/components/RecordUpdateInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment-timezone';
+import { LOCAL_TIMEZONE } from '../constants';
+
+const DATE_AND_TIME_DISPLAY_FORMAT = `MMM D, YYYY`;
+
+function RecordUpdateInfo({ updateDate }) {
+  const formattedDate = moment
+    .utc(updateDate)
+    .tz(LOCAL_TIMEZONE)
+    .format(DATE_AND_TIME_DISPLAY_FORMAT);
+  return <span className="secondary-color">Updated on {formattedDate}</span>;
+}
+
+RecordUpdateInfo.propTypes = { updateDate: PropTypes.string.isRequired };
+
+export default RecordUpdateInfo;
