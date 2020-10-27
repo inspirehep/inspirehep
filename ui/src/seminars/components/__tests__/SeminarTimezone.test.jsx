738fb2e9ce6c2df074a9f80840ece3e6a653cfc9
Harris Tzovanakis <me@drjova.com>
2020-10-27 15:18:19 +0100

    ui: mock the dates for seminars

--- ui/src/seminars/components/__tests__/SeminarTimezone.test.jsx
+++ ui/src/seminars/components/__tests__/SeminarTimezone.test.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { advanceTo, clear } from 'jest-date-mock';
 import SeminarTimezone from '../SeminarTimezone';
 
 describe('SeminarTimezone', () => {
   it('renders with timezone', () => {
+    advanceTo('2020-09-10');
     const wrapper = shallow(<SeminarTimezone timezone="Europe/Zurich" />);
     expect(wrapper).toMatchSnapshot();
+    clear();
   });
 });
