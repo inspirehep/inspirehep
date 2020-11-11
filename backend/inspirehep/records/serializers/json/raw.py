d8b7b56d704b3ea2c10d60b636d51d60543c8a46
Harris Tzovanakis <me@drjova.com>
2020-11-11 14:00:05 +0100

    serializers: json raw use inspire envelope schema

--- backend/inspirehep/records/serializers/json/raw.py
+++ backend/inspirehep/records/serializers/json/raw.py
@@ -6,19 +6,22 @@
 # the terms of the MIT License; see LICENSE file for more details.
 
 
-from invenio_records_rest.serializers import RecordSchemaJSONV1
 from invenio_records_rest.serializers.response import record_responsify
 
 from inspirehep.accounts.api import is_superuser_or_cataloger_logged_in
-from inspirehep.records.marshmallow.base import ForbiddenSchema
+from inspirehep.records.marshmallow.base import EnvelopeSchema, ForbiddenSchema
 from inspirehep.serializers import ConditionalMultiSchemaJSONSerializer
 
 records_raw_json = ConditionalMultiSchemaJSONSerializer(
     [
-        (lambda _: is_superuser_or_cataloger_logged_in(), RecordSchemaJSONV1,),
+        (
+            lambda _: is_superuser_or_cataloger_logged_in(),
+            EnvelopeSchema,
+        ),
         (None, ForbiddenSchema),
     ]
 )
 raw_json_detail_response = record_responsify(
-    records_raw_json, "application/vnd+inspire.record.raw+json",
+    records_raw_json,
+    "application/vnd+inspire.record.raw+json",
 )
