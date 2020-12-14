42599ad9a3b137d00632d952e1c118dad7e8e0b3
Micha Moskovic <michamos@gmail.com>
2020-12-14 11:43:50 +0100

    poetry: use pyproject.toml instead of setup.py
    * Newer versions of poetry don't use the info of `setup.py`, which is a
      problem as we rely heavily on entry-points. The solution is to move
      everything to `pyproject.toml` instead, and drop the `setup.py`.
    * I also looked into dropping `setup.cfg`. This is used for `isort`,
      `flake8` and `coverage` config. New versions of `isort` and `coverage`
      can use `pyproject.toml` instead, so I updated them and adapted their
      config. For now the `setup.cfg` file is still needed for `flake8 (see
      https://gitlab.com/pycqa/flake8/-/issues/428).
    * To make this commit, I needed to setup the `prettier` pre-commit hook.
      The repo has changed, so I changed the `pre-commit` config, and also
      had to extend the `prettier` ignore list.

--- backend/run-code-checks.sh
+++ backend/run-code-checks.sh
@@ -6,5 +6,5 @@
 # inspirehep is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-poetry run isort -rc -c -df  && \
+poetry run isort --diff --check . && \
 poetry run flake8
