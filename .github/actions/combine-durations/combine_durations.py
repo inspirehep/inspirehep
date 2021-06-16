595ca97d34de5a317ddb3e12c8fab312b8758397
Micha Moskovic <michamos@gmail.com>
2021-06-16 15:50:33 +0200

    github: auto-update test durations during CI

---
+++ .github/actions/combine-durations/combine_durations.py
@@ -0,0 +1,23 @@
+import json
+import sys
+from pathlib import Path
+
+split_prefix = sys.argv[1]
+durations_path = Path(sys.argv[2])
+
+split_paths = Path(".").glob(f"{split_prefix}*/{durations_path}")
+previous_durations = json.loads(durations_path.read_text())
+new_durations = previous_durations.copy()
+
+for path in split_paths:
+    durations = json.loads(path.read_text())
+    new_durations.update(
+        {
+            name: duration
+            for (name, duration) in durations.items()
+            if previous_durations.get(name) != duration
+        }
+    )
+
+with open(durations_path, "w") as f:
+    json.dump(new_durations, f)
