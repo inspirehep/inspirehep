3dc7798c981a590881bef86412facfb717e8185d
Oguz Demirbasci <ogguzx@gmail.com>
2021-07-07 10:17:30 +0200

    editor: page refresh when 412 error responded Ref: #2013

--- record-editor/src/app/record-editor/record-toolbar/record-toolbar.component.ts
+++ record-editor/src/app/record-editor/record-toolbar/record-toolbar.component.ts
@@ -48,7 +48,8 @@ import { HOVER_TO_DISMISS_INDEFINITE_TOAST } from '../../shared/constants';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecordToolbarComponent extends SubscriberComponent
+export class RecordToolbarComponent
+  extends SubscriberComponent
   implements OnInit {
   // `undefined` if there is no record being edited
   record: object;
@@ -75,21 +76,21 @@ export class RecordToolbarComponent extends SubscriberComponent
   ngOnInit() {
     this.globalAppStateService.hasAnyValidationProblem$
       .takeUntil(this.isDestroyed)
-      .subscribe(hasAnyValidationProblem => {
+      .subscribe((hasAnyValidationProblem) => {
         this.hasAnyValidationProblem = hasAnyValidationProblem;
         this.changeDetectorRef.markForCheck();
       });
 
     this.globalAppStateService.jsonBeingEdited$
       .takeUntil(this.isDestroyed)
-      .subscribe(jsonBeingEdited => {
+      .subscribe((jsonBeingEdited) => {
         this.record = jsonBeingEdited;
         this.changeDetectorRef.markForCheck();
       });
 
     this.globalAppStateService.pidTypeBeingEdited$
       .takeUntil(this.isDestroyed)
-      .subscribe(pidTypeBeingEdited => {
+      .subscribe((pidTypeBeingEdited) => {
         this.pidType = pidTypeBeingEdited;
       });
   }
@@ -118,7 +119,7 @@ export class RecordToolbarComponent extends SubscriberComponent
     const references = this.record['references'];
     this.apiService
       .getLinkedReferences(references)
-      .then(linkedReferences => {
+      .then((linkedReferences) => {
         this.record['references'] = linkedReferences;
         const recordWithLinkedReferences = Object.assign({}, this.record);
         this.globalAppStateService.jsonBeingEdited$.next(
@@ -133,9 +134,10 @@ export class RecordToolbarComponent extends SubscriberComponent
 
   private cleanupAndSaveRecord(record) {
     this.recordCleanupService.cleanup(record);
-    this.apiService
-      .saveRecord(record)
-      .subscribe(() => this.onSaveSuccess(), error => this.onSaveError(error));
+    this.apiService.saveRecord(record).subscribe(
+      () => this.onSaveSuccess(),
+      (error) => this.onSaveError(error)
+    );
   }
 
   private onSaveSuccess() {
@@ -158,6 +160,9 @@ export class RecordToolbarComponent extends SubscriberComponent
       );
     } else {
       this.toastrService.error('Could not save the record', 'Error');
+    }
+    if (error.status === 412) {
+      setTimeout(() => window.location.reload(), 2000);
     }
   }
 
