34d66d0f596fcdb5582af0ea972163380b6fdd2c
Micha Moskovic <michamos@gmail.com>
2022-04-14 11:37:43 +0200

    ui: fix link broken link on author submission

--- ui/src/submissions/authors/containers/AuthorSubmissionPageContainer.jsx
+++ ui/src/submissions/authors/containers/AuthorSubmissionPageContainer.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Map } from 'immutable';
+import { Link } from 'react-router-dom';
 import { object } from 'yup';
 
 import { submit } from '../../../actions/submissions';
 import AuthorSubmission from '../components/AuthorSubmission';
-import ExternalLink from '../../../common/components/ExternalLink.tsx';
 import uniqueOrcid from '../schemas/uniqueOrcid';
 import { AUTHORS_PID_TYPE } from '../../../common/constants';
 import SubmissionPage from '../../common/components/SubmissionPage';
+import { AUTHORS } from '../../../common/routes';
 
 const extraSchemaForNewAuthor = object().shape({ orcid: uniqueOrcid() });
 
@@ -33,12 +34,9 @@ class AuthorSubmissionPage extends Component {
         title="Suggest author"
         description={
           <span>
-            This form allows you to create the profile of a new author. All
-            modifications are transferred to{' '}
-            <ExternalLink href="//inspirehep.net/hepnames">
-              inspirehep.net/hepnames
-            </ExternalLink>{' '}
-            upon approval.
+            This form allows you to create the profile of a new author. It will
+            be added to the <Link to={AUTHORS}>authors collection</Link> upon
+            approval.
           </span>
         }
       >
@@ -59,11 +57,11 @@ AuthorSubmissionPage.propTypes = {
   query: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
-const stateToProps = state => ({
+const stateToProps = (state) => ({
   error: state.submissions.get('submitError'),
   query: state.router.location.query,
 });
 
-const dispatchToProps = dispatch => ({ dispatch });
+const dispatchToProps = (dispatch) => ({ dispatch });
 
 export default connect(stateToProps, dispatchToProps)(AuthorSubmissionPage);
