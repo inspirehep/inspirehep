e26214bfc065ba6209fdf63a01a36a931f5cad4d
MJedr <jedrych.marcysia@gmail.com>
2022-09-07 08:30:45 +0200

    curation: refactore pdg cli

--- backend/inspirehep/curation/cli.py
+++ backend/inspirehep/curation/cli.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# inspirehep is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+
 import click
 import requests
 from elasticsearch_dsl import Q
@@ -5,24 +13,6 @@ from flask.cli import with_appcontext
 
 from inspirehep.curation.tasks import update_pdg_keywords_in_records
 from inspirehep.search.api import LiteratureSearch
-from inspirehep.utils import chunker
-
-
-def _remove_pdg_keywords_from_record_keywords(record):
-    record_keywords = record["keywords"]
-    for keyword_idx, keyword_object in enumerate(record_keywords):
-        if keyword_object.get("schema") == "PDG":
-            del record_keywords[keyword_idx]
-    if not record_keywords:
-        del record["keywords"]
-
-
-def _update_record_keywords_with_new_pdg_keywords(record, pdg_keywords):
-    keywords = record.get("keywords", [])
-    for pdg_keyword in pdg_keywords:
-        keywords.append({"schema": "PDG", "value": pdg_keyword})
-    if keywords:
-        record["keywords"] = keywords
 
 
 @click.group()
@@ -32,9 +22,10 @@ def curation():
 
 @curation.command("update-pdg-keywords")
 @click.option("--url", help="URL pointing to PDG JSON data")
+@click.option("-bs", "--batch-size", default=50)
 @with_appcontext
 @click.pass_context
-def update_pdg_keywords(ctx, url):
+def update_pdg_keywords(ctx, url, batch_size):
     pdg_json_response = requests.get(url, headers={"Accept": "application/json"})
 
     if pdg_json_response.status_code != 200:
@@ -46,12 +37,17 @@ def update_pdg_keywords(ctx, url):
         record["inspireId"]: record["pdgIdList"] for record in pdg_json_data
     }
     records_with_pdg_keywords_query = Q("match", keywords__schema="PDG")
-    search_obj = (
-        LiteratureSearch()
-        .query(records_with_pdg_keywords_query)
-        .params(size=1000, scroll="60m")
-    )
-    records_with_pdg_recids = (rec["control_number"] for rec in search_obj.scan())
+    scan_obj = (
+        LiteratureSearch().query(records_with_pdg_keywords_query).params(size=1000)
+    ).scan()
 
-    for batch in chunker(records_with_pdg_recids, 50):
+    generator_empty = False
+    while not generator_empty:
+        batch = []
+        for _ in range(batch_size):
+            try:
+                record_data = next(scan_obj)
+                batch.append(record_data["control_number"])
+            except StopIteration:
+                generator_empty = True
         update_pdg_keywords_in_records.delay(batch, record_ids_pdg_keywords_dict)
