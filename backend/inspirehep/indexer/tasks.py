14725a0d9f4ae24cc7a7d653ecb8983bcd607985
MJedr <jedrych.marcysia@gmail.com>
2022-10-24 13:04:36 +0200

    unify indexer classes

--- backend/inspirehep/indexer/tasks.py
+++ backend/inspirehep/indexer/tasks.py
@@ -8,7 +8,6 @@
 import structlog
 from celery import shared_task
 from elasticsearch import ConnectionError, ConnectionTimeout, RequestError
-from flask import current_app
 from sqlalchemy.exc import (
     DisconnectionError,
     OperationalError,
@@ -19,12 +18,8 @@ from sqlalchemy.exc import (
 from sqlalchemy.orm.exc import NoResultFound, StaleDataError
 
 from inspirehep.indexer.api import get_references_to_update
-from inspirehep.indexer.base import (
-    InspireRecordIndexer,
-    LiteratureRecordFulltextIndexer,
-)
+from inspirehep.indexer.base import InspireRecordIndexer
 from inspirehep.records.api import InspireRecord
-from inspirehep.utils import setup_celery_task_signals
 
 LOGGER = structlog.getLogger()
 
@@ -90,47 +85,3 @@ def index_record(self, uuid, record_version=None, force_delete=None):
 
     if uuids_to_reindex:
         batch_index(list(uuids_to_reindex))
-
-
-@shared_task(
-    bind=True,
-    queue="indexer_task",
-    acks_late=True,
-    retry_backoff=True,
-    retry_kwargs={"max_retries": 3},
-    autoretry_for=CELERY_INDEX_RECORD_RETRY_ON_EXCEPTIONS,
-)
-def index_fulltext(self, record_id, record_version):
-    if current_app.config.get("FEATURE_FLAG_ENABLE_SIGNAL_HANDLER"):
-        setup_celery_task_signals([record_id], self.name)
-
-    LOGGER.info("Indexing record including fulltext", uuid=record_id)
-    record = InspireRecord.get_record(record_id, record_version)
-    if record.pid_type != "lit":
-        return
-    LiteratureRecordFulltextIndexer().index(record, record_version=record_version)
-
-
-@shared_task(ignore_result=False, bind=True)
-def batch_index_literature_fulltext(self, records_uuids, request_timeout=None):
-    """Process all provided references and index them in bulk.
-    Be sure that uuids are not duplicated in batch.
-    Args:
-        records_uuids (list): list of uuids to process. All duplicates will be removed.
-        request_timeout: Timeout in which ES should respond. Otherwise break.
-
-    Returns:
-        dict: dict with success count and failure list
-                (with uuids of failed records)
-    """
-    LOGGER.info(
-        f"Starting task `batch_index_literature_fulltext` for {len(records_uuids)} records"
-    )
-
-    if current_app.config.get("FEATURE_FLAG_ENABLE_SIGNAL_HANDLER"):
-        setup_celery_task_signals(records_uuids, self.name)
-
-    return LiteratureRecordFulltextIndexer().bulk_index(
-        records_uuids,
-        max_chunk_bytes=current_app.config.get("ES_FULLTEXT_MAX_BULK_CHUNK_SIZE"),
-    )
