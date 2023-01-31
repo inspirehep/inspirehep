42afd7ab1681b1ae6e782833c2c467facaf65f76
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2023-01-31 11:26:17 +0100

    ui: display persistent identifiers
    * ref: cern-sis/issues-inspire#224

---
+++ ui/src/literature/components/PersistentIdentifiers.tsx
@@ -0,0 +1,43 @@
+import { List, Map } from 'immutable';
+import React from 'react';
+import EventTracker from '../../common/components/EventTracker';
+import InlineList from '../../common/components/InlineList';
+import LinkWithTargetBlank from '../../common/components/LinkWithTargetBlank';
+
+const PersistentIdentifiers = ({
+  identifiers,
+}: {
+  identifiers: List<string>;
+}) => {
+  const renderIdentifier = (identifier: Map<string, string>) => {
+    const schema = identifier.get('schema');
+    const value = identifier.get('value');
+
+    if (schema === 'HDL') {
+      return (
+        <EventTracker
+          eventCategory="Literature detail"
+          eventAction="Link"
+          eventId="HDL link"
+        >
+          <LinkWithTargetBlank href={`//hdl.handle.net/${value}`}>
+            {value}
+          </LinkWithTargetBlank>
+        </EventTracker>
+      );
+    } else {
+      return <span>{value}</span>;
+    }
+  };
+
+  return (
+    <InlineList
+      label="URN/HDL"
+      items={identifiers}
+      extractKey={(identifier: Map<string, string>) => identifier.get('value')}
+      renderItem={renderIdentifier}
+    />
+  );
+};
+
+export default PersistentIdentifiers;
---
+++ ui/src/literature/components/__tests__/PersistentIdentifiers.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { fromJS } from 'immutable';
+
+import PersistentIdentifiers from '../PersistentIdentifiers';
+
+describe('PersistentIdentifiers', () => {
+  it('renders with identifiers', () => {
+    const identifiers = fromJS([
+      {
+        value: "1866/20706",
+        schema: "HDL"
+      },
+      {
+        value: "12345",
+        schema: "URN"
+      }
+    ]);
+    const wrapper = shallow(<PersistentIdentifiers identifiers={identifiers} />);
+    expect(wrapper.dive()).toMatchSnapshot();
+  });
+});
