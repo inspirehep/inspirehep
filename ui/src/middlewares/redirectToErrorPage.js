9e09eef064da2199b93145858808393f835b5ff7
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2023-02-10 14:39:17 +0100

    ui: redirect to 500 page on serializer error

--- ui/src/middlewares/redirectToErrorPage.js
+++ ui/src/middlewares/redirectToErrorPage.js
@@ -9,6 +9,9 @@ export default function() {
       // INFO: 'push' and 'replace' methods from 'connected-react-router' 
       // remove error causing url from history. To be able to keep in 
       // and retrieve it from history in error pages we use window API
+      if (error.status === 400) {
+        window.location.assign(`${ERRORS}/500`);
+      }
       window.location.assign(`${ERRORS}/${error.status}`);
     }
     return next(action);
