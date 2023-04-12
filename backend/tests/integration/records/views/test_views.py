ae0291658f79e8a46f2ad4ff389db537454bd6fc
MJedr <jedrych.marcysia@gmail.com>
2023-04-12 10:37:20 +0200

    fix types in reference self curation

--- backend/tests/integration/records/views/test_views.py
+++ backend/tests/integration/records/views/test_views.py
@@ -5,6 +5,7 @@
 # inspirehep is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
+import mock
 import orjson
 from helpers.providers.faker import faker
 from helpers.utils import (
@@ -443,3 +444,57 @@ def test_self_curation_returns_401_for_not_authenticated_user(inspire_app):
         )
 
     assert response.status_code == 401
+
+
+@mock.patch("inspirehep.records.views.reference_self_curation_task")
+def test_reference_self_curation_task_is_called_with_proper_args(
+    mock_self_curation_task, inspire_app
+):
+    user = create_user()
+    literature_data = {
+        "references": [
+            {
+                "reference": {
+                    "dois": ["10.1103/PhysRev.92.649"],
+                    "misc": ["The 7.68MeV state in 12C"],
+                    "label": "31",
+                    "authors": [
+                        {"full_name": "Dunbar, D.N.F."},
+                        {"full_name": "Pixley, R.E."},
+                        {"full_name": "Wenzel, W.A."},
+                        {"full_name": "Whaling, W."},
+                    ],
+                    "publication_info": {
+                        "year": 1953,
+                        "page_end": "650",
+                        "page_start": "649",
+                        "journal_title": "Phys.Rev.",
+                        "journal_volume": "92",
+                    },
+                }
+            },
+        ]
+    }
+
+    record = create_record("lit", data=literature_data)
+
+    data = {
+        "record_id": str(record.id),
+        "revision_id": int(record.revision_id),
+        "reference_index": 0,
+        "new_reference_recid": 1,
+    }
+
+    with inspire_app.test_client() as client:
+        login_user_via_session(client, email=user.email)
+        client.post(
+            "/literature/reference-self-curation",
+            content_type="application/json",
+            data=orjson.dumps(data),
+        )
+    assert mock_self_curation_task.mock_calls[0][1] == (
+        str(record.id),
+        int(record.revision_id),
+        0,
+        1,
+    )
