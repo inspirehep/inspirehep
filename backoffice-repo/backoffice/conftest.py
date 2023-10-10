f7c620d5f1cd0f7a65550a49620915d1a65cd31c
MJedr <jedrych.marcysia@gmail.com>
2023-10-10 16:05:01 +0200

    init project

---
+++ backoffice-repo/backoffice/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from backoffice.users.models import User
+from backoffice.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
