66b9243e6faae45d12d41c440ca97af823c6355e
MJedr <jedrych.marcysia@gmail.com>
2023-11-02 16:22:28 +0100

    update login url

--- backoffice-repo/config/settings/base.py
+++ backoffice-repo/config/settings/base.py
@@ -120,7 +120,7 @@ AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
 LOGIN_REDIRECT_URL = "users:redirect"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
-LOGIN_URL = "/admin/login/"
+LOGIN_URL = "account_login"
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
