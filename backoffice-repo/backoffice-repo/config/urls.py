93522fbfb9dd921d751421ee92032fded46c7cd9
MJedr <jedrych.marcysia@gmail.com>
2023-11-07 15:38:14 +0100

    add social django

--- backoffice-repo/backoffice-repo/config/urls.py
+++ backoffice-repo/backoffice-repo/config/urls.py
@@ -7,6 +7,7 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
+from social_django.urls import urlpatterns as social_django_urls
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -14,6 +15,7 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     path("oauth2/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path("auth/", include(social_django_urls, namespace="social")),
     # User management
     path("users/", include("backoffice.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
