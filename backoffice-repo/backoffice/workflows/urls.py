d19fe94bd16c139b95085c5fc9d20595a5c57d43
MJedr <jedrych.marcysia@gmail.com>
2023-11-10 17:12:01 +0100

    add workflows to admin

--- backoffice-repo/backoffice/workflows/urls.py
+++ backoffice-repo/backoffice/workflows/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("workflows/", include("workflows.urls")),
-    path("admin/", admin.site.urls),
 ]
