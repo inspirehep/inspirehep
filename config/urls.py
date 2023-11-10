514f1da29e25238bf0c637f9febb458a52c85a92
MJedr <jedrych.marcysia@gmail.com>
2023-11-10 17:12:01 +0100

    add workflows to admin

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,10 @@ from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
+from backoffice.workflows.admin import workflow_admin_site
+
 urlpatterns = [
+    path("dashboard/", workflow_admin_site.urls),
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
