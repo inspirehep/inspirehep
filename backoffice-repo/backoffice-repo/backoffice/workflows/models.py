d32241d0c816d60d3d9da0eecdbd9cc30c92e756
MJedr <jedrych.marcysia@gmail.com>
2023-11-20 15:18:09 +0100

    update workflow models - add ticket-workflow table - add workflow type field to model

--- backoffice-repo/backoffice-repo/backoffice/workflows/models.py
+++ backoffice-repo/backoffice-repo/backoffice/workflows/models.py
@@ -4,21 +4,37 @@ from django.db import models
 
 
 class Workflow(models.Model):
-    PREPROCESSING = "PREPROCESSING"
-    APPROVAL = "APPROVAL"
-    POSTPROCESSING = "POSTPROCESSING"
+    DEFAULT_STATUS_CHOICE = "PREPROCESSING"
+    DEFAULT_WORKFLOW_TYPE = "HEP_create"
     STATUS_CHOICES = (
-        (PREPROCESSING, "Preprocessing"),
-        (APPROVAL, "Approval"),
-        (POSTPROCESSING, "Postprocessing"),
+        ("PREPROCESSING", "Preprocessing"),
+        ("APPROVAL", "Approval"),
+        ("POSTPROCESSING", "Postprocessing"),
+    )
+
+    WORKFLOW_TYPES = (
+        ("HEP_CREATE", "HEP create"),
+        ("HEP_UPDATE", "HEP update"),
+        ("AUTHOR_CREATE", "Author create"),
+        ("AUTHOR_UPDATE", "Author update"),
     )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    workflow_type = models.CharField(
+        max_length=30,
+        choices=WORKFLOW_TYPES,
+        default=DEFAULT_WORKFLOW_TYPE,
+    )
     data = models.JSONField()
     status = models.CharField(
         max_length=30,
         choices=STATUS_CHOICES,
-        default=PREPROCESSING,
+        default=DEFAULT_STATUS_CHOICE,
     )
     core = models.BooleanField()
     is_update = models.BooleanField()
+
+
+class WorkflowTicket(models.Model):
+    workflow_id = models.ForeignKey(Workflow, on_delete=models.CASCADE)
+    ticket_id = models.CharField(max_length=32)  # in SNOW it's GUID
