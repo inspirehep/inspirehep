d6111ea08845ed64526bd4dc03df432b34b5734b
MJedr <jedrych.marcysia@gmail.com>
2023-12-03 22:57:12 +0100

    update workflow models * add ticket type to workflow_status table * modify workflow statuses

--- backoffice/workflows/api/serializers.py
+++ backoffice/workflows/api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from backoffice.workflows.models import Workflow
+from backoffice.workflows.models import Workflow, WorkflowTicket
 
 
 class WorkflowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workflow
+        fields = "__all__"
+
+
+class WorkflowTicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkflowTicket
         fields = "__all__"
