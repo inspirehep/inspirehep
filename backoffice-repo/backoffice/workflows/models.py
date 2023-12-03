0fa2612b7bc5cbd2619dfac8e3e137d2c6c2805a
MJedr <jedrych.marcysia@gmail.com>
2023-12-03 22:57:12 +0100

    update workflow models * add ticket type to workflow_status table * modify workflow statuses

--- backoffice-repo/backoffice/workflows/models.py
+++ backoffice-repo/backoffice/workflows/models.py
@@ -2,22 +2,17 @@ import uuid
 
 from django.db import models
 
+from .constants import (
+    DEFAULT_STATUS_CHOICE,
+    DEFAULT_TICKET_TYPE,
+    DEFAULT_WORKFLOW_TYPE,
+    STATUS_CHOICES,
+    TICKET_TYPES,
+    WORKFLOW_TYPES,
+)
+
 
 class Workflow(models.Model):
-    DEFAULT_STATUS_CHOICE = "PREPROCESSING"
-    DEFAULT_WORKFLOW_TYPE = "HEP_create"
-    STATUS_CHOICES = (
-        ("PREPROCESSING", "Preprocessing"),
-        ("APPROVAL", "Approval"),
-        ("POSTPROCESSING", "Postprocessing"),
-    )
-
-    WORKFLOW_TYPES = (
-        ("HEP_CREATE", "HEP create"),
-        ("HEP_UPDATE", "HEP update"),
-        ("AUTHOR_CREATE", "Author create"),
-        ("AUTHOR_UPDATE", "Author update"),
-    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     workflow_type = models.CharField(
@@ -38,3 +33,4 @@ class Workflow(models.Model):
 class WorkflowTicket(models.Model):
     workflow_id = models.ForeignKey(Workflow, on_delete=models.CASCADE)
     ticket_id = models.CharField(max_length=32)  # in SNOW it's GUID
+    ticket_type = models.CharField(max_length=30, choices=TICKET_TYPES, default=DEFAULT_TICKET_TYPE)
