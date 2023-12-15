a334330c6e0ef2c6c193b4723cbe9c8df5a645d5
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2023-12-15 11:54:07 +0100

    test

--- e2e/cypress/e2e/claiming.test.js
+++ e2e/cypress/e2e/claiming.test.js
@@ -521,7 +521,6 @@ describe('Author collection', () => {
           cy.get('.ant-notification-notice-description').should(
             'have.text',
             'This paper cannot be claimed automatically. Please contact us at authors@inspirehep.net',
-            'Test'
           );
         });
       });
