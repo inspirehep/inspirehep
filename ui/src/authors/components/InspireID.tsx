d08dc7adf0efadc06123bb25e4764a29304e7e64
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2024-01-16 13:10:13 +0100

    ui: add INSPIRE ID
    * ref: cern-sis/issues-inspire#431

---
+++ ui/src/authors/components/InspireID.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InspireID = ({ id }: { id: string }) => {
+  return (
+    <div>
+      <span>INSPIRE ID: {id}</span>
+    </div>
+  );
+};
+
+export default InspireID;
