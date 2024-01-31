9535fd427c1c1715809fd4032a3d67714cdca6ce
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2024-01-31 12:13:46 +0100

    ui: linkify URN identifiers
    * ref: cern-sis/issues-inspire/issues#435

--- ui/src/literature/components/PersistentIdentifiers.tsx
+++ ui/src/literature/components/PersistentIdentifiers.tsx
@@ -1,34 +1,49 @@
-import { List, Map } from 'immutable';
 import React from 'react';
+import { List, Map } from 'immutable';
+
 import EventTracker from '../../common/components/EventTracker';
 import InlineDataList from '../../common/components/InlineList';
 import LinkWithTargetBlank from '../../common/components/LinkWithTargetBlank';
+
+const renderIdentifier = (identifier: Map<string, string>) => {
+  const schema = identifier.get('schema');
+  const value = identifier.get('value');
+  let eventId = '';
+  let linkHref = '';
+
+  switch (schema) {
+    case 'HDL':
+      eventId = 'HDL link';
+      linkHref = `//hdl.handle.net/${value}`;
+      break;
+    case 'URN':
+      eventId = 'URN link';
+      linkHref = `https://nbn-resolving.org//${value}`;
+      break;
+    default:
+      break;
+  }
+
+  if (schema === 'HDL' || schema === 'URN') {
+    return (
+      <EventTracker
+        eventCategory="Literature detail"
+        eventAction="Link"
+        eventId={eventId}
+      >
+        <LinkWithTargetBlank href={linkHref}>{value}</LinkWithTargetBlank>
+      </EventTracker>
+    );
+  }
+
+  return <span>{value}</span>;
+};
 
 const PersistentIdentifiers = ({
   identifiers,
 }: {
   identifiers: List<string>;
 }) => {
-  const renderIdentifier = (identifier: Map<string, string>) => {
-    const schema = identifier.get('schema');
-    const value = identifier.get('value');
-
-    if (schema === 'HDL') {
-      return (
-        <EventTracker
-          eventCategory="Literature detail"
-          eventAction="Link"
-          eventId="HDL link"
-        >
-          <LinkWithTargetBlank href={`//hdl.handle.net/${value}`}>
-            {value}
-          </LinkWithTargetBlank>
-        </EventTracker>
-      );
-    } 
-    return <span>{value}</span>;
-  };
-
   return (
     <InlineDataList
       label="URN/HDL"
