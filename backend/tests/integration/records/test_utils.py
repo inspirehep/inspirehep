d85ddcf93266c4bbbdd766ddbe6a2311f80e166b
Karolina Siemieniuk-Morawska <55505399+karolina-siemieniuk-morawska@users.noreply.github.com>
2024-03-04 13:55:59 +0000

    wip

--- backend/tests/integration/records/test_utils.py
+++ backend/tests/integration/records/test_utils.py
@@ -99,7 +99,7 @@ def test_get_parent_records(inspire_app):
     }
     rec = create_record("lit", data=data)
     extracted_parent_records = get_parent_records(rec)
-    assert extracted_parent_records == sorted(list(parent_record_1, parent_record_2), key=itemgetter("control_number"))
+    assert extracted_parent_records == sorted(list((parent_record_1, parent_record_2)), key=itemgetter("control_number"))
 
 
 def test_get_parent_record(inspire_app):
