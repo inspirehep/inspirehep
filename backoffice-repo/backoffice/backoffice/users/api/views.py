d79ab1f4ccfa79d6e65de5212bf3ac4c88b4f858
Harris Tzovanakis <me@drjova.com>
2024-07-26 09:55:43 +0200

    bakcoffice: fix tests path

--- backoffice-repo/backoffice/backoffice/users/api/views.py
+++ backoffice-repo/backoffice/backoffice/users/api/views.py
@@ -4,7 +4,8 @@ from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from users.api.serializers import UserSerializer
+
+from backoffice.users.api.serializers import UserSerializer
 
 User = get_user_model()
 
