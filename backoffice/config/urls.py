ee49892532302ac3f6792680f1423367f5d1cb0a
DonHaul <ramiro.animus@gmail.com>
2024-08-08 10:54:45 +0200

    sentry: package version downgraded to 1.19.1
    * ref: cern-sis/issues-inspire/issues/512

--- backoffice/config/urls.py
+++ backoffice/config/urls.py
@@ -43,10 +43,6 @@ urlpatterns += [
 ]
 
 
-def sentry_trigger_error(request):
-    division_by_zero = 1 / 0  # noqa
-
-
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
@@ -67,7 +63,6 @@ if settings.DEBUG:
             kwargs={"exception": Exception("Page not Found")},
         ),
         path("500/", default_views.server_error),
-        path("sentry-debug/", sentry_trigger_error),
     ]
 
     if "debug_toolbar" in settings.INSTALLED_APPS:
