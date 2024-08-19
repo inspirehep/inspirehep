c2ac0875cf9312c24d75b99a65caa0d3be1ce724
karolina-siemieniuk-morawska <55505399+karolina-siemieniuk-morawska@users.noreply.github.com>
2024-08-19 12:13:01 +0200

    ui: fix wrong orcid source
    * ref: cern-sis/issues-inspire#541

--- ui/src/holdingpen-new/components/Links.tsx
+++ ui/src/holdingpen-new/components/Links.tsx
@@ -13,58 +13,74 @@ interface LinksProps {
   ids: Map<string, any>;
 }
 
-const Links: React.FC<LinksProps> = ({ urls, ids }) => {
-  function getLinkData(schema: string, value: string) {
-    switch (schema) {
-      case 'LINKEDIN':
-        return {
-          href: `https://www.linkedin.com/in/${value}`,
-          icon: <LinkedinOutlined />,
-        };
-      case 'TWITTER':
-        return {
-          href: `https://twitter.com/${value}`,
-          icon: <TwitterOutlined />,
-        };
-      case 'ORCID':
-        return {
-          href: `https://orcid.org/my-orcid?orcid=${value}`,
-          icon: <img src={orcidLogo} alt="ORCID" width={16} height={16} />,
-        };
-      default:
-        return {
-          href: value,
-          icon: <LinkOutlined />,
-        };
-    }
+function getLinkData(schema: string, value: string) {
+  switch (schema) {
+    case 'LINKEDIN':
+      return {
+        href: `https://www.linkedin.com/in/${value}`,
+        icon: <LinkedinOutlined />,
+      };
+    case 'TWITTER':
+      return {
+        href: `https://twitter.com/${value}`,
+        icon: <TwitterOutlined />,
+      };
+    case 'ORCID':
+      return {
+        href: `https://orcid.org/my-orcid?orcid=${value}`,
+        icon: <img src={orcidLogo} alt="ORCID" width={16} height={16} />,
+      };
+    default:
+      return {
+        href: value,
+        icon: <LinkOutlined />,
+      };
   }
+}
+
+export const Ids: React.FC<{ ids: Map<string, any>; noIcon?: boolean }> = ({
+  ids,
+  noIcon = false,
+}) => (
+  <>
+    {ids?.map((link: Map<string, any>) => (
+      <p key={link?.get('value')} className={noIcon ? 'mb0' : ''}>
+        {!noIcon && getLinkData(link?.get('schema'), link?.get('value'))?.icon}
+        {link?.get('schema') && (
+          <b className="dib ml1 ttc">{link?.get('schema').toLowerCase()}:</b>
+        )}{' '}
+        <a
+          href={getLinkData(link?.get('schema'), link?.get('value'))?.href}
+          target="_blank"
+        >
+          {link?.get('value')}
+        </a>
+      </p>
+    ))}
+  </>
+);
+
+const Urls: React.FC<{ urls: Map<string, any> }> = ({ urls }) => (
+  <>
+    {urls?.map((link: Map<string, any>) => (
+      <p key={link?.get('value')}>
+        {getLinkData(link?.get('schema'), link?.get('value'))?.icon}
+        {link?.get('description') && (
+          <b className="dib ml1 ttc">{link?.get('description')}:</b>
+        )}{' '}
+        <a href={link?.get('value')} target="_blank">
+          {link?.get('value')}
+        </a>
+      </p>
+    ))}
+  </>
+);
+
+const Links: React.FC<LinksProps> = ({ urls, ids }) => {
   return (
     <>
-      {ids?.map((link: Map<string, any>) => (
-        <p key={link?.get('value')}>
-          {getLinkData(link?.get('schema'), link?.get('value'))?.icon}
-          {link?.get('schema') && (
-            <b className="dib ml1 ttc">{link?.get('schema').toLowerCase()}:</b>
-          )}{' '}
-          <a
-            href={getLinkData(link?.get('schema'), link?.get('value'))?.href}
-            target="_blank"
-          >
-            {link?.get('value')}
-          </a>
-        </p>
-      ))}
-      {urls?.map((link: Map<string, any>) => (
-        <p key={link?.get('value')}>
-          {getLinkData(link?.get('schema'), link?.get('value'))?.icon}
-          {link?.get('description') && (
-            <b className="dib ml1 ttc">{link?.get('description')}:</b>
-          )}{' '}
-          <a href={link?.get('value')} target="_blank">
-            {link?.get('value')}
-          </a>
-        </p>
-      ))}
+      <Ids ids={ids} />
+      <Urls urls={urls} />
     </>
   );
 };
