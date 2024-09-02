1fcb5a2334d1f166ef11744c9938f4ae683a8274
DonHaul <ramiro.animus@gmail.com>
2024-09-02 17:35:04 +0200

    fixing github workflows

--- backoffice-repo/workflows/plugins/hooks/tenacity_config.py
+++
@@ -1,9 +0,0 @@
-import tenacity
-
-
-def tenacity_retry_kwargs():
-    return {
-        "wait": tenacity.wait_exponential(),
-        "stop": tenacity.stop_after_attempt(5),
-        "retry": tenacity.retry_if_exception_type(Exception),
-    }
