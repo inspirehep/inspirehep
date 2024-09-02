b3553e1628c41fb70b65e5247924b7767da63ea6
DonHaul <ramiro.animus@gmail.com>
2024-09-02 17:48:50 +0200

    wow

--- backoffice-repo/backoffice-repo/backoffice/backoffice/management/groups.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth.models import Group
-
-admin_group, created = Group.objects.get_or_create(name="admin")
-curator_group, created = Group.objects.get_or_create(name="curator")
--- backoffice-repo/backoffice-repo/workflows/tests/test_dags_integrity.py
+++
@@ -1,6 +0,0 @@
-from airflow.models import DagBag
-
-
-def test_dagbag():
-    dag_bag = DagBag(include_examples=False)
-    assert not dag_bag.import_errors
