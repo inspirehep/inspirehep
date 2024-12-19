348744d01d1a739228b05ef305cf73525a0b031b
DonHaul <ramiro.animus@gmail.com>
2024-12-19 16:55:45 +0100

    workflows: add testing to wf callback failure
    * ref: cern-sis/issues-inspire/issues/643

--- workflows/tests/test_dags_integrity.py
+++ workflows/tests/test_dags_integrity.py
@@ -1,6 +1,17 @@
 from airflow.models import DagBag
 
 
-def test_dagbag():
+class TestDagsIntegrity:
     dag_bag = DagBag(include_examples=False)
-    assert not dag_bag.import_errors
+
+    def test_dagbag(self):
+        assert not self.dag_bag.import_errors
+
+    def test_on_failure_callback(self):
+        for dag_id, dag in self.dag_bag.dags.items():
+            if (
+                dag.__dict__.get("on_failure_callback")
+                and dag.__dict__.get("on_failure_callback").__name__
+                == "set_workflow_status_to_error"
+            ):
+                assert "collection" in dag.__dict__["params"], dag_id
