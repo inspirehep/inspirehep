fb441d9291e2dd19aaeef4775247e5311a31fc37
Harris Tzovanakis <me@drjova.com>
2025-01-21 14:28:37 +0100

    ui: fix citation count in data

--- ui/src/data/components/__tests__/DataItem.test.jsx
+++ ui/src/data/components/__tests__/DataItem.test.jsx
@@ -57,6 +57,59 @@ describe('DataItem', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
+  it('renders with `0` citation count', () => {
+    const metadata = fromJS({
+      titles: [
+        {
+          source: 'pariatur adipisicing amet',
+          subtitle: 'voluptate eiusmod fugiat',
+          title: 'Test title',
+        },
+      ],
+      authors: [
+        {
+          affiliations: [
+            {
+              curated_relation: true,
+              record: {
+                $ref: 'http://M1/api/institutions/12346',
+              },
+              value: 'ut',
+            },
+          ],
+        },
+      ],
+      urls: [{ value: 'http://url.com' }],
+      dois: [
+        {
+          source: 'in ad et',
+          value: '10.8756/tTM',
+          material: 'data',
+        },
+        {
+          source: 'mollit deserunt eu',
+          value: '10.5/.Aww=bT@',
+          material: 'version',
+        },
+        {
+          source: 'adipisicing et',
+          value: '10.0.9747720/#}O=W:$',
+          material: 'part',
+        },
+      ],
+      control_number: 1234,
+      citation_count: 0
+    });
+
+    const { asFragment } = render(
+      <MemoryRouter initialEntries={['/data']} initialIndex={0}>
+        <DataItem metadata={metadata} />
+      </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+
   it('renders with only needed props', () => {
     const metadata = fromJS({
       titles: [
