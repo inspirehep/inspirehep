88fbda0aef5ab32439b58ba2c1b14387c378f4ab
DonHaul <ramiro.animus@gmail.com>
2025-01-23 13:22:43 +0100

    ui: fix style AuthorMainInfo
    * ref: cern-sis/issues-inspire/issues/697

--- ui/src/backoffice/common/components/Links/Links.tsx
+++ ui/src/backoffice/common/components/Links/Links.tsx
@@ -1,38 +1,36 @@
 import React from 'react';
 import { Map } from 'immutable';
-import { CopyOutlined, LinkOutlined, LinkedinOutlined, XOutlined } from '@ant-design/icons';
+import {
+  CopyOutlined,
+  LinkOutlined,
+  LinkedinOutlined,
+  XOutlined,
+} from '@ant-design/icons';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import orcidLogo from '../../../../common/assets/orcid.svg';
 
-type LinksProps  = {
+type LinksProps = {
   urls: Map<string, any>;
   ids: Map<string, any>;
-}
-
+};
 
 type IdsProps = {
   ids: Map<string, any>;
   noIcon?: boolean;
-}
+};
 
 type UrlsProps = {
   urls: Map<string, any>;
-}
-
-type SocialPlatformMap = {
-  [key: string]: string;
 };
 
-const SocialPlatformMap = {
-  ORCID: "ORCID",
-  MASTODON: "Mastodon",
-  LINKEDIN: "LinkedIn",
-  BLUESKY: "Bluesky",
-  TWITTER: "Twitter",
-};
+type SocialPlatformMap = Record<string, string>;
 
-const getPlatformDisplayName = (key: string): string => {
-  return SocialPlatformMap[key] || key;
+const socialPlatformMap: SocialPlatformMap = {
+  ORCID: 'ORCID',
+  MASTODON: 'Mastodon',
+  LINKEDIN: 'LinkedIn',
+  BLUESKY: 'Bluesky',
+  TWITTER: 'Twitter',
 };
 
 function getLinkData(schema: string, value: string) {
@@ -81,33 +79,35 @@ function getLinkData(schema: string, value: string) {
   }
 }
 
-export const Ids = ({
-  ids,
-  noIcon = false,
-}: IdsProps) => (
+export const Ids = ({ ids, noIcon = false }: IdsProps) => (
   <>
     {ids?.map((link: Map<string, any>) => {
-        const schema = link?.get('schema');
-        const value = link?.get('value');
-        const linkData = getLinkData(schema, value);
-        const icon = linkData?.icon;
-        const href = linkData?.href;
-        const showCopyBtn = linkData?.show_copy_btn;
+      const schema = link?.get('schema');
+      const value = link?.get('value');
+      const linkData = getLinkData(schema, value);
+      const icon = linkData?.icon;
+      const href = linkData?.href;
+      const showCopyBtn = linkData?.show_copy_btn;
 
-        return (
-          <p key={value} className={noIcon ? 'mb0' : ''}>
-            {!noIcon && icon}
-            {schema && (
-              <b className="dib ttc">{getPlatformDisplayName(schema)}:</b>
-            )}{' '}
-            <a href={href} target="_blank"> {value} </a>
-            {showCopyBtn && (
-              <CopyToClipboard text={value}>
-                <span className="ml1 pointer"><CopyOutlined /></span>
-              </CopyToClipboard>
-            )}
-          </p>
-        );
+      return (
+        <p key={value} className={noIcon ? 'mb0' : ''}>
+          {!noIcon && icon}
+          {schema && (
+            <b className="dib ttc">{socialPlatformMap[schema] || schema}:</b>
+          )}{' '}
+          <a href={href} target="_blank">
+            {' '}
+            {value}{' '}
+          </a>
+          {showCopyBtn && (
+            <CopyToClipboard text={value}>
+              <span className="ml1 pointer">
+                <CopyOutlined />
+              </span>
+            </CopyToClipboard>
+          )}
+        </p>
+      );
     })}
   </>
 );
@@ -122,9 +122,7 @@ export const Urls = ({ urls }: UrlsProps) => (
       return (
         <p key={value}>
           {linkData?.icon}
-          {description && (
-            <b className="dib ml1 ttc">{description}:</b>
-          )}{' '}
+          {description && <b className="dib ml1 ttc">{description}:</b>}{' '}
           <a href={value} target="_blank">
             {value}
           </a>
