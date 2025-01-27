3dc4715c423f9a2c53e4aeae74d9abe594c2bda2
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-01-27 17:26:27 +0100

    ui: hide data option from the main dropdown

--- ui/src/common/components/SearchBoxNamespaceSelect.tsx
+++ ui/src/common/components/SearchBoxNamespaceSelect.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 
 import SelectBox from './SelectBox';
-import { SEARCH_BOX_NAMESPACES } from '../../search/constants';
-
-const SCOPE_OPTIONS = SEARCH_BOX_NAMESPACES.map((value) => ({ value }));
+import { DATA_NS, SEARCH_BOX_NAMESPACES } from '../../search/constants';
 
 const SearchBoxNamespaceSelect = ({
+  canAccessDataCollection,
   searchScopeName,
   onSearchScopeChange,
 }: {
+  canAccessDataCollection: boolean;
   searchScopeName: string;
   onSearchScopeChange: Function;
-}) => (
-  <SelectBox
-    popupClassName="header-dropdown"
-    onChange={onSearchScopeChange}
-    value={searchScopeName}
-    options={SCOPE_OPTIONS}
-  />
-);
+}) => {
+  const filteredNamespaces = canAccessDataCollection
+    ? SEARCH_BOX_NAMESPACES
+    : SEARCH_BOX_NAMESPACES.filter((value) => value !== DATA_NS);
+  const SCOPE_OPTIONS = filteredNamespaces.map((value) => ({ value }));
+
+  return (
+    <SelectBox
+      popupClassName="header-dropdown"
+      onChange={onSearchScopeChange}
+      value={searchScopeName}
+      options={SCOPE_OPTIONS}
+    />
+  );
+};
 
 export default SearchBoxNamespaceSelect;
--- ui/src/common/containers/SearchBoxNamespaceSelectContainer.tsx
+++ ui/src/common/containers/SearchBoxNamespaceSelectContainer.tsx
@@ -3,10 +3,16 @@ import { Action, ActionCreator } from 'redux';
 
 import SearchBoxNamespaceSelect from '../components/SearchBoxNamespaceSelect';
 import { changeSearchBoxNamespace } from '../../actions/search';
+import { isCataloger, isSuperUser } from '../authorization';
 
-const stateToProps = (state: RootStateOrAny) => ({
-  searchScopeName: state.search.get('searchBoxNamespace'),
-});
+const stateToProps = (state: RootStateOrAny) => {
+  const isCatalogerLoggedIn = isCataloger(state.user.getIn(['data', 'roles']));
+  const isSuperUserLoggedIn = isSuperUser(state.user.getIn(['data', 'roles']));
+  return {
+    searchScopeName: state.search.get('searchBoxNamespace'),
+    canAccessDataCollection: isCatalogerLoggedIn || isSuperUserLoggedIn,
+  };
+};
 
 export const dispatchToProps = (dispatch: ActionCreator<Action>) => ({
   onSearchScopeChange(scope: string) {
