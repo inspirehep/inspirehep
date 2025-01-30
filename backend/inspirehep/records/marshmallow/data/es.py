5f4f2bb0dfd01aa6cad93418361528884f3890c0
PascalEgn <pascal.egner@googlemail.com>
2025-01-30 11:19:34 +0100

    backend: add data facet for author agg

--- backend/inspirehep/records/marshmallow/data/es.py
+++ backend/inspirehep/records/marshmallow/data/es.py
@@ -8,15 +8,22 @@ from inspire_dojson.utils import get_recid_from_ref
 from inspire_utils.record import get_value
 from marshmallow import fields
 
+from inspirehep.pidstore.api.base import PidStoreBase
+from inspirehep.records.api.base import InspireRecord
 from inspirehep.records.marshmallow.base import ElasticSearchBaseSchema
 from inspirehep.records.marshmallow.data.base import DataRawSchema
 from inspirehep.records.marshmallow.fields.list_with_limit import ListWithLimit
 from inspirehep.records.marshmallow.literature.common.author import AuthorSchemaV1
+from inspirehep.records.marshmallow.utils import (
+    get_display_name_for_author_name,
+    get_facet_author_name_for_author,
+)
 from inspirehep.search.api import LiteratureSearch
 
 
 class DataElasticSearchSchema(ElasticSearchBaseSchema, DataRawSchema):
     authors = ListWithLimit(fields.Nested(AuthorSchemaV1, dump_only=True), limit=10)
+    facet_author_name = fields.Method("get_facet_author_name")
 
     def fetch_authors_from_literature(self, data):
         literature = get_value(data, "literature")
@@ -34,6 +41,39 @@ class DataElasticSearchSchema(ElasticSearchBaseSchema, DataRawSchema):
             return None
 
         return literature_record[0].to_dict().get("authors")
+
+    def get_facet_author_name(self, record):
+        """Prepare record for ``facet_author_name`` field."""
+        authors_with_record = list(
+            InspireRecord.get_linked_records_from_dict_field(record, "authors.record")
+        )
+        found_authors_control_numbers = set(
+            [
+                author["control_number"]
+                for author in authors_with_record
+                if author.get("control_number")
+            ]
+        )
+        authors_without_record = [
+            author
+            for author in record.get("authors", [])
+            if "record" not in author
+            or int(
+                PidStoreBase.get_pid_from_record_uri(author["record"].get("$ref"))[1]
+            )
+            not in found_authors_control_numbers
+        ]
+        result = []
+
+        for author in authors_with_record:
+            result.append(get_facet_author_name_for_author(author))
+
+        for author in authors_without_record:
+            result.append(
+                "NOREC_{}".format(get_display_name_for_author_name(author["full_name"]))
+            )
+
+        return result
 
     def dump(self, obj, *args, **kwargs):
         if not obj.get("authors"):
