6c6c613b01c59a2acb1b24b25d73d4fe651a5292
DonHaul <ramiro.animus@gmail.com>
2025-02-11 16:10:15 +0100

    workflows[data]: add a yearly data harvest
    * ref: cern-sis/issues-inspire/issues/760

--- workflows/dags/data/bulk_data_harvest.py
+++ workflows/dags/data/bulk_data_harvest.py
@@ -1,7 +1,7 @@
 import datetime
 import logging
 
-from airflow.decorators import dag, task, task_group
+from airflow.decorators import dag, task
 from airflow.exceptions import AirflowException
 from airflow.macros import ds_add
 from airflow.models import Variable
@@ -12,21 +12,23 @@ from hooks.inspirehep.inspire_http_record_management_hook import (
     InspireHTTPRecordManagementHook,
 )
 from inspire_utils.dedupers import dedupe_list
+from tenacity import RetryError
 
 logger = logging.getLogger(__name__)
 
 
 @dag(
-    start_date=datetime.datetime(2024, 11, 28),
-    schedule="@daily",
+    start_date=datetime.datetime(2021, 1, 1),
+    schedule=None,
     catchup=False,
+    max_active_runs=5,
     tags=["data"],
     params={
         "last_updated_from": Param(type=["null", "string"], default=""),
         "last_updated_to": Param(type=["null", "string"], default=""),
     },
 )
-def data_harvest_dag():
+def bulk_data_harvest_dag():
     """Defines the DAG for the HEPData harvest workflow.
 
     Tasks:
@@ -52,40 +54,37 @@ def data_harvest_dag():
         from_date = (
             context["params"]["last_updated_from"]
             if context["params"]["last_updated_from"]
-            else ds_add(context["ds"], -1)
+            else ds_add(context["ds"], -366)
         )
-        to_date = context["params"]["last_updated_to"]
+        to_date = (
+            context["params"]["last_updated_to"]
+            if context["params"]["last_updated_from"]
+            else context["ds"]
+        )
 
         payload = {"inspire_ids": True, "last_updated": from_date, "sort_by": "latest"}
         hepdata_response = generic_http_hook.call_api(
             endpoint="/search/ids", method="GET", params=payload
         )
-        if to_date:
-            payload = {
-                "inspire_ids": True,
-                "last_updated": to_date,
-                "sort_by": "latest",
-            }
-            hepdata_to_response = generic_http_hook.call_api(
-                endpoint="/search/ids", method="GET", params=payload
-            )
-            return list(set(hepdata_response.json()) - set(hepdata_to_response.json()))
+        payload = {
+            "inspire_ids": True,
+            "last_updated": to_date,
+            "sort_by": "latest",
+        }
+        hepdata_to_response = generic_http_hook.call_api(
+            endpoint="/search/ids", method="GET", params=payload
+        )
+        return list(set(hepdata_response.json()) - set(hepdata_to_response.json()))
 
-        return hepdata_response.json()
+    @task
+    def download_record_versions(ids):
+        """Download the versions of the record.
 
-    @task_group
-    def process_record(record_id):
-        """Process the record by downloading the versions,
-        building the record and loading it to inspirehep.
+        Args: id (int): The id of the record.
+        Returns: dict: The record versions.
         """
-
-        @task(max_active_tis_per_dag=5)
-        def download_record_versions(id):
-            """Download the versions of the record.
-
-            Args: id (int): The id of the record.
-            Returns: dict: The record versions.
-            """
+        payloads = []
+        for id in ids:
             hepdata_response = generic_http_hook.call_api(
                 endpoint=f"/record/ins{id}?format=json"
             )
@@ -98,50 +97,53 @@ def data_harvest_dag():
                 )
                 response.raise_for_status()
                 record[version] = response.json()
+            payloads.append(record)
 
-            return record
+        return payloads
 
-        @task.virtualenv(
-            requirements=["inspire-schemas==61.6.9"],
-            system_site_packages=False,
-        )
-        def build_record(data_schema, inspire_url, payload, **context):
-            """Build the record from the payload.
+    @task.virtualenv(
+        requirements=["inspire-schemas==61.6.9"],
+        system_site_packages=False,
+    )
+    def build_records(data_schema, inspire_url, payloads, **context):
+        """Build the record from the payload.
 
-            Args: data_schema (str): The schema of the data.
-                    payload (dict): The payload of the record.
+        Args: data_schema (str): The schema of the data.
+                payload (dict): The payload of the record.
 
-            Returns: dict: The built record.
-            """
-            import datetime
-            import re
+        Returns: dict: The built record.
+        """
+        import datetime
+        import re
 
-            from inspire_schemas.builders import DataBuilder
+        from inspire_schemas.builders import DataBuilder
 
-            def add_version_specific_dois(record, builder):
-                """Add dois to the record."""
-                for data_table in record["data_tables"]:
-                    builder.add_doi(data_table["doi"], material="part")
-                for resource_with_doi in record["resources_with_doi"]:
-                    builder.add_doi(resource_with_doi["doi"], material="part")
+        def add_version_specific_dois(record, builder):
+            """Add dois to the record."""
+            for data_table in record["data_tables"]:
+                builder.add_doi(data_table["doi"], material="part")
+            for resource_with_doi in record["resources_with_doi"]:
+                builder.add_doi(resource_with_doi["doi"], material="part")
 
-                builder.add_doi(record["record"]["hepdata_doi"], material="version")
+            builder.add_doi(record["record"]["hepdata_doi"], material="version")
 
-            def add_keywords(record, builder):
-                """Add keywords to the record."""
-                for keyword, item in record.get("data_keywords", {}).items():
-                    if keyword == "cmenergies":
-                        if len(item) >= 1 and "lte" in item[0] and "gte" in item[0]:
-                            builder.add_keyword(
-                                f"{keyword}: {item[0]['lte']}-{item[0]['gte']}"
-                            )
-                    elif keyword == "observables":
-                        for value in item:
-                            builder.add_keyword(f"observables: {value}")
-                    else:
-                        for value in item:
-                            builder.add_keyword(value)
+        def add_keywords(record, builder):
+            """Add keywords to the record."""
+            for keyword, item in record.get("data_keywords", {}).items():
+                if keyword == "cmenergies":
+                    if len(item) >= 1 and "lte" in item[0] and "gte" in item[0]:
+                        builder.add_keyword(
+                            f"{keyword}: {item[0]['lte']}-{item[0]['gte']}"
+                        )
+                elif keyword == "observables":
+                    for value in item:
+                        builder.add_keyword(f"observables: {value}")
+                else:
+                    for value in item:
+                        builder.add_keyword(value)
 
+        inspire_records = []
+        for payload in payloads:
             builder = DataBuilder(source="HEPData")
 
             builder.add_creation_date(datetime.datetime.now(datetime.UTC).isoformat())
@@ -196,20 +198,23 @@ def data_harvest_dag():
 
             data = builder.record
             data["$schema"] = data_schema
-            return data
 
-        @task
-        def normalize_collaborations(record):
-            """Normalize the collaborations in the record.
+            inspire_records.append(data)
+        return inspire_records
 
-            Args: record (dict): The record to normalize.
-            Returns: dict: The normalized record.
-            """
+    @task
+    def normalize_collaborations(records):
+        """Normalize the collaborations in the record.
 
+        Args: record (dict): The record to normalize.
+        Returns: dict: The normalized record.
+        """
+        normalized_records = []
+        for record in records:
             collaborations = record.get("collaborations", [])
 
             if not collaborations:
-                return record
+                continue
 
             response = inspire_http_hook.call_api(
                 endpoint="api/curation/literature/collaborations-normalization",
@@ -228,54 +233,70 @@ def data_harvest_dag():
                 )
                 record["collaborations"] = response.json()["normalized_collaborations"]
 
-            return record
+            normalized_records.append(record)
+        return normalized_records
 
-        @task
-        def load_record(new_record):
-            """Load the record to inspirehep.
+    @task
+    def load_records(new_records):
+        """Load the record to inspirehep.
 
-            Args: new_record (dict): The record to create or update in inspire
-            """
+        Args: new_record (dict): The record to create or update in inspire
+        """
 
+        responses = []
+
+        for new_record in new_records:
             try:
                 response = inspire_http_record_management_hook.get_record(
                     pid_type="doi", control_number=new_record["dois"][0]["value"]
                 )
             except AirflowException:
-                logger.info("Creating Record")
-                post_response = inspire_http_record_management_hook.post_record(
-                    data=new_record, pid_type="data"
-                )
-                logger.info(
-                    f"Data Record Created: "
-                    f"{post_response.json()['metadata']['self']['$ref']}"
-                )
-                return post_response.json()
+                try:
+                    logger.info("Creating Record")
+                    post_response = inspire_http_record_management_hook.post_record(
+                        data=new_record, pid_type="data"
+                    )
+                    logger.info(
+                        f"Data Record Created: "
+                        f"{post_response.json()['metadata']['self']['$ref']}"
+                    )
+                    responses.append(post_response.json())
+                except RetryError:
+                    logger.info(
+                        f"Unable to create data record with doi "
+                        f"{new_record['dois'][0]['value']}"
+                    )
+                continue
 
             old_record = response["metadata"]
             revision_id = response.get("revision_id", 0)
             old_record.update(new_record)
             logger.info(f"Updating Record: {old_record['control_number']}")
-            response = inspire_http_record_management_hook.update_record(
-                data=old_record,
-                pid_type="data",
-                control_number=old_record["control_number"],
-                revision_id=revision_id + 1,
-            )
-            logger.info(
-                f"Data Record Updated: "
-                f"{response.json()['metadata']['self']['$ref']}"
-            )
-            return response.json()
+            try:
+                update_response = inspire_http_record_management_hook.update_record(
+                    data=old_record,
+                    pid_type="data",
+                    control_number=old_record["control_number"],
+                    revision_id=revision_id + 1,
+                )
+                logger.info(
+                    f"Data Record Updated: "
+                    f"{update_response.json()['metadata']['self']['$ref']}"
+                )
+            except RetryError:
+                logger.info(
+                    f"Unable to update data record {old_record['control_number']}"
+                )
+            responses.append(update_response.json())
 
-        hepdata_record_versions = download_record_versions(record_id)
-        record = build_record(
-            data_schema=data_schema, inspire_url=url, payload=hepdata_record_versions
-        )
-        record = normalize_collaborations(record)
-        load_record(record)
+        return responses
 
-    process_record.expand(record_id=collect_ids())
+    hepdata_record_versions = download_record_versions(collect_ids())
+    records = build_records(
+        data_schema=data_schema, inspire_url=url, payloads=hepdata_record_versions
+    )
+    records = normalize_collaborations(records)
+    load_records(records)
 
 
-data_harvest_dag()
+bulk_data_harvest_dag()
