619adfe5dc33822dc88f8f2275793f4271f83842
DonHaul <ramiro.animus@gmail.com>
2025-02-11 16:10:15 +0100

    workflows[data]: add a yearly data harvest
    * ref: cern-sis/issues-inspire/issues/760

--- workflows/plugins/hooks/tenacity_config.py
+++ workflows/plugins/hooks/tenacity_config.py
@@ -5,5 +5,5 @@ def tenacity_retry_kwargs():
     return {
         "wait": tenacity.wait_exponential(),
         "stop": tenacity.stop_after_attempt(5),
-        "retry": tenacity.retry_if_exception_type(Exception),
+        "retry": tenacity.retry_if_exception(lambda e: str(e) != "404:NOT FOUND"),
     }
