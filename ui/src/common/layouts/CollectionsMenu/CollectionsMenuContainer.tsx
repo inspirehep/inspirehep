2b2ad3251e86753ee376ef361e406879b0e22cf5
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-02-20 11:02:41 +0100

    ui: enable data in main bar for all users  * ref https://github.com/cern-sis/issues-inspire/issues/764

--- ui/src/common/layouts/CollectionsMenu/CollectionsMenuContainer.tsx
+++ ui/src/common/layouts/CollectionsMenu/CollectionsMenuContainer.tsx
@@ -1,16 +1,9 @@
 import { connect, RootStateOrAny } from 'react-redux';
 
 import CollectionsMenu from './CollectionsMenu';
-import { isCataloger, isSuperUser } from '../../authorization';
 
-const stateToProps = (state: RootStateOrAny) => {
-  const isCatalogerLoggedIn = isCataloger(state.user.getIn(['data', 'roles']));
-  const isSuperUserLoggedIn = isSuperUser(state.user.getIn(['data', 'roles']));
-  return {
-    canAccessDataCollection: isCatalogerLoggedIn || isSuperUserLoggedIn,
-    currentPathname: state.router.location.pathname,
-    currentHash: state.router.location.hash,
-  };
-};
+const stateToProps = (state: RootStateOrAny) => ({
+  currentPathname: state.router.location.pathname,
+});
 
 export default connect(stateToProps)(CollectionsMenu);
