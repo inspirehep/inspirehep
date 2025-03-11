c7f7ffe37cd5468e628f0f5fb1ec9e9020c9430d
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-03-11 15:12:42 +0100

    ui: fix timezone test  * ref https://github.com/cern-sis/issues-inspire/issues/818  * remove snapshots

--- ui/src/seminars/components/__tests__/SeminarCountWarning.test.jsx
+++ ui/src/seminars/components/__tests__/SeminarCountWarning.test.jsx
@@ -4,7 +4,11 @@ import SeminarCountWarning from '../SeminarCountWarning';
 
 describe('SeminarCountWarning', () => {
   it('renders', () => {
-    const { asFragment } = render(<SeminarCountWarning />);
-    expect(asFragment()).toMatchSnapshot();
+    const { getByText } = render(<SeminarCountWarning />);
+    expect(
+      getByText(
+        'Please note that the list of seminars is managed by users and INSPIRE makes no claims as to its completeness and accuracy.'
+      )
+    ).toBeInTheDocument();
   });
 });
