a85057542c779a026113abaf86ddfb22801d08cd
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-03-18 15:53:53 +0100

    monitoring: error messagees for hal push  * ref https://github.com/cern-sis/issues-inspire/issues/822

--- backend/inspirehep/logger/config.py
+++ backend/inspirehep/logger/config.py
@@ -82,6 +82,7 @@ FAILURE_MESSAGE_BY_TASK = {
     "inspirehep.indexer.tasks.index_record": "inspirehep.utils.get_failure_message_for_index_record",
     "inspirehep.matcher.tasks.match_references_by_uuids": "inspirehep.utils.get_failure_message_for_match_references_by_uuids",
     "inspirehep.records.tasks.redirect_references_to_merged_record": "inspirehep.utils.get_failure_message_for_redirect_references_to_merged_record",
+    "inspirehep.hal.tasks.hal_push": "inspirehep.utils.get_failure_message_for_hal_push",
 }
 
 
