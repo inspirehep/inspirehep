dcf505e6dbc41e658de335cbdef9d5be996df19e
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-03-24 13:41:12 +0100

    workflows: fix log url

--- workflows/plugins/include/utils/alerts.py
+++ workflows/plugins/include/utils/alerts.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from urllib.parse import urlparse, urlunparse
 
 from airflow.utils.email import send_email
 
@@ -14,10 +15,20 @@ def task_failure_alert(context):
         logger.error("Cannot send email.")
         return
 
+    base_url = os.environ.get("AIRFLOW_BASE_URL")
+    task_instance = context["task_instance"]
+    parsed_log_url = urlparse(task_instance.log_url)
+    parsed_base_url = urlparse(base_url)
+    new_log_url = urlunparse(
+        parsed_log_url._replace(
+            scheme=parsed_base_url.scheme, netloc=parsed_base_url.netloc
+        )
+    )
+
     body = f"""
     <b>DAG</b>: `{context['dag'].dag_id}`<br>
-    <b>Task</b>: `{context['task_instance'].task_id}`<br>
-    <b>Log URL</b>: <a href="{context['task_instance'].log_url}">Log URL</a><br>
+    <b>Task</b>: `{task_instance.task_id}`<br>
+    <b>Log URL</b>: <a href="{new_log_url}">Log URL</a><br>
     """
     send_email(
         to=[recipient],
