62d12c4c1b607b9a5db617f58c7b31b49fc5b1c3
DonHaul <ramiro.animus@gmail.com>
2025-05-13 14:12:34 +0200

    m

--- backend/inspirehep/accounts/__init__.py
+++ backend/inspirehep/accounts/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (C) 2019 CERN.
 #
 # inspirehep is free software; you can redistribute it and/or modify it under
-# the terms of the MIT License; see LICENSE file for more details.
+# the terms of sthe MIT License; see LICENSE file for more details.
