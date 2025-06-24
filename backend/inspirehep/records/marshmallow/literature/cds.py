283dfdecbf2cfab00d2d5684c918167ece141bed
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-06-24 14:31:12 +0200

    cds: optimize query  * ref https://github.com/cern-sis/issues-inspire/issues/876

--- backend/inspirehep/records/marshmallow/literature/cds.py
+++ backend/inspirehep/records/marshmallow/literature/cds.py
@@ -1,5 +1,4 @@
 from inspire_dojson.utils import get_recid_from_ref
-from invenio_pidstore.errors import PIDDoesNotExistError
 from marshmallow import fields
 
 from inspirehep.records.api import InspireRecord
@@ -8,44 +7,57 @@ from inspirehep.records.marshmallow.literature.base import (
 )
 
 
-def merge_orcids(author):
+def merge_orcids(author, resolved_authors_by_id):
     recid = get_recid_from_ref(author.get("record", {}))
     has_orcid = any(
         author_id.get("schema") == "ORCID" for author_id in author.get("ids", [])
     )
-    if recid and not has_orcid:
-        try:
-            author_record = InspireRecord.get_record_by_pid_value(
-                pid_type="aut", pid_value=recid
-            )
-            orcids = [
-                id_
-                for id_ in author_record.get("ids", [])
-                if id_.get("schema") == "ORCID"
-            ]
-            if orcids:
-                if author.get("ids"):
-                    author["ids"].extend(orcids)
-                else:
-                    author["ids"] = orcids
-        except PIDDoesNotExistError:
-            pass
+    if not recid or has_orcid:
+        return
+
+    author_record = resolved_authors_by_id.get(recid)
+    if not author_record:
+        return
+
+    orcids = [
+        id_ for id_ in author_record.get("ids", []) if id_.get("schema") == "ORCID"
+    ]
+    if orcids:
+        if author.get("ids"):
+            author["ids"].extend(orcids)
+        else:
+            author["ids"] = orcids
 
 
-def collect_ror_ids(author):
+def merge_affiliation_identifiers(author, resolved_institutions_by_id):
+    has_ror = any(
+        aff_id.get("schema") == "ROR"
+        for aff_id in author.get("affiliations_identifiers", [])
+    )
+    if has_ror:
+        return
+
     ror_ids = []
-    institutions = InspireRecord.get_linked_records_from_dict_field(
+    institutions_linked_pids = InspireRecord._get_linked_pids_from_field(
         author, "affiliations.record"
     )
-    for institution in institutions:
-        ror = [
+    for _, recid in institutions_linked_pids:
+        institution_record = resolved_institutions_by_id.get(recid)
+        if not institution_record:
+            continue
+
+        rors = [
             identifier
-            for identifier in institution.get("external_system_identifiers", [])
+            for identifier in institution_record.get("external_system_identifiers", [])
             if identifier.get("schema") == "ROR"
         ]
-        if ror:
-            ror_ids.extend(ror)
-    return ror_ids
+        ror_ids.extend(rors)
+
+    if ror_ids:
+        if author.get("affiliations_identifiers"):
+            author["affiliations_identifiers"].extend(ror_ids)
+        else:
+            author["affiliations_identifiers"] = ror_ids
 
 
 class LiteraturePublicSchemaCDS(LiteraturePublicSchema):
@@ -54,12 +66,21 @@ class LiteraturePublicSchemaCDS(LiteraturePublicSchema):
     @staticmethod
     def get_authors(data):
         updated_authors = []
+        author_records = InspireRecord.get_linked_records_from_dict_field(
+            data, "authors.record"
+        )
+        institution_records = InspireRecord.get_linked_records_from_dict_field(
+            data, "authors.affiliations.record"
+        )
+        resolved_authors_by_id = {
+            record["control_number"]: record for record in author_records
+        }
+        resolved_institutions_by_id = {
+            str(record["control_number"]): record for record in institution_records
+        }
         for author in data.get("authors", []):
-            merge_orcids(author)
-            ror_ids = collect_ror_ids(author)
-            if ror_ids:
-                author["affiliations_identifiers"] = ror_ids
-
+            merge_orcids(author, resolved_authors_by_id)
+            merge_affiliation_identifiers(author, resolved_institutions_by_id)
             updated_authors.append(author)
 
         return updated_authors
