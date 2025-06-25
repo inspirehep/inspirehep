14c4eba5b6f83c2f7383e0c7477619e938e93efd
DonHaul <ramiro.animus@gmail.com>
2025-06-25 10:30:36 +0200

    cli: fix delete_author

--- backoffice/backoffice/management/utils.py
+++ backoffice/backoffice/management/utils.py
@@ -3,11 +3,4 @@ from django.conf import settings
 
 
 def get_opensearch_client():
-    hosts_config = settings.OPENSEARCH_DSL.get("default", {}).get("hosts", [])
-    host, port = (
-        hosts_config.split(":")
-        if isinstance(hosts_config, str)
-        else hosts_config[0].split(":")
-    )
-
-    return OpenSearch(hosts=[{"host": host, "port": port}])
+    return OpenSearch(**settings.OPENSEARCH_DSL["default"])
