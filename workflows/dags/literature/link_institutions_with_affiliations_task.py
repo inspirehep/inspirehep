4d6d54ba192b1f826638499b440136534c6ae3b7
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2025-12-09 11:12:02 +0100

    workflows: fix KeyError  * ref https://github.com/cern-sis/issues-inspire/issues/1214

--- workflows/dags/literature/link_institutions_with_affiliations_task.py
+++ workflows/dags/literature/link_institutions_with_affiliations_task.py
@@ -3,6 +3,7 @@ from airflow.sdk import Variable, task
 from airflow.utils.trigger_rule import TriggerRule
 from hooks.inspirehep.inspire_http_hook import InspireHttpHook
 from include.utils import s3
+from inspire_utils.record import get_value
 
 
 @task(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
@@ -13,7 +14,8 @@ def link_institutions_with_affiliations(**context):
 
     workflow_id = context["params"]["workflow_id"]
     workflow_data = s3.read_workflow(s3_hook, bucket_name, workflow_id)
-    if not workflow_data["data"]["authors"]:
+    authors = get_value(workflow_data, "data.authors", [])
+    if not authors:
         return
 
     authors_with_linked_institutions_response = inspire_http_hook.call_api(
@@ -21,7 +23,11 @@ def link_institutions_with_affiliations(**context):
         method="GET",
         json={"authors": workflow_data["data"]["authors"]},
     )
-    authors = authors_with_linked_institutions_response.json()["authors"]
-    workflow_data["data"]["authors"] = authors
+    authors_json = authors_with_linked_institutions_response.json()
+    updated_authors = authors_json.get("authors", [])
+    if not updated_authors:
+        return
+
+    workflow_data["data"]["authors"] = updated_authors
 
     s3.write_workflow(s3_hook, workflow_data, bucket_name)
