1fe5f96d5130e7bdc9e6091e9e510a1e7bfa097f
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2026-01-29 15:47:52 +0100

    record-editor: display validation errors  * ref https://github.com/cern-sis/issues-inspire/issues/1291

--- record-editor/src/config.js
+++ record-editor/src/config.js
@@ -1,6 +1,7 @@
 (() => {
     const CONFIG = {
-      EDITOR_SCHEMA_URL: 'https://inspirebeta.net/schemas/records/authors.json',
+      EDITOR_AUTHORS_SCHEMA_URL: 'https://inspirebeta.net/schemas/records/authors.json',
+      EDITOR_HEP_SCHEMA_URL: 'https://inspirebeta.net/schemas/records/hep.json',
       EDITOR_BACKOFFICE_API_URL: 'http://localhost:8001/api',
     };
     // Currently used only for local development
