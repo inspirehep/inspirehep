4132bda3e94aebcac24c05fafaf8e2e589c23473
DonHaul <ramiro.animus@gmail.com>
2026-02-05 12:45:50 +0100

    workflows: refactor normalize_journal_entries
    * ref: cern-sis/issues-inspire/issues/1232

--- workflows/plugins/include/inspire/journal_title_normalization.py
+++ workflows/plugins/include/inspire/journal_title_normalization.py
@@ -1,23 +1,23 @@
-def process_entries(
-    entries,
-    title_getter,
-    normalized_titles_map,
-    normalized_references_map,
-    normalized_categories_map,
-    workflow_data,
-):
-    """Normalize journal titles and enrich with DB info."""
-    for entry in entries:
-        journal_title = title_getter(entry)
-        if not journal_title:
-            continue
-        normalized_title = normalized_titles_map.get(journal_title)
-        journal_ref = normalized_references_map.get(journal_title)
-        journal_inspire_categories = normalized_categories_map.get(journal_title)
-        entry["journal_title"] = normalized_title
-        if journal_ref:
-            entry["journal_record"] = journal_ref
-        if journal_inspire_categories:
-            workflow_data.setdefault("journal_inspire_categories", []).extend(
-                journal_inspire_categories
-            )
+import copy
+
+from inspire_utils.record import get_value
+
+
+def get_normalized_publication_info(publication_info, normalized_maps):
+    journal_title = get_value(publication_info, "journal_title")
+    if not journal_title:
+        return publication_info
+
+    normalized_title = get_value(normalized_maps, "normalized_journal_titles", []).get(
+        journal_title
+    )
+    normalized_journal_ref = get_value(
+        normalized_maps, "normalized_journal_references", []
+    ).get(journal_title)
+
+    normalized_publication_info = copy.deepcopy(publication_info)
+    normalized_publication_info["journal_title"] = normalized_title
+    if normalized_journal_ref:
+        normalized_publication_info["journal_record"] = normalized_journal_ref
+
+    return normalized_publication_info
