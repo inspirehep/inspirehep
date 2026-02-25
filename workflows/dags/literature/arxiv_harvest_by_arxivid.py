7feb44eb56e7eef0a0100479bef8b62598a0d118
DonHaul <ramiro.animus@gmail.com>
2026-02-25 14:03:39 +0100

    wokrflows: s3 refactor
    * ref: cern-sis/issues-inspire/issues/1336

--- workflows/dags/literature/arxiv_harvest_by_arxivid.py
+++ workflows/dags/literature/arxiv_harvest_by_arxivid.py
@@ -1,11 +1,10 @@
 import logging
 
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.sdk import Param, Variable, dag, task
+from airflow.sdk import Param, dag, task
 from hooks.backoffice.workflow_management_hook import HEP, WorkflowManagementHook
 from include.utils.alerts import FailedDagNotifier
 from include.utils.arxiv import build_records, fetch_record_by_id, load_records
-from include.utils.s3 import write_object
+from include.utils.s3 import S3JsonStore
 from literature.check_failures_task import check_failures
 
 logger = logging.getLogger(__name__)
@@ -50,8 +49,7 @@ def arxiv_harvest_by_arxivid_dag():
 
         workflow_management_hook = WorkflowManagementHook(HEP)
 
-        s3_hook = S3Hook(aws_conn_id="s3_conn")
-        bucket_name = Variable.get("s3_bucket_name")
+        s3_store = S3JsonStore(aws_conn_id="s3_conn")
 
         xml_records = []
         for arxiv_id in arxiv_ids:
@@ -69,13 +67,11 @@ def arxiv_harvest_by_arxivid_dag():
 
         failed_load_records = load_records(parsed_records, workflow_management_hook)
 
-        return write_object(
-            s3_hook,
+        return s3_store.write_object(
             {
                 "failed_build_records": failed_build_records,
                 "failed_load_records": failed_load_records,
-            },
-            bucket_name,
+            }
         )
 
     arxiv_ids = get_arxiv_ids()
--- workflows/dags/literature/validate.py
+++ workflows/dags/literature/validate.py
@@ -1,6 +1,5 @@
 from airflow.exceptions import AirflowFailException
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.sdk import Variable, task
+from airflow.sdk import task
 from hooks.backoffice.workflow_management_hook import (
     HEP,
     WorkflowManagementHook,
@@ -12,8 +11,7 @@ from include.utils.constants import STATUS_ERROR_VALIDATION
 @task(on_failure_callback=None)
 def validate_record(**context):
     workflow_id = context["params"]["workflow_id"]
-    s3_hook = S3Hook(aws_conn_id="s3_conn")
-    bucket_name = Variable.get("s3_bucket_name")
+    s3_store = s3.S3JsonStore(aws_conn_id="s3_conn")
     workflow_management_hook = WorkflowManagementHook(HEP)
     workflow_data = workflow_management_hook.get_workflow(workflow_id, validate=True)
 
@@ -26,4 +24,4 @@ def validate_record(**context):
         raise AirflowFailException(
             f"Validation failed for workflow {workflow_id}: {validation_errors}"
         )
-    s3.write_workflow(s3_hook, workflow_data, bucket_name)
+    s3_store.write_workflow(workflow_data)
