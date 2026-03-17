7a07e9c63be77b222275a4dff3c16693ea8245c6
Ioannis Tsanaktsidis <tsanakts.10@gmail.com>
2026-03-17 14:41:24 +0100

    backoffice: raise max_page_size to match ui  * ref https://github.com/cern-sis/issues-inspire/issues/1392

--- backoffice/backoffice/utils/pagination.py
+++ backoffice/backoffice/utils/pagination.py
@@ -4,4 +4,4 @@ from django_elasticsearch_dsl_drf.pagination import QueryFriendlyPageNumberPagin
 class OSStandardResultsSetPagination(QueryFriendlyPageNumberPagination):
     page_size = 10
     page_size_query_param = "size"
-    max_page_size = 100
+    max_page_size = 250
