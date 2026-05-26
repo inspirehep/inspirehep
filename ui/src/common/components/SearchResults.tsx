9679dd60a66fab88a9da0dd3e614c25d7eb3c7e9
Elise Recejac <elise.recejac@cern.ch>
2026-05-26 16:12:24 +0200

    backoffice(ui): remove useless isBackoffice condition * ref: https://github.com/cern-sis/issues-inspire/issues/1495

--- ui/src/common/components/SearchResults.tsx
+++ ui/src/common/components/SearchResults.tsx
@@ -9,33 +9,22 @@ const SearchResults = ({
   results,
   page,
   pageSize,
-  isBackoffice = false,
 }: {
   renderItem: Function;
   isCatalogerLoggedIn: boolean;
   results: List<any>;
   page: number | undefined;
   pageSize: number | undefined;
-  isBackoffice?: boolean;
 }) => {
-  const renderResults = (result: any, index: number) => {
-    if (!isBackoffice) {
-      return (
-        <div className="mv2" key={result.get('id')}>
-          {renderItem(
-            result,
-            isCatalogerLoggedIn,
-            getSearchRank(index, page || 1, pageSize || 25)
-          )}
-        </div>
-      );
-    }
-    return (
-      <div className="mv2" key={result?.get('id')}>
-        {renderItem(result)}
-      </div>
-    );
-  };
+  const renderResults = (result: any, index: number) => (
+    <div className="mv2" key={result.get('id')}>
+      {renderItem(
+        result,
+        isCatalogerLoggedIn,
+        getSearchRank(index, page || 1, pageSize || 25)
+      )}
+    </div>
+  );
   return (
     <div data-testid="search-results">
       {results.map((result, index) => renderResults(result, index))}
