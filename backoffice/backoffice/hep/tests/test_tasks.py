6fa4af8f3fc21b8471b220153415c1be349454d1
DonHaul <ramiro.animus@gmail.com>
2026-07-07 17:13:11 +0200

    backoffice: instant batch
    * ref: cern-sis/issues-inspire/issues/1579

--- backoffice/backoffice/hep/tests/test_tasks.py
+++ backoffice/backoffice/hep/tests/test_tasks.py
@@ -16,12 +16,12 @@ class TestBatchResolveWorkflowsTask(BaseTransactionTestCase):
     def _create_workflow(self):
         return HepWorkflow.objects.create(
             data={},
-            status=HepStatusChoices.APPROVAL,
+            status=HepStatusChoices.RUNNING,
             workflow_type=HepWorkflowType.HEP_CREATE,
             id=uuid.uuid4(),
         )
 
-    @patch("backoffice.hep.utils.airflow_utils.clear_airflow_dag_tasks")
+    @patch("backoffice.common.airflow_utils.clear_airflow_dag_tasks")
     def test_batch_resolve_workflows_resolves_all(self, mock_clear):
         mock_clear.return_value = (b"", 200)
         wfs = [self._create_workflow() for _ in range(3)]
@@ -30,14 +30,14 @@ class TestBatchResolveWorkflowsTask(BaseTransactionTestCase):
             "ids": [str(wf.id) for wf in wfs],
         }
 
-        batch_resolve_workflows(data, self.curator.id)
+        batch_resolve_workflows(data)
 
+        self.assertEqual(mock_clear.call_count, 3)
         for wf in wfs:
             wf.refresh_from_db()
             self.assertEqual(wf.status, HepStatusChoices.RUNNING)
-            self.assertEqual(wf.decisions.first().action, HepResolutions.hep_accept)
 
-    @patch("backoffice.hep.utils.airflow_utils.clear_airflow_dag_tasks")
+    @patch("backoffice.common.airflow_utils.clear_airflow_dag_tasks")
     def test_batch_resolve_workflows_marks_failures_as_error(self, mock_clear):
         ok = (b"", 200)
         mock_clear.side_effect = [ok, RequestException("Airflow clear failed"), ok]
@@ -47,13 +47,12 @@ class TestBatchResolveWorkflowsTask(BaseTransactionTestCase):
             "ids": [str(wf.id) for wf in wfs],
         }
 
-        batch_resolve_workflows(data, self.curator.id)
+        batch_resolve_workflows(data)
 
+        self.assertEqual(mock_clear.call_count, 3)
         wfs[0].refresh_from_db()
         wfs[1].refresh_from_db()
         wfs[2].refresh_from_db()
         self.assertEqual(wfs[0].status, HepStatusChoices.RUNNING)
         self.assertEqual(wfs[1].status, HepStatusChoices.ERROR)
         self.assertEqual(wfs[2].status, HepStatusChoices.RUNNING)
-
-        self.assertEqual(wfs[1].decisions.first().action, HepResolutions.hep_accept)
