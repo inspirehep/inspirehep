80e6865b04b6e06a86bd5ca52a6fad0a03a059bf
DonHaul <ramiro.animus@gmail.com>
2026-08-05 15:22:57 +0200

    workflows: fix status being wrongly re-set to PROCESSING
    * ref: cern-sis/issues-inspire/issues/1642

--- workflows/dags/literature/set_workflow_status_tasks.py
+++ workflows/dags/literature/set_workflow_status_tasks.py
@@ -3,9 +3,11 @@ from hooks.backoffice.workflow_management_hook import (
     HEP,
     WorkflowManagementHook,
 )
+from include.utils import s3
 from include.utils.constants import STATUS_RUNNING
 
 workflow_management_hook = WorkflowManagementHook(HEP)
+s3_store = s3.S3JsonStore(aws_conn_id="s3_conn")
 
 
 @task
@@ -15,6 +17,8 @@ def set_workflow_status_to_running(
     """
     Set the status of the workflow to the given status name.
     """
-    workflow_management_hook.set_workflow_status(
-        status_name=STATUS_RUNNING, workflow_id=context["params"]["workflow_id"]
+    response = workflow_management_hook.set_workflow_status(
+        status_name=STATUS_RUNNING,
+        workflow_id=context["params"]["workflow_id"],
     )
+    s3_store.write_workflow(response.json())
