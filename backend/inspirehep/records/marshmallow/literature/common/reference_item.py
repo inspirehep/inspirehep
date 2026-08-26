3cbb7b4922c17a3b69602a9e271e703a88d8dd9d
Elise Recejac <elise.recejac@cern.ch>
2026-08-26 13:21:44 +0200

    backend: remove everything linked to ReferenceDiff page * ref: https://github.com/cern-sis/issues-inspire/issues/1685

--- backend/inspirehep/records/marshmallow/literature/common/reference_item.py
+++ backend/inspirehep/records/marshmallow/literature/common/reference_item.py
@@ -6,7 +6,7 @@
 
 from inspire_dojson.utils import get_recid_from_ref, strip_empty_values
 from inspire_utils.helpers import force_list
-from inspire_utils.record import get_value, get_values_for_schema
+from inspire_utils.record import get_value
 from inspirehep.records.marshmallow.fields.list_with_limit import ListWithLimit
 from inspirehep.records.marshmallow.fields.nested_without_empty_objects import (
     NestedField,
@@ -133,14 +133,3 @@ class ReferenceItemSchemaV1(Schema):
     @post_dump
     def strip_empty(self, data):
         return strip_empty_values(data)
-
-
-class ReferenceItemSchemaV2(ReferenceItemSchemaV1):
-    raw_ref = fields.Raw()
-
-    def get_reference_data(self, reference, reference_records):
-        reference_data = super().get_reference_data(reference, reference_records)
-        raw_refs = get_values_for_schema(reference.get("raw_refs", []), "text")
-        if raw_refs:
-            reference_data["raw_ref"] = raw_refs[0]
-        return reference_data
